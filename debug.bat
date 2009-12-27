8aa5634b59cd915a4204e8c227017c73fc66a477
drulli <drulli@dev.java.net>
2009-12-27 22:33:59 +0000

    Removed old files.

--- debug.bat
+++
@@ -1,2 +0,0 @@
-set MAVEN_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n
-mvn hpi:run
--- debug.sh
+++
@@ -1,3 +0,0 @@
-export MAVEN_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n"
-rm -rf $HUDSON_HOME/plugins/checkstyle*
-mvn clean hpi:run
