e7425a53e7cf5e3ca1d9449f8b3521e5d7253887
drulli <drulli@dev.java.net>
2010-08-10 12:04:16 +0000

    Improved regexp for Eclipse compiler to handle multi-line source code references.

--- src/test/java/hudson/plugins/warnings/parser/AntEclipseParserTest.java
+++ src/test/java/hudson/plugins/warnings/parser/AntEclipseParserTest.java
@@ -6,9 +6,13 @@ import hudson.plugins.analysis.util.model.Priority;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 /**
  * Tests the class {@link AntEclipseParser}.
@@ -59,6 +63,60 @@ public class AntEclipseParserTest extends ParserTester {
                 "C:/Desenvolvimento/Java/jfg/src/jfg/AttributeException.java",
                 AntEclipseParser.WARNING_TYPE, "", Priority.NORMAL);
     }
+
+    /**
+     * Parses a warning log with 2 eclipse messages, the affected source text spans one and two lines.
+     *
+     * @throws IOException
+     *      if the file could not be read
+     * @see <a href="http://issues.hudson-ci.org/browse/HUDSON-7077">Issue 7077</a>
+     */
+    @Test
+    public void issue7077() throws IOException {
+        Collection<FileAnnotation> warnings = new AntEclipseParser().parse(openFile("issue7077.txt"));
+        List<FileAnnotation> sorted = Lists.newArrayList(warnings);
+        Collections.sort(sorted);
+
+        assertEquals(WRONG_NUMBER_OF_WARNINGS_DETECTED, 2, warnings.size());
+
+        checkWarning(sorted.get(1),
+                90,
+                "Type safety: The method setBoHandler(BoHandler) belongs to the raw type BoQuickSearchControl.Builder. References to generic type BoQuickSearchControl<S>.Builder<T> should be parameterized",
+                "/ige/hudson/work/jobs/esvclient__development/workspace/target/rcp-build/plugins/ch.ipi.esv.client.customer/src/main/java/ch/ipi/esv/client/customer/search/CustomerQuickSearch.java",
+                AntEclipseParser.WARNING_TYPE, "", Priority.NORMAL);
+        checkWarning(sorted.get(0),
+                90,
+                "Type safety: The expression of type BoQuickSearchControl needs unchecked conversion to conform to BoQuickSearchControl<CustomerBO>",
+                "/ige/hudson/work/jobs/esvclient__development/workspace/target/rcp-build/plugins/ch.ipi.esv.client.customer/src/main/java/ch/ipi/esv/client/customer/search/CustomerQuickSearch.java",
+                AntEclipseParser.WARNING_TYPE, "", Priority.NORMAL);
+    }
+
+    /**
+     * Parses a warning log with several eclipse messages.
+     *
+     * @throws IOException
+     *      if the file could not be read
+     * @see <a href="http://issues.hudson-ci.org/browse/HUDSON-7077">Issue 7077</a>
+     */
+    @Test
+    public void issue7077all() throws IOException {
+        Collection<FileAnnotation> warnings = new AntEclipseParser().parse(openFile("issue7077-all.txt"));
+        List<FileAnnotation> sorted = Lists.newArrayList(warnings);
+        Collections.sort(sorted);
+
+        assertEquals(WRONG_NUMBER_OF_WARNINGS_DETECTED, 45, sorted.size());
+
+        int number = 0;
+        for (FileAnnotation fileAnnotation : sorted) {
+            boolean containsHat = fileAnnotation.getMessage().contains("^");
+            if (containsHat) {
+                System.err.println(fileAnnotation);
+            }
+            assertFalse("Message " + number + " contains ^", containsHat);
+            number++;
+        }
+    }
+
 
     /** {@inheritDoc} */
     @Override
