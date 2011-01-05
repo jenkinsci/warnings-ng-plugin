6800315e859802439e9646cce27a853b4371b916
drulli <drulli@dev.java.net>
2011-01-05 14:05:40 +0000

    [HUDSON-8399] Added a multi-line dynamic parser class.

--- src/main/java/hudson/plugins/warnings/parser/DynamicDocumentParser.java
+++ src/main/java/hudson/plugins/warnings/parser/DynamicDocumentParser.java
@@ -3,16 +3,16 @@ package hudson.plugins.warnings.parser;
 import java.util.regex.Matcher;
 
 /**
- * A line parser that uses a configurable regular expression and Groovy script
- * to parse warnings.
+ * A multi-line parser that uses a configurable regular expression and Groovy
+ * script to parse warnings.
  *
  * @author Ulli Hafner
  */
-public class DynamicParser extends RegexpLineParser {
+public class DynamicDocumentParser extends RegexpLineParser {
     private final GroovyExpressionMatcher expressionMatcher;
 
     /**
-     * Creates a new instance of {@link DynamicParser}.
+     * Creates a new instance of {@link DynamicDocumentParser}.
      *
      * @param name
      *            name of the parser
@@ -21,7 +21,7 @@ public class DynamicParser extends RegexpLineParser {
      * @param script
      *            Groovy script
      */
-    public DynamicParser(final String name, final String regexp, final String script) {
+    public DynamicDocumentParser(final String name, final String regexp, final String script) {
         super(regexp, name, true);
 
         expressionMatcher = new GroovyExpressionMatcher(script, FALSE_POSITIVE);
