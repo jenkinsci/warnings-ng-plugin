0f6bbb865c3a4d2e86bf573f1af46be251c01a2a
drulli <drulli@dev.java.net>
2011-02-07 16:07:02 +0000

    Deleted sources and added link to new home of plug-in.

--- go.sh
+++
@@ -1,7 +0,0 @@
-rm -rf $HUDSON_HOME/plugins/warnings*
-
-mvn install
-cp -f target/*.hpi $HUDSON_HOME/plugins/
-
-cd $HUDSON_HOME
-java -jar hudson.war
--- release.sh
+++
@@ -1,4 +0,0 @@
-svn update
-mvn -B -Djava.net.id=drulli release:prepare release:perform 
-
-
