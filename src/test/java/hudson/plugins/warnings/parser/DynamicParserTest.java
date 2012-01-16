e1f36ebe085e4835c26529b602507d26db7faaa6
Ulli Hafner <ullrich.hafner@gmail.com>
2012-01-16 21:09:52 +0100

    [JENKINS-11926] Added test case.

--- src/test/java/hudson/plugins/warnings/parser/DynamicParserTest.java
+++ src/test/java/hudson/plugins/warnings/parser/DynamicParserTest.java
@@ -1,12 +1,18 @@
 package hudson.plugins.warnings.parser;
 
 import static junit.framework.Assert.*;
+import hudson.plugins.analysis.util.StringPluginLogger;
 import hudson.plugins.analysis.util.model.FileAnnotation;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 /**
  * Test the class {@link DynamicParser}.
@@ -34,11 +40,11 @@ public class DynamicParserTest extends PhpParserTest {
 
 
     /**
-     * Parses a file with one warning that are started by ant.
+     * Parses a file with 9 warnings of a custom parser.
      *
      * @throws IOException
      *      if the file could not be read
-     * @see <a href="http://issues.jenkins-ci.org/browse/JENKINS-9926">Issue 9926</a>
+     * @see <a href="http://issues.jenkins-ci.org/browse/JENKINS-12280">Issue 12280</a>
      */
     @Test
     public void issue12280() throws IOException {
@@ -53,6 +59,59 @@ public class DynamicParserTest extends PhpParserTest {
 
         assertEquals(WRONG_NUMBER_OF_WARNINGS_DETECTED, 9, warnings.size());
     }
+
+    /**
+     * Parses a file with several warnings from a custom parser.
+     *
+     * @throws IOException
+     *      if the file could not be read
+     * @see <a href="http://issues.jenkins-ci.org/browse/JENKINS-11926">Issue 11926</a>
+     */
+    @Test
+    public void issue11926() throws IOException {
+        Collection<FileAnnotation> warnings = createCustomParser().parse(openFile("issue11926.txt"));
+
+        assertEquals(WRONG_NUMBER_OF_WARNINGS_DETECTED, 4, warnings.size());
+    }
+
+
+    private DynamicParser createCustomParser() {
+        return new DynamicParser("issue11926",
+                "FLMSG (\\S+)\\s+([0-9]+) ([^\\s,]+),([0-9]+) (\\S+)",
+                "import hudson.plugins.warnings.parser.Warning\n"
+                        + "String fileName = matcher.group(3)\n"
+                        + "String code = matcher.group(2)\n"
+                        + "String lineNumber = matcher.group(4)\n"
+                        + "String type = matcher.group(1)\n"
+                        + "String message = matcher.group(5)\n"
+                        + "return new Warning(fileName, Integer.parseInt(lineNumber), type, code, message);");
+    }
+
+    /**
+     * Parses a file with several warnings from a custom parser.
+     *
+     * @throws IOException
+     *      if the file could not be read
+     * @see <a href="http://issues.jenkins-ci.org/browse/JENKINS-11926">Issue 11926</a>
+     */
+    @Test
+    public void issueReadFromFile() throws IOException {
+        File file = File.createTempFile("warnings", "test");
+        FileUtils.writeStringToFile(file, IOUtils.toString(openFile("issue11926.txt")), "UTF-8");
+
+        DynamicParser dynamicParser = createCustomParser();
+        ParserRegistry registry = new ParserRegistry(Lists.newArrayList(dynamicParser), "UTF-8", "", "");
+
+        file.deleteOnExit();
+
+        StringPluginLogger logger = new StringPluginLogger("warnings");
+        Collection<FileAnnotation> warnings = registry.parse(file, logger);
+
+        assertEquals("Wrong logging message", "[warnings] issue11926 : Found 4 warnings.\n", logger.toString());
+        assertEquals(WRONG_NUMBER_OF_WARNINGS_DETECTED, 1, warnings.size());
+    }
+
+
 
     /** {@inheritDoc} */
     @Override
