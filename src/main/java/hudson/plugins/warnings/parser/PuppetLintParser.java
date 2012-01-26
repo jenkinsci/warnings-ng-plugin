0d884547515bf9729849fe17b62570f76f07f080
Jan Vansteenkiste <jan@vstone.eu>
2012-01-26 19:15:59 +0100

    Detect puppet module and use it as packageName

--- src/main/java/hudson/plugins/warnings/parser/PuppetLintParser.java
+++ src/main/java/hudson/plugins/warnings/parser/PuppetLintParser.java
@@ -2,7 +2,8 @@ package hudson.plugins.warnings.parser;
 
 import hudson.plugins.analysis.util.model.Priority;
 import java.util.regex.Matcher;
-
+import java.util.regex.Pattern;
+import org.apache.commons.lang.StringUtils;
 /**
  * A parser for puppet-lint checks warnings.
  *
@@ -14,24 +15,29 @@ public class PuppetLintParser extends RegexpLineParser {
     static final String WARNING_TYPE = "Puppet-Lint";
 
     /** Pattern of puppet-lint compiler warnings. */
-    private static final String PUPPET_LINT_PATTERN = "^\\s*([^:]+):([0-9]+):([^:]+):([^:]+):\\s*(.*)$";
+    private static final String PUPPET_LINT_PATTERN_WARNING = "^\\s*([^:]+):([0-9]+):([^:]+):([^:]+):\\s*(.*)$";
+    
+    private static final String PUPPET_LINT_PATTERN_PACKAGE = "^(.*/?modules/)?([^/]*)/manifests(.*)?(/([^/]*)\\.pp)$";
 
+    private final Pattern packagePattern;
+    
     /**
      * Creates a new instance of <code>PuppetLintParser</code>.
      */
     public PuppetLintParser() {
-        super(PUPPET_LINT_PATTERN, WARNING_TYPE);
+        super(PUPPET_LINT_PATTERN_WARNING, WARNING_TYPE);
+        packagePattern = Pattern.compile(PUPPET_LINT_PATTERN_PACKAGE);
     }
 
     /** {@inheritDoc} */
     @Override
     protected Warning createWarning(final Matcher matcher) {
-
-        String fileName = matcher.group(1);
-        String start = matcher.group(2);
-        String category = matcher.group(3);
-        String level = matcher.group(4);
-        String message = matcher.group(5);
+        
+        final String fileName = matcher.group(1);
+        final String start = matcher.group(2);
+        final String category = matcher.group(3);
+        final String level = matcher.group(4);
+        final String message = matcher.group(5);
 
         Priority priority = Priority.NORMAL;
 
@@ -39,7 +45,38 @@ public class PuppetLintParser extends RegexpLineParser {
             priority = Priority.HIGH;
         }
 
-        return new Warning(fileName, Integer.parseInt(start), WARNING_TYPE, category, message, priority);
+        Warning warning = new Warning(fileName, Integer.parseInt(start), WARNING_TYPE, category, message, priority);
+        String moduleName = detectModuleName(fileName);
+        if (!StringUtils.isBlank(moduleName)) {
+            warning.setPackageName(moduleName);
+        }
+        return warning;
     }
+    
+    
+    /**
+     * Detects the module name for the specified filename.
+     *
+     * @param warning the warning
+     */
+    private String detectModuleName(final String fileName) {
+        if (! StringUtils.isBlank(fileName)) {
+            Matcher matcher = packagePattern.matcher(fileName);
+            if (matcher.find()) {
+                String main = matcher.group(2);
+                String subclassed = matcher.group(3);
+                //String klass = matcher.group(5);
+                String module = "::" + main;
+                if (! StringUtils.isBlank(subclassed)) {
+                    module += StringUtils.replace(subclassed, "/","::");
+                }
+                return module;
+            }
+        }
+        return StringUtils.EMPTY;
+    }
+    
+    
+    
 }
 
--- src/test/java/hudson/plugins/warnings/parser/PuppetLintParserTest.java
+++ src/test/java/hudson/plugins/warnings/parser/PuppetLintParserTest.java
@@ -1,5 +1,7 @@
 package hudson.plugins.warnings.parser;
 
+import static junit.framework.Assert.assertEquals;
+
 import hudson.plugins.analysis.util.model.FileAnnotation;
 import hudson.plugins.analysis.util.model.Priority;
 import java.io.IOException;
@@ -15,7 +17,7 @@ import org.junit.Test;
 public class PuppetLintParserTest extends ParserTester {
     /**
      * Tests the Puppet-Lint parsing.
-     *
+     * 
      */
     @Test
     public void testParse() throws IOException {
@@ -23,21 +25,46 @@ public class PuppetLintParserTest extends ParserTester {
         Iterator<FileAnnotation> iterator = results.iterator();
 
         FileAnnotation annotation = iterator.next();
-        checkWarning(annotation,
+        checkLintWarning(annotation,
                 1, "failtest not in autoload module layout",
-                "failtest.pp", PuppetLintParser.WARNING_TYPE, "autoloader_layout", Priority.HIGH);
+                "failtest.pp", PuppetLintParser.WARNING_TYPE, "autoloader_layout", Priority.HIGH, "-");
 
         annotation = iterator.next();
-        checkWarning(annotation,
+        checkLintWarning(annotation,
                 3, "line has more than 80 characters",
-                "failtest.pp", PuppetLintParser.WARNING_TYPE, "80chars", Priority.NORMAL);
+                "./modules/test/manifests/init.pp", PuppetLintParser.WARNING_TYPE, "80chars", Priority.NORMAL, "::test");
 
         annotation = iterator.next();
-        checkWarning(annotation,
+        checkLintWarning(annotation,
                 10, "line has more than 80 characters",
-                "morefail.pp", PuppetLintParser.WARNING_TYPE, "80chars", Priority.NORMAL);
+                "./modules/test/manifests/sub/class/morefail.pp", PuppetLintParser.WARNING_TYPE, "80chars", Priority.NORMAL, "::test::sub::class");
     }
 
+    /**
+     * Checks the properties of the specified warning.
+     *
+     * @param annotation
+     *            the warning to check
+     * @param lineNumber
+     *            the expected line number
+     * @param message
+     *            the expected message
+     * @param fileName
+     *            the expected filename
+     * @param type
+     *            the expected type
+     * @param category
+     *            the expected category
+     * @param priority
+     *            the expected priority
+     * @param packageName
+     *            the expected package name
+     */
+    protected void checkLintWarning(final FileAnnotation annotation, final int lineNumber, final String message, final String fileName, final String type, final String category, final Priority priority, final String packageName) {
+        checkWarning(annotation, lineNumber, message, fileName, type, category, priority);
+        assertEquals("Wrong packageName detected.", packageName, annotation.getPackageName());
+    }
+    
     /**
      * Creates the parser.
      *
