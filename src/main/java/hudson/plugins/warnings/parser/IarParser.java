8db84b67b2218376c296cb191526d30147132065
Ulli Hafner <ullrich.hafner@gmail.com>
2012-04-23 14:29:28 +0200

    Improved IAR Parser using the specifications from IAR team.

--- src/main/java/hudson/plugins/warnings/parser/IarParser.java
+++ src/main/java/hudson/plugins/warnings/parser/IarParser.java
@@ -6,34 +6,40 @@ import hudson.plugins.analysis.util.model.Priority;
 import java.util.regex.Matcher;
 
 /**
- * A parser for the IAR C/C++ compiler warnings.
+ * A parser for the IAR C/C++ compiler warnings. Note, that since release 4.1
+ * this parser requires that IAR compilers are started with option
+ * '----no_wrap_diagnostics'. Then the IAR compilers will create single-line
+ * warnings.
  *
  * @author Claus Klein
+ * @author Ulli Hafner
  */
 @Extension
-public class IarParser extends RegexpDocumentParser {
+public class IarParser extends RegexpLineParser {
+    private static final String OR = "|";
+    private static final String FATAL_ERROR = "Fatal error";
+    private static final String WARNING = "Warning";
+    private static final String REMARK = "Remark";
+    private static final String ERROR = "Error";
+
     private static final long serialVersionUID = 7695540852439013425L;
 
-    /**
-     * Pattern of IAR compiler warnings.
-     *
-     * @note the warning message my be wrapped over multiple lines!
-     * So we use MULTILINE (?m), DOTALL (?s), and CASE_INSENSITIVE (?i) flags
-     *
-     * The IAR warning pattern is like this:
-     * "filename",linenumber level[tag]: multiline message
-     */
     private static final String IAR_WARNING_PATTERN =
-        "(?s)(?i)\\s*\\^[\\r]??\\n\"(.+?\\.[chpsola0-9]+)\",(\\d+)\\s*(Remark|Warning|Error)\\s*\\[(Pe\\d+)\\]\\s*:\\s*(.+?)([\\r]??\\n){2}?";
+        "^\"(.*?)\",(\\d+)\\s+(" + ERROR + OR + REMARK + OR + WARNING + OR + FATAL_ERROR + ")\\[(\\w+)\\]: (.*)$";
 
     /**
-     * Creates a new instance of <code>IarParser</code>.
+     * Creates a new instance of {@link IarParser}.
      */
     public IarParser() {
         super(Messages._Warnings_iar_ParserName(),
                 Messages._Warnings_iar_LinkName(),
                 Messages._Warnings_iar_TrendName(),
                 IAR_WARNING_PATTERN, true);
+    }
+
+    @Override
+    protected boolean isLineInteresting(final String line) {
+        return line.contains(WARNING) || line.contains("rror") || line.contains(REMARK);
     }
 
     @Override
@@ -45,22 +51,22 @@ public class IarParser extends RegexpDocumentParser {
     protected Warning createWarning(final Matcher matcher) {
         Priority priority;
         String message = normalizeWhitespaceInMessage(matcher.group(5));
-        if ("remark".equalsIgnoreCase(matcher.group(3))) {
+        if (REMARK.equals(matcher.group(3))) {
             priority = Priority.LOW;
         }
-        else if ("warning".equalsIgnoreCase(matcher.group(3))) {
+        else if (WARNING.equals(matcher.group(3))) {
             priority = Priority.NORMAL;
         }
-        else if ("error".equalsIgnoreCase(matcher.group(3))) {
+        else if (ERROR.equals(matcher.group(3))) {
+            priority = Priority.HIGH;
+        }
+        else if (FATAL_ERROR.equals(matcher.group(3))) {
             priority = Priority.HIGH;
         }
         else {
-            priority = Priority.HIGH;
-            String category = "IAR Error";
-            return new Warning(matcher.group(1), 0, getGroup(), category, message, priority);
+            return FALSE_POSITIVE;
         }
-        String category = matcher.group(4);
-        return createWarning(matcher.group(1), getLineNumber(matcher.group(2)), category, message, priority);
+        return createWarning(matcher.group(1), getLineNumber(matcher.group(2)), matcher.group(4), message, priority);
     }
 
     private String normalizeWhitespaceInMessage(final String message) {
--- src/test/java/hudson/plugins/warnings/parser/IarParserTest.java
+++ src/test/java/hudson/plugins/warnings/parser/IarParserTest.java
@@ -13,7 +13,7 @@ import org.junit.Test;
 /**
  * Tests the class {@link IarParser}.
  *
- * @author Claus Klein
+ * @author Ulli Hafner
  */
 public class IarParserTest extends ParserTester {
     private static final String TYPE = new IarParser().getGroup();
@@ -28,62 +28,16 @@ public class IarParserTest extends ParserTester {
     public void testWarningsParser() throws IOException {
         Collection<FileAnnotation> warnings = new IarParser().parse(openFile());
 
-        assertEquals("Wrong number of warnings detected.", 8, warnings.size());
-
         Iterator<FileAnnotation> iterator = warnings.iterator();
-        FileAnnotation annotation = iterator.next();
-        checkWarning(annotation,
-                451,
-                "`void yyunput(int, char*)' defined but not used",
-                "testhist.l",
-                TYPE, "Pe0815", Priority.NORMAL);
-        annotation = iterator.next();
-        checkWarning(annotation,
-                73,
-                "implicit typename is deprecated, please see the documentation for details",
-                "/u1/drjohn/bfdist/packages/RegrTest/V00-03-01/RgtAddressLineScan.cc",
-                TYPE, "Pe0815", Priority.HIGH);
-        annotation = iterator.next();
-        checkWarning(annotation,
-                39,
-                "foo.h: No such file or directory",
-                "foo.cc",
-                TYPE, "Pe0815", Priority.HIGH);
-        annotation = iterator.next();
-        checkWarning(annotation,
-                56,
-                "type qualifier on return type is meaningless",
-                "D:/src/CSpiBus.h",
-                TYPE, "Pe815", Priority.NORMAL);
-        annotation = iterator.next();
-        checkWarning(annotation,
-                38,
-                "identifier \"OS_EnterNestableInterrupt\" is undefined",
-                "D:/src/InterruptTabO7.cpp",
-                TYPE, "Pe020", Priority.HIGH);
-        annotation = iterator.next();
-        checkWarning(annotation,
-                229,
-                "variable \"ret\" was set but never used",
-                "z:/src/O7_LabSample_embOS/EmbosConfig/CANopen/main.cpp",
-                TYPE, "Pe550", Priority.NORMAL);
-        annotation = iterator.next();
-        checkWarning(annotation,
-                525,
-                "variable \"ret\" was set but never used",
-                "z:/src/O7_LabSample_embOS/EmbosConfig/CANopen/main.cpp",
-                TYPE, "Pe550", Priority.NORMAL);
-        annotation = iterator.next();
-        checkWarning(annotation,
-                78,
-                "missing return statement at end of non-void function \"CPwmOutput::setDutyCycle\"",
-                "z:/src/O7_LabSample_embOS/EmbosConfig/CPwmOutput.cpp",
-                TYPE, "Pe940", Priority.NORMAL);
+
+        assertEquals("Wrong number of warnings detected.", 9, warnings.size());
+        checkWarning(iterator.next(), 3, "an inline function cannot be root as well",
+                "/tmp/x/icc-error-memory.c", TYPE, "Be031", Priority.HIGH);
     }
 
     @Override
     protected String getWarningsFile() {
-        return "iar.txt";
+        return "iar-nowrap.log";
     }
 }
 
