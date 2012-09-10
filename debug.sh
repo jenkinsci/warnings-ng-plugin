c86e157eb6024ed3f4c52472a8eca366d79882d7
Ulli Hafner <ullrich.hafner@gmail.com>
2012-09-10 17:52:59 +0200

    Update of build scripts.

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 export MAVEN_OPTS="-Xmx1024m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n"
-rm -rf $HUDSON_HOME/plugins/warnings*
+rm -rf $JENKINS_HOME/plugins/warnings*
 mvn clean hpi:run
--- go.sh
+++ go.sh
@@ -1,8 +1,8 @@
-rm -rf $HUDSON_HOME/plugins/warnings*
+rm -rf $JENKINS_HOME/plugins/warnings*
 
 mvn install || { echo "Build failed"; exit 1; }
  
-cp -f target/*.hpi $HUDSON_HOME/plugins/
+cp -f target/*.hpi $JENKINS_HOME/plugins/
 
-cd $HUDSON_HOME
-./debug.sh
+cd $JENKINS_HOME
+./go.sh
