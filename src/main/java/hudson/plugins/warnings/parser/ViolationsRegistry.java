ea23ff0f6836dbd221c610724d817ff138145cfd
a6min <misho.ashmin@gmail.com>
2013-05-28 16:49:30 +0200

    PerlCriticParser added. Not fully implemented yet.

--- src/main/java/hudson/plugins/warnings/parser/ViolationsRegistry.java
+++ src/main/java/hudson/plugins/warnings/parser/ViolationsRegistry.java
@@ -7,7 +7,6 @@ import hudson.plugins.violations.types.csslint.CssLintParser;
 import hudson.plugins.violations.types.gendarme.GendarmeParser;
 import hudson.plugins.violations.types.jcreport.JcReportParser;
 import hudson.plugins.violations.types.pep8.Pep8Parser;
-import hudson.plugins.violations.types.perlcritic.PerlCriticParser;
 import hudson.plugins.violations.types.stylecop.StyleCopParser;
 
 /**
@@ -44,10 +43,6 @@ public final class ViolationsRegistry {
                 Messages._Warnings_Pep8_ParserName(),
                 Messages._Warnings_Pep8_LinkName(),
                 Messages._Warnings_Pep8_TrendName()));
-        parsers.add(new ViolationsAdapter(new PerlCriticParser(),
-                Messages._Warnings_PerlCritic_ParserName(),
-                Messages._Warnings_PerlCritic_LinkName(),
-                Messages._Warnings_PerlCritic_TrendName()));
         parsers.add(new ViolationsAdapter(new StyleCopParser(),
                 Messages._Warnings_StyleCop_ParserName(),
                 Messages._Warnings_StyleCop_LinkName(),
