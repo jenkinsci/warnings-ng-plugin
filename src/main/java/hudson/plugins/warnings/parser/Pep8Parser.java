154f361c5a650720d363db0d738d1d8005aa8b1c
Marvin Schuetz <marvin-spiky@gmx.net>
2013-05-28 15:38:11 +0200

    Pep8 parser hinzugefügt

---
+++ src/main/java/hudson/plugins/warnings/parser/Pep8Parser.java
@@ -0,0 +1,34 @@
+package hudson.plugins.warnings.parser;
+
+import java.util.regex.Matcher;
+
+/**
+ * FIXME: Document type Pep8Parser.
+ *
+ * @author Ulli Hafner
+ */
+public class Pep8Parser extends RegexpLineParser {
+
+
+
+    private static final long serialVersionUID = -8444940209330966997L;
+    private static final String PEP8_WARNING_PATTERN = "(.*):(\\d+):\\d+: (\\D\\d*) (.*)";
+    /**
+     * Creates a new instance of {@link Pep8Parser}.
+     */
+    public Pep8Parser() {
+        super(Messages._Warnings_Pep8_ParserName(),
+                Messages._Warnings_Pep8_LinkName(),
+                Messages._Warnings_Pep8_TrendName(),
+                PEP8_WARNING_PATTERN, true);
+    }
+    @Override
+    protected Warning createWarning(final Matcher matcher) {
+        String message = matcher.group(4);
+        String category = classifyIfEmpty(matcher.group(1), message);
+
+        return createWarning(matcher.group(3), getLineNumber(matcher.group(2)), category, message);
+    }
+
+}
+
