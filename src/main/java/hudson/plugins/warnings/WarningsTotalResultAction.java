d3f19342dd59f6534bcfeaf3c2e120123b99caa1
Basti <Basti@dhcp-138-246-247-154.dynamic.eduroam.mwn.de>
2013-06-18 16:45:27 +0200

    Checkin before Jenkins TEST

---
+++ src/main/java/hudson/plugins/warnings/WarningsTotalResultAction.java
@@ -0,0 +1,73 @@
+package hudson.plugins.warnings;
+
+import org.jvnet.localizer.Localizable;
+
+import hudson.model.AbstractBuild;
+
+import hudson.plugins.analysis.core.HealthDescriptor;
+import hudson.plugins.analysis.core.AbstractResultAction;
+import hudson.plugins.analysis.core.PluginDescriptor;
+
+/**
+ * FIXME: Document type WarningsTotalResultAction.
+ *
+ * @author Ulli Hafner
+ */
+public class WarningsTotalResultAction extends AbstractResultAction<WarningsResult>{
+
+   /**
+   * Creates a new instance of <code>WarningsResultAction</code>.
+   *
+   * @param owner
+   *            the associated build of this action
+   * @param healthDescriptor
+   *            health descriptor to use
+   * @param result
+   *            the result in this build
+   * @param parserName the name of the parser
+   */
+  public WarningsTotalResultAction(final AbstractBuild<?, ?> owner, final HealthDescriptor healthDescriptor, final WarningsResult result) {
+      super(owner, new WarningsHealthDescriptor(healthDescriptor), result);
+  }
+
+//  @Override
+//  public String getUrlName() {
+//      return WarningsDescriptor.getResultUrl(parserName);
+//  }
+//
+//  /**
+//   * Returns the parser group this result belongs to.
+//   *
+//   * @return the parser group
+//   */
+//  public String getParser() {
+//      return parserName;
+//  }
+//
+//  @Override
+//  protected String getSmallImage() {
+//      return ParserRegistry.getParser(parserName).getSmallImage();
+//  }
+//
+//  @Override
+//  public String getLargeImageName() {
+//      return ParserRegistry.getParser(parserName).getLargeImage();
+//  }
+
+  /** {@inheritDoc} */
+  public String getDisplayName() {
+      return "All parser warnings";
+  }
+
+  @Override
+  protected PluginDescriptor getDescriptor() {
+      return new WarningsDescriptor();
+  }
+
+  @SuppressWarnings("PMD")
+  @edu.umd.cs.findbugs.annotations.SuppressWarnings("")
+  private transient Localizable actionName;
+
+
+}
+
