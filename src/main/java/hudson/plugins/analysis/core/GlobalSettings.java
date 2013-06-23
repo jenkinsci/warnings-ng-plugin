eaaa4b6f8dc64e8cb972acc97902fd23aacac2c3
Ulli Hafner <ullrich.hafner@gmail.com>
2013-06-23 17:25:06 +0200

    Added global configuration for all static code analysis plug-ins.

---
+++ src/main/java/hudson/plugins/analysis/core/GlobalSettings.java
@@ -0,0 +1,79 @@
+package hudson.plugins.analysis.core;
+
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.StaplerRequest;
+
+import hudson.Extension;
+
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import hudson.model.Run;
+import hudson.model.listeners.RunListener;
+
+/**
+ * Global settings common to all static analysis plug-ins. The actual extension point {@link RunListener} is not used
+ * yet, this object is only used to provide a model for the view global.jelly.
+ *
+ * @author Ulli Hafner
+ */
+@Extension
+public class GlobalSettings extends RunListener<Run<?, ?>> implements Describable<GlobalSettings> {
+    /** {@inheritDoc} */
+    @SuppressWarnings("unchecked")
+    public Descriptor<GlobalSettings> getDescriptor() {
+        return Jenkins.getInstance().getDescriptorOrDie(getClass());
+    }
+
+    /**
+     * Descriptor for {@link GlobalSettings}.
+     *
+     * @author Ulli Hafner
+     */
+    @Extension
+    public static class DescriptorImpl extends Descriptor<GlobalSettings> {
+        private Boolean isExample;
+
+        @Override
+        public String getDisplayName() {
+            return StringUtils.EMPTY;
+        }
+
+        /**
+         * Creates a new instance of {@link GlobalSettings.DescriptorImpl}.
+         */
+        public DescriptorImpl() {
+            super();
+
+            load();
+        }
+
+        @Override
+        public boolean configure(final StaplerRequest req, final JSONObject json) throws FormException {
+            req.bindJSON(this, json);
+            save();
+
+            return true;
+        }
+
+        /**
+         * Returns the value of the example boolean property.
+         *
+         * @return the status of the example boolean property
+         */
+        public Boolean getExample() {
+            return isExample;
+        }
+
+        /**
+         * Sets the value of the example boolean property.
+         *
+         * @param value the value to set
+         */
+        public void setExample(final Boolean value) {
+            isExample = value;
+        }
+    }
+}
