865525146872b16cfce4728a2076ab921e31519a
Basti <Basti@dhcp-138-246-247-154.dynamic.eduroam.mwn.de>
2013-07-01 11:01:03 +0200

    Test File Methoden hinzugefügt

--- src/main/java/hudson/plugins/warnings/WarningsTotalResult.java
+++ src/main/java/hudson/plugins/warnings/WarningsTotalResult.java
@@ -1,5 +1,7 @@
 package hudson.plugins.warnings;
 
+import java.io.File;
+
 import com.thoughtworks.xstream.XStream;
 
 import hudson.model.AbstractBuild;
@@ -72,6 +74,48 @@ public class WarningsTotalResult extends BuildResult{
     @Override
     protected Class<? extends ResultAction<? extends BuildResult>> getResultActionType() {
         return WarningsTotalResultAction.class;
+    }
+
+    //übernommen aus WarningsResult
+
+    private static final String FILENAME_SUFFIX = "-totalWarnings.xml";
+    static final String ORIGINAL_COMPILER_WARNINGS_XML = "totalCompiler-warnings.xml";
+
+
+    String getFileName(final FileChecker fileChecker, final int groupUrl) {
+        String fileName = ORIGINAL_COMPILER_WARNINGS_XML;
+        if (fileChecker.canRead(fileName)) {
+            return fileName;
+        }
+
+        fileName = createFileName(groupUrl);
+        if (fileChecker.canRead(fileName)) {
+            return fileName;
+        }
+
+        //return group.replaceAll("\\W+", "") + FILENAME_SUFFIX;
+        return "totalWarningsGrp";
+    }
+
+    String createFileName(final int groupUrl) {
+        return "compiler-" + groupUrl + FILENAME_SUFFIX;
+    }
+
+    /**
+     * Provides a way to hide file system access during testing.
+     *
+     * @author Ulli Hafner
+     */
+    static class FileChecker {
+        private final File root;
+
+        FileChecker(final File root) {
+            this.root = root;
+        }
+
+        boolean canRead(final String fileName) {
+            return new File(root, fileName).canRead();
+        }
     }
 }
 
