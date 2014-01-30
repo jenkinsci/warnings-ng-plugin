9c1db98e801ff2b0eb762b859ab7f5d13b6bd6c1
Ulli Hafner <ullrich.hafner@gmail.com>
2014-01-30 21:59:09 +0100

    [ FIXED JENKINS-20070] Clear SAX system property afterwards.

---
+++ src/main/java/hudson/plugins/analysis/util/SaxSetup.java
@@ -0,0 +1,29 @@
+package hudson.plugins.analysis.util;
+
+import org.apache.xerces.parsers.SAXParser;
+
+/**
+ * Registers the correct SAX driver if the environment variable is set.
+ */
+public class SaxSetup {
+    /** Property of SAX parser factory. */
+    private static final String SAX_DRIVER_PROPERTY = "org.xml.sax.driver";
+
+    private String oldProperty;
+
+    public SaxSetup() {
+        oldProperty = System.getProperty(SAX_DRIVER_PROPERTY);
+        if (oldProperty != null) {
+            System.setProperty(SAX_DRIVER_PROPERTY, SAXParser.class.getName());
+        }
+    }
+
+    public void cleanup() {
+        if (oldProperty != null) {
+            System.setProperty(SAX_DRIVER_PROPERTY, oldProperty);
+        }
+        else {
+            System.clearProperty(SAX_DRIVER_PROPERTY);
+        }
+    }
+}
