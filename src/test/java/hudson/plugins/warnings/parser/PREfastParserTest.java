3546ade66cda0186fb648494072c5535aa1ca51e
charleswhchan <charleswhchan@github.com>
2014-03-05 21:20:22 -0800

    CC - Initial copy, template based on PuppetLintParserTest.java

---
+++ src/test/java/hudson/plugins/warnings/parser/PREfastParserTest.java
@@ -0,0 +1,89 @@
+package hudson.plugins.warnings.parser;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.junit.Test;
+
+import hudson.plugins.analysis.util.model.FileAnnotation;
+import hudson.plugins.analysis.util.model.Priority;
+
+/**
+ * Tests the class {@link PuppetLintParser}.
+ *
+ * @author Jan Vansteenkiste <jan@vstone.eu>
+ */
+public class PREfastParserTest extends ParserTester {
+    private static final String TYPE = new PuppetLintParser().getGroup();
+
+    /**
+     * Tests the Puppet-Lint parsing.
+     *
+     * @throws IOException
+     *             in case of an error
+     */
+    @Test
+    public void testParse() throws IOException {
+        Collection<FileAnnotation> results = createParser().parse(openFile());
+        Iterator<FileAnnotation> iterator = results.iterator();
+
+        FileAnnotation annotation = iterator.next();
+        checkLintWarning(annotation,
+                1, "failtest not in autoload module layout",
+                "failtest.pp", TYPE, "autoloader_layout", Priority.HIGH, "-");
+
+        annotation = iterator.next();
+        checkLintWarning(annotation,
+                3, "line has more than 80 characters",
+                "./modules/test/manifests/init.pp", TYPE, "80chars", Priority.NORMAL, "::test");
+
+        annotation = iterator.next();
+        checkLintWarning(annotation,
+                10, "line has more than 80 characters",
+                "./modules/test/manifests/sub/class/morefail.pp", TYPE, "80chars", Priority.NORMAL, "::test::sub::class");
+    }
+
+    /**
+     * Checks the properties of the specified warning.
+     *
+     * @param annotation
+     *            the warning to check
+     * @param lineNumber
+     *            the expected line number
+     * @param message
+     *            the expected message
+     * @param fileName
+     *            the expected filename
+     * @param type
+     *            the expected type
+     * @param category
+     *            the expected category
+     * @param priority
+     *            the expected priority
+     * @param packageName
+     *            the expected package name
+     */
+    // CHECKSTYLE:OFF
+    private void checkLintWarning(final FileAnnotation annotation, final int lineNumber, final String message, final String fileName, final String type, final String category, final Priority priority, final String packageName) {
+        checkWarning(annotation, lineNumber, message, fileName, type, category, priority);
+        assertEquals("Wrong packageName detected.", packageName, annotation.getPackageName());
+    }
+    // CHECKSTYLE:ON
+
+    /**
+     * Creates the parser.
+     *
+     * @return the warnings parser
+     */
+    protected AbstractWarningsParser createParser() {
+        return new PuppetLintParser();
+    }
+
+    @Override
+    protected String getWarningsFile() {
+        return "puppet-lint.txt";
+    }
+}
