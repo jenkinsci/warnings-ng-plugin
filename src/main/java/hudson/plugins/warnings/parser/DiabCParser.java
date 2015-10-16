8ce6be265a14532b8fe3c91c7ee724adf7255262
Ben <benjamin.dumazel@gmail.com>
2015-10-16 09:41:01 -0500

    Handle warnings from the Diab C++ compiler (etoa). Try to handle the C++ Lint info generated as well (info).

--- src/main/java/hudson/plugins/warnings/parser/DiabCParser.java
+++ src/main/java/hudson/plugins/warnings/parser/DiabCParser.java
@@ -15,7 +15,7 @@ import hudson.plugins.analysis.util.model.Priority;
 public class DiabCParser extends RegexpLineParser {
     private static final long serialVersionUID = -1251248150596418456L;
 
-    private static final String DIAB_CPP_WARNING_PATTERN = "^\\s*\"(.*)\"\\s*,\\s*line\\s*(\\d+)\\s*:\\s*(warning|error|fatal\\serror)\\s*\\(dcc:(\\d+)\\)\\s*:\\s*(.*)$";
+    private static final String DIAB_CPP_WARNING_PATTERN = "^\\s*\"(.*)\"\\s*,\\s*line\\s*(\\d+)\\s*:\\s*(info|warning|error|fatal\\serror)\\s*\\((?:dcc|etoa):(\\d+)\\)\\s*:\\s*(.*)$";
 
     /**
      * Creates a new instance of <code>HpiCompileParser</code>.
@@ -30,7 +30,7 @@ public class DiabCParser extends RegexpLineParser {
     @Override
     protected Warning createWarning(final Matcher matcher) {
         Priority priority;
-        if ("warning".equalsIgnoreCase(matcher.group(3))) {
+        if (("warning".equalsIgnoreCase(matcher.group(3))) || ("info".equalsIgnoreCase(matcher.group(3)))) {
             priority = Priority.NORMAL;
         }
         else {
