17e6fa8346c6851ba810456b23225b864c32b386
Traitanit Huangsri <traitanit.hua@ascendcorp.com>
2017-04-02 19:46:33 +0700

    Support default rflint output file format

--- src/main/java/hudson/plugins/warnings/parser/RFLintParser.java
+++ src/main/java/hudson/plugins/warnings/parser/RFLintParser.java
@@ -4,14 +4,22 @@ import hudson.Extension;
 import hudson.plugins.analysis.util.model.Priority;
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
+ * A parser for <a href="http://robotframework.org/">Robot Framework</a>
+ * Parse output from <a href="https://github.com/boakley/robotframework-lint">robotframework-lint</a>
+ * To generate rflint file
+ * cmd$ pip install robotframework-lint
+ * cmd$ rflint path/to/test.robot
  * Created by traitanit on 3/27/2017 AD.
  */
 @Extension
 public class RFLintParser extends RegexpLineParser {
 
-    private static final String RFLINT_ERROR_PATTERN = "(.*): ([W|E|I]): (\\d+), (\\d+): (.*) \\((.*)\\)";
+    private static final String RFLINT_ERROR_PATTERN = "([W|E|I]): (\\d+), (\\d+): (.*) \\((.*)\\)";
+    private static final String RFLINT_FILE_PATTERN = "\\+\\s(.*)";
+    private String fileName;
 
     public RFLintParser(){
         super(Messages._Warnings_RFLint_ParserName(),
@@ -22,13 +30,20 @@ public class RFLintParser extends RegexpLineParser {
 
     @Override
     protected boolean isLineInteresting(String line) {
-        return line.contains("W") || line.contains("E") || line.contains("I");
+        Pattern filePattern = Pattern.compile(RFLINT_FILE_PATTERN);
+        Matcher matcher = filePattern.matcher(line);
+        if (matcher.find()) {
+            fileName = matcher.group(1);
+            return false;
+        }
+        return Pattern.matches(RFLINT_ERROR_PATTERN, line);
     }
+
 
     @Override
     protected Warning createWarning(Matcher matcher) {
-        String message = matcher.group(5);
-        String category = classifyIfEmpty(matcher.group(2), message);
+        String message = matcher.group(4);
+        String category = classifyIfEmpty(matcher.group(1), message);
         Priority priority = Priority.LOW;
         switch (category.charAt(0)){
             case 'E':
@@ -43,7 +58,9 @@ public class RFLintParser extends RegexpLineParser {
                 priority = Priority.LOW;
                 category = "IGNORE";
                 break;
+            default:
+                break;
         }
-        return createWarning(matcher.group(1), getLineNumber(matcher.group(3)), category, message, priority);
+        return createWarning(fileName, getLineNumber(matcher.group(2)), category, message, priority);
     }
 }
