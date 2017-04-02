7872406354894f4230f0be913ac5c76882709365
Ulli Hafner <ullrich.hafner@gmail.com>
2017-04-02 23:43:54 +0200

    Fixed missing import.

--- src/main/java/hudson/plugins/warnings/WarningsDescriptor.java
+++ src/main/java/hudson/plugins/warnings/WarningsDescriptor.java
@@ -3,6 +3,7 @@ package hudson.plugins.warnings;
 import java.io.IOException;
 import java.util.Collection;
 
+import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.Ancestor;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.QueryParameter;
