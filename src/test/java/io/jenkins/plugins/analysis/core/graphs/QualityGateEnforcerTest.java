3e8dc15ec8f0c217e78bbd02906eec45b3303c8b
Ulli Hafner <ullrich.hafner@gmail.com>
2017-10-03 14:35:43 +0200

    Refactored naming of analysis run values.

---
+++ src/test/java/io/jenkins/plugins/analysis/core/graphs/QualityGateEnforcerTest.java
@@ -0,0 +1,53 @@
+package io.jenkins.plugins.analysis.core.graphs;
+
+import org.junit.jupiter.api.Test;
+
+import io.jenkins.plugins.analysis.core.QualityGate;
+import io.jenkins.plugins.analysis.core.QualityGateEnforcer;
+import io.jenkins.plugins.analysis.core.StaticAnalysisRun;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+import hudson.model.Result;
+
+/**
+ * Tests the class {@link QualityGateEnforcer}.
+ *
+ * @author Ullrich Hafner
+ */
+class QualityGateEnforcerTest {
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndNoQualityGateIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGate();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success).as("No issues and no quality gate should always be a SUCCESS").isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndFailureQualityGateIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGate();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success).as("No issues and failure quality gate should always be a SUCCESS").isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldFailBuildIfFailureThresholdIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalHighPrioritySize()).thenReturn(1);
+
+        QualityGate qualityGate = new QualityGate(1);
+
+        Result failure = enforcer.evaluate(run, qualityGate);
+
+        assertThat(failure).as("One issue should return FAILURE").isEqualTo(Result.FAILURE);
+    }
+}
