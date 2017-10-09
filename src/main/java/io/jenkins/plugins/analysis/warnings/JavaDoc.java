afc47af38d51886627334a8974b5b1ee84f76887
Ulli Hafner <ullrich.hafner@gmail.com>
2017-10-09 23:30:16 +0200

    Improved logging and enabled scanning of console parsing.

--- src/main/java/io/jenkins/plugins/analysis/warnings/JavaDoc.java
+++ src/main/java/io/jenkins/plugins/analysis/warnings/JavaDoc.java
@@ -7,38 +7,27 @@ import java.util.List;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 
-import io.jenkins.plugins.analysis.core.steps.StaticAnalysisTool;
-
 import hudson.Extension;
 import hudson.plugins.analysis.util.model.FileAnnotation;
-import hudson.plugins.warnings.WarningsDescriptor;
 import hudson.plugins.warnings.parser.AbstractWarningsParser;
 import hudson.plugins.warnings.parser.FileWarningsParser;
 import hudson.plugins.warnings.parser.Messages;
 import hudson.plugins.warnings.parser.ParserRegistry;
 
 /**
- * Provides customized messages for the Java parser.
+ * Provides customized messages for the JavaDoc parser.
  *
  * @author Ullrich Hafner
  */
-@Extension
-public class Java extends StaticAnalysisTool {
-    private static final String JAVA_SMALL_ICON = WarningsDescriptor.IMAGE_PREFIX + "java-24x24.png";
-    private static final String JAVA_LARGE_ICON = WarningsDescriptor.IMAGE_PREFIX + "java-48x48.png";
-
+public class JavaDoc extends Java {
     @DataBoundConstructor
-    public Java() {
-        this("java");
-    }
-
-    protected Java(final String id) {
-        super(id);
+    public JavaDoc() {
+        super("javadoc");
     }
 
     @Override
     public Collection<FileAnnotation> parse(final File file, final String moduleName) throws InvocationTargetException {
-        List<AbstractWarningsParser> parsers = ParserRegistry.getParsers("Java Compiler");
+        List<AbstractWarningsParser> parsers = ParserRegistry.getParsers("JavaDoc");
 
         return new FileWarningsParser(parsers, getDefaultEncoding()).parse(file, moduleName);
     }
@@ -50,24 +39,14 @@ public class Java extends StaticAnalysisTool {
 
     @Override
     public String getLinkName() {
-        return Messages.Warnings_JavaParser_LinkName();
+        return Messages.Warnings_JavaDoc_LinkName();
     }
 
     @Override
     public String getTrendName() {
-        return Messages.Warnings_JavaParser_TrendName();
-    }
-
-    @Override
-    public String getSmallIconUrl() {
-        return JAVA_SMALL_ICON;
-    }
-
-    @Override
-    public String getLargeIconUrl() {
-        return JAVA_LARGE_ICON;
+        return Messages.Warnings_JavaDoc_TrendName();
     }
 
     @Extension
-    public static final StaticAnalysisToolDescriptor D = new StaticAnalysisToolDescriptor(Java.class);
+    public static final StaticAnalysisToolDescriptor D = new StaticAnalysisToolDescriptor(JavaDoc.class);
 }
