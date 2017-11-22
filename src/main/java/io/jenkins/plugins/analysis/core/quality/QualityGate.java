67d345deb000f5a2cf8319bf238881a46f1e7cb6
Ulli Hafner <ullrich.hafner@gmail.com>
2017-11-22 23:29:53 +0100

    Added JavaDoc for the TDD example. Fixed wrong property in test.

--- src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGate.java
+++ src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGate.java
@@ -8,18 +8,37 @@ package io.jenkins.plugins.analysis.core.quality;
 public class QualityGate {
     private final int failureThreshold;
 
+    /**
+     * Creates a new instance of {@link QualityGate}.
+     *
+     * @param failureThreshold
+     *         the number of issues that will fail a build
+     */
     public QualityGate(final int failureThreshold) {
         this.failureThreshold = failureThreshold;
     }
 
+    /**
+     * Creates a new instance of {@link QualityGate}. No thresholds are set.
+     */
     public QualityGate() {
         this(0);
     }
 
+    /**
+     * Determines if a failure threshold for the total number of issues is set.
+     *
+     * @return {@code true} if the failure threshold for the total number of issues is set
+     */
     public boolean hasFailureThreshold() {
         return failureThreshold > 0;
     }
 
+    /**
+     * Returns the failure threshold for the total number of issues.
+     *
+     * @return the failure threshold for the total number of issues
+     */
     public int getFailureThreshold() {
         return failureThreshold;
     }
