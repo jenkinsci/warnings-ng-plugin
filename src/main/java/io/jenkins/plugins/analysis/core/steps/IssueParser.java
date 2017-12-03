91616cdc1caa893897f80aa218132ead0cd12aa1
Ulli Hafner <ullrich.hafner@gmail.com>
2017-12-04 00:52:07 +0100

    Improved parser API: added charset and correct exceptions.

--- src/main/java/io/jenkins/plugins/analysis/core/steps/IssueParser.java
+++ src/main/java/io/jenkins/plugins/analysis/core/steps/IssueParser.java
@@ -2,33 +2,38 @@ package io.jenkins.plugins.analysis.core.steps;
 
 import java.io.File;
 import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.Charset;
 
 import edu.hm.hafner.analysis.Issue;
 import edu.hm.hafner.analysis.IssueBuilder;
 import edu.hm.hafner.analysis.Issues;
+import edu.hm.hafner.analysis.ParsingCanceledException;
+import edu.hm.hafner.analysis.ParsingException;
 
 /**
  * Parses a file and returns the issues reported in this file.
  *
  * @author Ulli Hafner
  */
-// FIXME: encoding?
 public interface IssueParser extends Serializable {
     /**
      * Returns the issues found in the specified file.
      *
      * @param file
      *         the file to parse
+     * @param charset
+     *         the encoding to use when reading files
      * @param builder
      *         the issue builder to use
      *
      * @return the issues found
-     * @throws InvocationTargetException
-     *         if the file could not be parsed (wrap your exception in this exception)
+     * @throws ParsingException
+     *         Signals that during parsing a non recoverable error has been occurred
+     * @throws ParsingCanceledException
+     *         Signals that the parsing has been aborted by the user
      */
-    // FIXME: why dont use ParserException?
-    Issues<Issue> parse(File file, IssueBuilder builder) throws InvocationTargetException;
+    Issues<Issue> parse(File file, Charset charset, IssueBuilder builder)
+            throws ParsingException, ParsingCanceledException;
 
     /**
      * Returns the ID of this parser. Issues that are reported by this parser will set property {@link
