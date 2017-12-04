5eb69af82d117e135ae8035b4e54ddd96c530734
Ulli Hafner <ullrich.hafner@gmail.com>
2017-12-04 01:06:35 +0100

    Moved Eclipse Jenkins Console Test to warnings plugin.

--- src/test/java/io/jenkins/plugins/analysis/warnings/EclipseTest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/EclipseTest.java
@@ -1,17 +1,47 @@
 package io.jenkins.plugins.analysis.warnings;
 
-import org.junit.jupiter.api.Test;
+import edu.hm.hafner.analysis.AbstractParser;
+import edu.hm.hafner.analysis.AbstractParserTest;
+import edu.hm.hafner.analysis.Issue;
+import edu.hm.hafner.analysis.Issues;
+import static edu.hm.hafner.analysis.assertj.Assertions.*;
+import edu.hm.hafner.analysis.assertj.SoftAssertions;
+import edu.hm.hafner.analysis.parser.EclipseParser;
+
+import hudson.console.ConsoleNote;
 
 /**
  * Tests the class {@link Eclipse}.
  *
  * @author Ullrich Hafner
  */
-class EclipseTest {
+class EclipseTest extends AbstractParserTest {
+    public EclipseTest() {
+        super("issue11675.txt");
+    }
+
+    private void assertThatMessageContainsWord(final Issue annotation) {
+        assertThat(annotation.getMessage()).matches("[a-zA-Z].*");
+    }
+
     /**
-     * FIXME: write comment.
+     * Parses a warning log with console annotations which are removed.
+     *
+     * @see <a href="http://issues.jenkins-ci.org/browse/JENKINS-11675">Issue 11675</a>
      */
-    @Test
-    void should() {
+    @Override
+    protected void assertThatIssuesArePresent(final Issues<Issue> issues, final SoftAssertions softly) {
+        assertThat(issues).hasSize(8);
+
+        for (Issue annotation : issues) {
+            assertThatMessageContainsWord(annotation);
+        }
+    }
+
+    @Override
+    protected AbstractParser createParser() {
+        EclipseParser parser = new EclipseParser();
+        parser.setTransformer(line -> ConsoleNote.removeNotes(line));
+        return parser;
     }
 }
