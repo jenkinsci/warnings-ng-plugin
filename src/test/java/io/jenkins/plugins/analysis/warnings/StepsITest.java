f0a4a227fd86e7ed7cead86bf59f4323e2149e8d
Ulli Hafner <ullrich.hafner@gmail.com>
2017-12-12 23:33:17 +0100

    Added some integration tests.

--- src/test/java/io/jenkins/plugins/analysis/warnings/StepsITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/StepsITest.java
@@ -7,6 +7,7 @@ import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.Tag;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import static edu.hm.hafner.analysis.assertj.Assertions.*;
@@ -14,6 +15,7 @@ import io.jenkins.plugins.analysis.core.steps.AnalysisResult;
 import io.jenkins.plugins.analysis.core.steps.PublishIssuesStep;
 import io.jenkins.plugins.analysis.core.steps.ResultAction;
 import io.jenkins.plugins.analysis.core.steps.ScanForIssuesStep;
+import io.jenkins.plugins.analysis.core.steps.StaticAnalysisTool;
 
 import hudson.FilePath;
 import hudson.model.Result;
@@ -25,39 +27,24 @@ import hudson.model.Result;
  * @see ScanForIssuesStep
  * @see PublishIssuesStep
  */
+@Tag("IntegrationTest")
 @SuppressWarnings({"OverlyBroadThrowsClause", "ProhibitedExceptionDeclared"})
 public class StepsITest {
+    private static final String PUBLISH_ISSUES_STEP = "publishIssues issues:[issues]";
     /** Starts Jenkins and provides several useful helper methods. */
     @Rule
     public final JenkinsRule j = new JenkinsRule();
 
     /**
-     * A simple test that runs the Java parser on the empty console log: the build  should report 0 issues.
+     * Runs the Eclipse parser on an output file that contains several issues: the build should report 8 issues.
      *
      * @throws Exception
      *         in case of an error
      */
     @Test
-    public void shouldFindNoIssuesIfNoFileGiven() throws Exception {
-        WorkflowJob job = createJob();
-        job.setDefinition(asStage("def java = scanForIssues tool: [$class: 'Java']",
-                "publishIssues issues:[java], useStableBuildAsReference: true"));
-
-        AnalysisResult result = scheduleBuild(job);
-        assertThat(result.getTotalSize()).isEqualTo(0);
-    }
-
-    /**
-     * Runs the Eclipse parser on a file that contains several issues: the build should report 8 issues.
-     *
-     * @throws Exception
-     *         in case of an error
-     */
-    @Test
-    public void shouldFindAllEclipseWarnings() throws Exception {
-        WorkflowJob job = createJob("eclipse.txt");
-        job.setDefinition(asStage("def issues = scanForIssues tool: [$class: 'Eclipse'], pattern: '**/warnings.txt'",
-                "publishIssues issues:[issues]"));
+    public void shouldFindAllEclipseIssues() throws Exception {
+        WorkflowJob job = createJobWithWorkspaceFile("eclipse.txt");
+        job.setDefinition(parseAndPublish(Eclipse.class));
 
         AnalysisResult result = scheduleBuild(job);
 
@@ -65,16 +52,55 @@ public class StepsITest {
         assertThat(result.getIssues()).hasSize(8);
     }
 
-    private WorkflowJob createJob(final String fileName) throws IOException, InterruptedException {
+    /**
+     * Runs the JavaC parser on an output file of the Eclipse compiler: the build should report no issues.
+     *
+     * @throws Exception
+     *         in case of an error
+     */
+    @Test
+    public void shouldNoJavacIssuesInEclipseOutput() throws Exception {
+        WorkflowJob job = createJobWithWorkspaceFile("eclipse.txt");
+        job.setDefinition(parseAndPublish(Java.class));
+
+        AnalysisResult result = scheduleBuild(job);
+
+        assertThat(result.getTotalSize()).isEqualTo(0);
+    }
+
+    /**
+     * Runs the all Java parsers on three output files: the build should report issues of all tools.
+     *
+     * @throws Exception
+     *         in case of an error
+     */
+    @Test
+    public void shouldCombineIssuesOfSeveralFiles() throws Exception {
+        WorkflowJob job = createJobWithWorkspaceFile("eclipse.txt");
+        job.setDefinition(parseAndPublish(Java.class));
+
+        AnalysisResult result = scheduleBuild(job);
+
+        assertThat(result.getTotalSize()).isEqualTo(0);
+    }
+
+    private CpsFlowDefinition parseAndPublish(final Class<? extends StaticAnalysisTool> parserClass) {
+        return asStage(createScanForIssuesStep(parserClass), PUBLISH_ISSUES_STEP);
+    }
+
+    private String createScanForIssuesStep(final Class<? extends StaticAnalysisTool> parserClass) {
+        return String.format("def issues = scanForIssues tool: [$class: '%s'], pattern:'**/issues.txt'", parserClass.getSimpleName());
+    }
+
+    private WorkflowJob createJobWithWorkspaceFile(final String fileName) throws IOException, InterruptedException {
         WorkflowJob job = createJob();
         copyFileToWorkspace(fileName, job);
         return job;
     }
 
-    private void copyFileToWorkspace(final String fileName, final WorkflowJob job)
-            throws IOException, InterruptedException {
+    private void copyFileToWorkspace(final String fileName, final WorkflowJob job) throws IOException, InterruptedException {
         FilePath workspace = j.jenkins.getWorkspaceFor(job);
-        workspace.child("warnings.txt").copyFrom(getClass().getResourceAsStream(fileName));
+        workspace.child("issues.txt").copyFrom(getClass().getResourceAsStream(fileName));
     }
 
     private WorkflowJob createJob() throws IOException {
@@ -110,6 +136,10 @@ public class StepsITest {
         script.append("  }\n");
         script.append("}\n");
 
+
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println(script);
+        System.out.println("----------------------------------------------------------------------");
         return new CpsFlowDefinition(script.toString(), true);
     }
 }
