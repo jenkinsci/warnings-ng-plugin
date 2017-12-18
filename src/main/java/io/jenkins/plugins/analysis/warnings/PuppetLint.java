5dab202218216b5d4c5482d6b0e8082c03b1fba8
Johannes <johannesbruehl@googlemail.com>
2017-12-18 14:19:29 +0100

    Added Integration Tests for four Parsers

---
+++ src/main/java/io/jenkins/plugins/analysis/warnings/PuppetLint.java
@@ -0,0 +1,44 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import edu.hm.hafner.analysis.*;
+import edu.hm.hafner.analysis.parser.PuppetLintParser;
+import hudson.Extension;
+import io.jenkins.plugins.analysis.core.steps.DefaultLabelProvider;
+import io.jenkins.plugins.analysis.core.steps.StaticAnalysisTool;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.File;
+import java.nio.charset.Charset;
+
+public class PuppetLint extends StaticAnalysisTool {
+    private static final String PARSER_NAME = Messages.Warnings_Puppet_ParserName();
+
+    @DataBoundConstructor
+    public PuppetLint() {
+        // empty constructor required for stapler
+    }
+
+    /**
+     * Registers this tool as extension point implementation.
+     */
+    @Extension
+    public static class Descriptor extends StaticAnalysisToolDescriptor {
+        public Descriptor() {
+            super(new LabelProvider());
+        }
+    }
+
+    /**
+     * Provides the labels for the parser.
+     */
+    private static class LabelProvider extends DefaultLabelProvider {
+        private LabelProvider() {
+            super("puppetlint", PARSER_NAME);
+        }
+    }
+
+    @Override
+    public Issues<Issue> parse(File file, Charset charset, IssueBuilder builder) throws ParsingException, ParsingCanceledException {
+        return new PuppetLintParser().parse(file, charset, builder);
+    }
+}
