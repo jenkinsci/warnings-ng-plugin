32fc0f8a5cb3d2cb46120285d2e1c029e3bb67f9
Ulli Hafner <ullrich.hafner@gmail.com>
2017-12-18 22:34:40 +0100

    Use Quality Gate to set the result of an analysis run.

--- src/main/java/io/jenkins/plugins/analysis/core/quality/ThresholdSet.java
+++ src/main/java/io/jenkins/plugins/analysis/core/quality/ThresholdSet.java
@@ -1,18 +1,22 @@
 package io.jenkins.plugins.analysis.core.quality;
 
+import java.io.Serializable;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Stores a set of four thresholds and checks if one or more of them are reached.
- * This class is test driven developed.
  *
  * @author Michael Schmid
  */
-public class ThresholdSet {
+public class ThresholdSet implements Serializable {
     private final int totalThreshold;
     private final int highThreshold;
     private final int normalThreshold;
     private final int lowThreshold;
 
-    public ThresholdSet(final int totalThreshold, final int highThreshold, final int normalThreshold, final int lowThreshold) {
+    public ThresholdSet(final int totalThreshold, final int highThreshold, final int normalThreshold,
+            final int lowThreshold) {
         this.totalThreshold = totalThreshold;
         this.highThreshold = highThreshold;
         this.normalThreshold = normalThreshold;
@@ -62,11 +66,7 @@ public class ThresholdSet {
      * @return true if reached or exceeded, else false
      */
     private boolean isSingleThresholdReached(final int threshold, final int toCheck) {
-        boolean result = false;
-        if (threshold > 0 && toCheck >= threshold) {
-            result = true;
-        }
-        return result;
+        return threshold > 0 && toCheck >= threshold;
     }
 
     /**
@@ -83,14 +83,47 @@ public class ThresholdSet {
      *
      * @return true if one or more of the counts retched or exceeded the threshold, else false
      */
-    public boolean isThresholdReached(final int totalToCheck, final int highToCheck, final int normalToCheck, final int lowToCheck) {
+    public boolean isThresholdReached(final int totalToCheck, final int highToCheck, final int normalToCheck,
+            final int lowToCheck) {
         return isTotalThresholdReached(totalToCheck)
                 || isHighThresholdReached(highToCheck)
                 || isNormalThresholdReached(normalToCheck)
                 || isLowThresholdReached(lowToCheck);
     }
 
-    static class ThresholdSetBuilder {
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ThresholdSet that = (ThresholdSet) o;
+
+        if (totalThreshold != that.totalThreshold) {
+            return false;
+        }
+        if (highThreshold != that.highThreshold) {
+            return false;
+        }
+        if (normalThreshold != that.normalThreshold) {
+            return false;
+        }
+        return lowThreshold == that.lowThreshold;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = totalThreshold;
+        result = 31 * result + highThreshold;
+        result = 31 * result + normalThreshold;
+        result = 31 * result + lowThreshold;
+        return result;
+    }
+
+    public static class ThresholdSetBuilder {
         private int totalThreshold = 0;
         private int highThreshold = 0;
         private int normalThreshold = 0;
@@ -100,24 +133,131 @@ public class ThresholdSet {
             return new ThresholdSet(getTotalThreshold(), getHighThreshold(), getNormalThreshold(), getLowThreshold());
         }
 
+        /**
+         * Sets the threshold for the total size of issues.
+         *
+         * @param totalThreshold
+         *         the number of overall issues
+         *
+         * @return this
+         */
         public ThresholdSetBuilder setTotalThreshold(final int totalThreshold) {
             this.totalThreshold = totalThreshold;
             return this;
         }
 
+        /**
+         * Sets the threshold for the total size of issues.
+         *
+         * @param totalThreshold
+         *         the number of overall issues
+         *
+         * @return this
+         */
+        public ThresholdSetBuilder setTotalThreshold(final String totalThreshold) {
+            return setTotalThreshold(asInt(totalThreshold));
+        }
+
+        /**
+         * Sets the threshold for the size of high priority issues.
+         *
+         * @param highThreshold
+         *         the number of high priority issues
+         *
+         * @return this
+         */
         public ThresholdSetBuilder setHighThreshold(final int highThreshold) {
             this.highThreshold = highThreshold;
             return this;
         }
 
+        /**
+         * Sets the threshold for the size of high priority issues.
+         *
+         * @param highThreshold
+         *         the number of high priority issues
+         *
+         * @return this
+         */
+        public ThresholdSetBuilder setHighThreshold(final String highThreshold) {
+            return setHighThreshold(asInt(highThreshold));
+        }
+
+        /**
+         * Sets the threshold for the size of normal priority issues.
+         *
+         * @param normalThreshold
+         *         the number of normal priority issues
+         *
+         * @return this
+         */
         public ThresholdSetBuilder setNormalThreshold(final int normalThreshold) {
             this.normalThreshold = normalThreshold;
             return this;
         }
 
+        /**
+         * Sets the threshold for the size of normal priority issues.
+         *
+         * @param normalThreshold
+         *         the number of normal priority issues
+         *
+         * @return this
+         */
+        public ThresholdSetBuilder setNormalThreshold(final String normalThreshold) {
+            return setNormalThreshold(asInt(normalThreshold));
+        }
+
+        /**
+         * Sets the threshold for the size of low priority issues.
+         *
+         * @param lowThreshold
+         *         the number of low priority issues
+         *
+         * @return this
+         */
         public ThresholdSetBuilder setLowThreshold(final int lowThreshold) {
             this.lowThreshold = lowThreshold;
             return this;
+        }
+
+        /**
+         * Sets the threshold for the size of low priority issues.
+         *
+         * @param lowThreshold
+         *         the number of low priority issues
+         *
+         * @return this
+         */
+        public ThresholdSetBuilder setLowThreshold(final String lowThreshold) {
+            return setLowThreshold(asInt(lowThreshold));
+        }
+
+        /**
+         * Converts the provided string threshold into an integer value.
+         *
+         * @param threshold
+         *         string representation of the threshold value
+         *
+         * @return integer threshold
+         * @throws IllegalArgumentException
+         *         if the provided string can't be converted to an integer value
+         *         greater or equal zero
+         */
+        private int asInt(final String threshold) {
+            if (StringUtils.isNotBlank(threshold)) {
+                try {
+                    int value = Integer.parseInt(threshold);
+                    if (value >= 0) { // negative values are not allowed
+                        return value;
+                    }
+                }
+                catch (NumberFormatException exception) {
+                    // not valid
+                }
+                throw new IllegalArgumentException("Not a readable integer value >= 0: " + threshold);
+            }
+            return 0; // a blank string means that a user disables the check
         }
 
         private int getTotalThreshold() {
@@ -135,7 +275,6 @@ public class ThresholdSet {
         private int getLowThreshold() {
             return lowThreshold;
         }
-
     }
 }
 
