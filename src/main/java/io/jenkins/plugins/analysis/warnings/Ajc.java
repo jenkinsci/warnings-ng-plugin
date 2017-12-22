648fa49917f021a39380e578f9ae7d88074f086d
Ulli Hafner <ullrich.hafner@gmail.com>
2017-12-22 22:47:21 +0100

    Introduced new base class for static analysis tools.
    Removed file handling from analysis-model since this part of the
    code might be Jenkins related. Reading files is now done in
    analysis-core.

--- src/main/java/io/jenkins/plugins/analysis/warnings/Ajc.java
+++ src/main/java/io/jenkins/plugins/analysis/warnings/Ajc.java
@@ -1,23 +1,20 @@
 package io.jenkins.plugins.analysis.warnings;
 
-import edu.hm.hafner.analysis.Issue;
-import edu.hm.hafner.analysis.IssueBuilder;
-import edu.hm.hafner.analysis.Issues;
-import edu.hm.hafner.analysis.parser.AjcParser;
-import hudson.Extension;
-import io.jenkins.plugins.analysis.core.steps.DefaultLabelProvider;
-import io.jenkins.plugins.analysis.core.steps.StaticAnalysisTool;
 import org.kohsuke.stapler.DataBoundConstructor;
 
-import java.io.File;
-import java.nio.charset.Charset;
+import edu.hm.hafner.analysis.AbstractParser;
+import edu.hm.hafner.analysis.parser.AjcParser;
+import io.jenkins.plugins.analysis.core.steps.DefaultLabelProvider;
+import io.jenkins.plugins.analysis.core.steps.StreamBasedParser;
+
+import hudson.Extension;
 
 /**
  * Provides a parser and customized messages for the AspectJ (ajc) Compiler.
  *
  * @author Ullrich Hafner
  */
-public class Ajc extends StaticAnalysisTool {
+public class Ajc extends StreamBasedParser {
     private static final String PARSER_NAME = Messages.Warnings_AjcParser_ParserName();
 
     @DataBoundConstructor
@@ -26,8 +23,8 @@ public class Ajc extends StaticAnalysisTool {
     }
 
     @Override
-    public Issues<Issue> parse(final File file, final Charset charset, final IssueBuilder builder) {
-        return new AjcParser().parse(file, charset, builder);
+    protected AbstractParser createParser() {
+        return new AjcParser();
     }
 
     /** Registers this tool as extension point implementation. */
--- src/main/java/io/jenkins/plugins/analysis/warnings/AnsibleLint.java
+++ src/main/java/io/jenkins/plugins/analysis/warnings/AnsibleLint.java
@@ -1,23 +1,20 @@
 package io.jenkins.plugins.analysis.warnings;
 
-import edu.hm.hafner.analysis.Issue;
-import edu.hm.hafner.analysis.IssueBuilder;
-import edu.hm.hafner.analysis.Issues;
-import edu.hm.hafner.analysis.parser.AnsibleLintParser;
-import hudson.Extension;
-import io.jenkins.plugins.analysis.core.steps.DefaultLabelProvider;
-import io.jenkins.plugins.analysis.core.steps.StaticAnalysisTool;
 import org.kohsuke.stapler.DataBoundConstructor;
 
-import java.io.File;
-import java.nio.charset.Charset;
+import edu.hm.hafner.analysis.AbstractParser;
+import edu.hm.hafner.analysis.parser.AnsibleLintParser;
+import io.jenkins.plugins.analysis.core.steps.DefaultLabelProvider;
+import io.jenkins.plugins.analysis.core.steps.StreamBasedParser;
+
+import hudson.Extension;
 
 /**
  * Provides a parser and customized messages for the Ansible Lint Compiler.
  *
  * @author Ullrich Hafner
  */
-public class AnsibleLint extends StaticAnalysisTool {
+public class AnsibleLint extends StreamBasedParser {
     private static final String PARSER_NAME = Messages.Warnings_AnsibleLint_ParserName();
 
     @DataBoundConstructor
@@ -26,8 +23,8 @@ public class AnsibleLint extends StaticAnalysisTool {
     }
 
     @Override
-    public Issues<Issue> parse(final File file, final Charset charset, final IssueBuilder builder) {
-        return new AnsibleLintParser().parse(file, charset, builder);
+    protected AbstractParser createParser() {
+        return new AnsibleLintParser();
     }
 
     /** Registers this tool as extension point implementation. */
