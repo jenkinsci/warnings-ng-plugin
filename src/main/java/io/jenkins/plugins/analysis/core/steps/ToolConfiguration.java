38e5aaf59b82756e99e7eac6021c4b982829e433
Ulli Hafner <ullrich.hafner@gmail.com>
2018-03-22 09:44:08 +0100

    Added a repeatable for the static analysis tool section in Publisher.
    Fixed display of property tabs.

---
+++ src/main/java/io/jenkins/plugins/analysis/core/steps/ToolConfiguration.java
@@ -0,0 +1,103 @@
+package io.jenkins.plugins.analysis.core.steps;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.AncestorInPath;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+import io.jenkins.plugins.analysis.core.model.StaticAnalysisTool;
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.AbstractDescribableImpl;
+import hudson.model.AbstractProject;
+import hudson.model.Descriptor;
+import hudson.util.FormValidation;
+
+/**
+ * Defines the configuration to parse a set of files using a predefined parser.
+ *
+ * @author Ulli Hafner
+ */
+public class ToolConfiguration extends AbstractDescribableImpl<ToolConfiguration> {
+    private final String pattern;
+    private StaticAnalysisTool tool;
+
+    /**
+     * Creates a new instance of {@link ToolConfiguration}.
+     *
+     * @param pattern
+     *            the pattern of files to parse
+     * @param tool
+     *            the name of the parser to use
+     */
+    @DataBoundConstructor
+    public ToolConfiguration(final String pattern, final StaticAnalysisTool tool) {
+        super();
+
+        this.pattern = pattern;
+        this.tool = tool;
+    }
+
+    public StaticAnalysisTool getTool() {
+        return tool;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%s)", tool.getName(), pattern);
+    }
+
+    /**
+     * Dummy descriptor for {@link ToolConfiguration}.
+     *
+     * @author Ulli Hafner
+     */
+    @Extension
+    public static class DescriptorImpl extends Descriptor<ToolConfiguration> {
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return StringUtils.EMPTY;
+        }
+
+        /**
+         * Performs on-the-fly validation on the ant pattern for input files.
+         *
+         * @param project
+         *         the project
+         * @param pattern
+         *         the file pattern
+         *
+         * @return the validation result
+         */
+        public FormValidation doCheckPattern(@AncestorInPath final AbstractProject<?, ?> project,
+                @QueryParameter final String pattern) {
+            if (project != null) { // there is no workspace in pipelines
+                try {
+                    FilePath workspace = project.getSomeWorkspace();
+                    if (workspace != null && workspace.exists()) {
+                        String result = workspace.validateAntFileMask(pattern,
+                                FilePath.VALIDATE_ANT_FILE_MASK_BOUND);
+                        if (result != null) {
+                            return FormValidation.error(result);
+                        }
+                    }
+                }
+                catch (InterruptedException | IOException ignore) {
+                    // ignore and return ok
+                }
+            }
+
+            return FormValidation.ok();
+        }
+    }
+}
+
