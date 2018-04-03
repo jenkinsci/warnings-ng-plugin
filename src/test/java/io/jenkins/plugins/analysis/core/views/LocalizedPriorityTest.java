32bbc8cf490a0f1f95325bed16aa0030c00bb612
Anna-Maria Hardi <gallenbe@hm.edu>
2018-04-03 21:10:14 +0200

    Completed Tests for class LocalizedPriority.

--- src/test/java/io/jenkins/plugins/analysis/core/views/LocalizedPriorityTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/views/LocalizedPriorityTest.java
@@ -1,6 +1,5 @@
 package io.jenkins.plugins.analysis.core.views;
 
-
 import edu.hm.hafner.analysis.Priority;
 import org.junit.jupiter.api.Test;
 import static edu.hm.hafner.analysis.assertj.Assertions.*;
@@ -18,4 +17,45 @@ class LocalizedPriorityTest {
         assertThat(actualResult).contains("Hoch");
         assertThat(actualResult).inUnicode();
     }
+
+    @Test
+    void priorityIsHighLong() {
+        String actualResult = LocalizedPriority.getLongLocalizedString(Priority.HIGH);
+        assertThat(actualResult).isNotEmpty();
+        assertThat(actualResult).contains("Hohe Priorität");
+        assertThat(actualResult).inUnicode();
+    }
+
+    @Test
+    void priorityIsLow() {
+        String actualResult = LocalizedPriority.getLocalizedString(Priority.LOW);
+        assertThat(actualResult).isNotEmpty();
+        assertThat(actualResult).contains("Niedrig");
+        assertThat(actualResult).inUnicode();
+    }
+
+    @Test
+    void priorityIsLowLong() {
+        String actualResult = LocalizedPriority.getLongLocalizedString(Priority.LOW);
+        assertThat(actualResult).isNotEmpty();
+        assertThat(actualResult).contains("Niedrige Priorität");
+        assertThat(actualResult).inUnicode();
+    }
+
+    @Test
+    void priorityIsNormal() {
+        String actualResult = LocalizedPriority.getLocalizedString(Priority.NORMAL);
+        assertThat(actualResult).isNotEmpty();
+        assertThat(actualResult).contains("Normal");
+        assertThat(actualResult).inUnicode();
+    }
+
+    @Test
+    void priorityIsNormalLong() {
+        String actualResult = LocalizedPriority.getLongLocalizedString(Priority.NORMAL);
+        assertThat(actualResult).isNotEmpty();
+        assertThat(actualResult).contains("Normale Priorität");
+        assertThat(actualResult).inUnicode();
+    }
+
 }
