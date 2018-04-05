8b02b78fc9e19ee3c099c48c2f412d71159d9363
Ulli Hafner <ullrich.hafner@gmail.com>
2018-04-05 17:17:41 +0200

    Added a test for the combo box model for all Jenkins jobs.

--- src/test/java/io/jenkins/plugins/analysis/core/steps/IssuesRecorderTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/steps/IssuesRecorderTest.java
@@ -1,10 +1,16 @@
 package io.jenkins.plugins.analysis.core.steps;
 
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
 
+import io.jenkins.plugins.analysis.core.JenkinsFacade;
 import io.jenkins.plugins.analysis.core.steps.IssuesRecorder.Descriptor;
-import static io.jenkins.plugins.analysis.core.testutil.FormValidationAssert.*;
+import static io.jenkins.plugins.analysis.core.testutil.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import hudson.model.Job;
+import hudson.util.ComboBoxModel;
 import hudson.util.FormValidation;
 
 /**
@@ -14,11 +20,38 @@ import hudson.util.FormValidation;
  */
 class IssuesRecorderTest {
     @Test
-    void shouldBeOkIfIfEncodingIsEmpty() {
+    void shouldBeOkIfEncodingIsEmpty() {
         Descriptor descriptor = new Descriptor();
 
         FormValidation actualResult = descriptor.doCheckSourceCodeEncoding("");
 
         assertThat(actualResult).isOk();
+    }
+    
+    @Test
+    void shouldContainEmptyJobPlaceHolder() {
+        JenkinsFacade jenkins = mock(JenkinsFacade.class);
+        Descriptor descriptor = new Descriptor(jenkins);
+
+        ComboBoxModel actualModel = descriptor.doFillReferenceJobItems();
+
+        assertThat(actualModel).hasSize(1);
+        assertThat(actualModel).containsExactly(IssuesRecorder.NO_REFERENCE_JOB);
+    }
+
+    @Test
+    void shouldContainSingleElementAndPlaceHolder() {
+        JenkinsFacade jenkins = mock(JenkinsFacade.class);
+        Job job = mock(Job.class);
+        String name = "Job Name";
+        when(jenkins.getFullNameOf(job)).thenReturn(name);
+        when(jenkins.getAllJobs()).thenReturn(Collections.singleton(name));
+
+        Descriptor descriptor = new Descriptor(jenkins);
+
+        ComboBoxModel actualModel = descriptor.doFillReferenceJobItems();
+
+        assertThat(actualModel).hasSize(2);
+        assertThat(actualModel).containsExactly("-", name);
     }
 }
