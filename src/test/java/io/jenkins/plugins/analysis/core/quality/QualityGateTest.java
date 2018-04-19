b69082915ecdb3e1fb99bf79d32fff39b0561de4
loelala <avogel@hm.edu>
2018-04-19 13:37:23 +0200

    removed unused import

--- src/test/java/io/jenkins/plugins/analysis/core/quality/QualityGateTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/quality/QualityGateTest.java
@@ -2,7 +2,6 @@ package io.jenkins.plugins.analysis.core.quality;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.function.Function;
 
 import org.junit.jupiter.api.Test;
