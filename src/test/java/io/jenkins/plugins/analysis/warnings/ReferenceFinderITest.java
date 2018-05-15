9a4e497212e0ffc3b96d085769841801eb628240
arne2 <arne.schoentag@imagefiguren.de>
2018-05-15 17:29:11 +0200

    add test - not working yet

---
+++ src/test/java/io/jenkins/plugins/analysis/warnings/ReferenceFinderITest.java
@@ -0,0 +1,239 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.function.Consumer;
+
+import org.junit.Test;
+
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+
+import io.jenkins.plugins.analysis.core.model.AnalysisResult;
+import static io.jenkins.plugins.analysis.core.model.Assertions.*;
+import io.jenkins.plugins.analysis.core.steps.IssuesRecorder;
+import io.jenkins.plugins.analysis.core.steps.ToolConfiguration;
+import io.jenkins.plugins.analysis.core.testutil.IntegrationTest;
+import io.jenkins.plugins.analysis.core.views.ResultAction;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Result;
+
+public class ReferenceFinderITest extends IntegrationTest {
+
+    // @Myself: TODO Remove before merge
+    // mvn build
+    // go.sh in parent ausführen
+    // Reference Part anschauen
+    // Jenkins Issue (JIRA) Testfälle dokumentieren
+    // Finden:
+    // Neuer Build (optional)
+    // (Add)Post-build action: "Record static analysis results"
+    // Feld "Reference Job"
+    // publisher.setOverallResultMustBeSuccess();
+    // publisher.setIgnoreAnalysisResult();
+    private String jobName = "Job";
+
+
+    @Test
+    public void shouldCreateUnstableResultIfPreviousIsUnstable() throws IOException, InterruptedException {
+
+        /*String refName = "ReferenceJob";
+        FreeStyleProject ref = createJobWithWorkspaceFile(refName, "eclipse.txt");
+        enableWarnings(ref, publisher -> publisher.setUnstableTotalAll(7));
+
+        AnalysisResult refResult = scheduleBuildAndAssertStatus(ref, Result.UNSTABLE);
+        assertThat(refResult).hasTotalSize(8);
+        assertThat(refResult).hasOverallResult(Result.UNSTABLE);
+
+        FreeStyleProject project = createJobWithWorkspaceFile(jobName, "eclipse2.txt");
+        IssuesRecorder recorder = enableWarnings(project, publisher -> {
+            publisher.setReferenceJobName(refName);
+            publisher.setUnstableTotalAll(7);
+        });
+
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.UNSTABLE);
+        assertThat(result).hasTotalSize(6);
+        assertThat(result).hasOverallResult(Result.UNSTABLE);*/
+
+
+
+        FreeStyleProject project = createJobWithWorkspaceFile(jobName, "eclipse.txt");
+        enableWarnings(project, publisher -> {
+//            publisher.setAggregatingResults(true);
+            publisher.setUnstableTotalAll(7);
+        });
+
+
+        AnalysisResult result;
+        result = scheduleBuildAndAssertStatus(project, Result.UNSTABLE);
+        assertThat(result).hasTotalSize(8);
+        assertThat(result).hasOverallResult(Result.UNSTABLE);
+
+        //System.out.println(project.getSomeWorkspace().list());
+        project.getSomeWorkspace().deleteContents();
+        copyFilesToWorkspace(project, "eclipse2.txt");
+
+        result = scheduleBuildAndAssertStatus(project, Result.UNSTABLE);
+        assertThat(result).hasTotalSize(6);
+        assertThat(result).hasOverallResult(Result.UNSTABLE);
+    }
+
+    @Test
+    public void shouldCreateSuccessResultIfPreviousIsIgnored() throws IOException, InterruptedException {
+        FreeStyleProject project = createJobWithWorkspaceFile(jobName, "eclipse.txt");
+        enableWarnings(project, publisher -> {
+            publisher.setUnstableTotalAll(7);
+            publisher.setIgnoreAnalysisResult(true);
+        });
+
+        AnalysisResult result;
+        result = scheduleBuildAndAssertStatus(project, Result.UNSTABLE);
+        assertThat(result).hasTotalSize(8);
+        assertThat(result).hasOverallResult(Result.UNSTABLE);
+
+        project.getSomeWorkspace().deleteContents();
+        copyFilesToWorkspace(project, "eclipse2.txt");
+
+        result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        assertThat(result).hasTotalSize(6);
+        assertThat(result).hasOverallResult(Result.SUCCESS);
+    }
+
+    @Test
+    public void shouldCreateSuccessResultIfOverallMustMeSuccess() throws IOException, InterruptedException {
+        FreeStyleProject project = createJobWithWorkspaceFile(jobName, "eclipse.txt");
+        enableWarnings(project, publisher -> {
+            publisher.setUnstableTotalAll(7);
+            publisher.setOverallResultMustBeSuccess(true);
+            publisher.setIgnoreAnalysisResult(true);
+        });
+
+        AnalysisResult result;
+        result = scheduleBuildAndAssertStatus(project, Result.UNSTABLE);
+        assertThat(result).hasTotalSize(8);
+        assertThat(result).hasOverallResult(Result.UNSTABLE);
+
+        project.getSomeWorkspace().deleteContents();
+        copyFilesToWorkspace(project, "eclipse2.txt");
+
+        result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        assertThat(result).hasTotalSize(6);
+        assertThat(result).hasOverallResult(Result.SUCCESS);
+    }
+
+    @Test
+    public void shouldCreateUnstableResultIfReferenceBuildIsUnstable() throws IOException, InterruptedException {
+        String refName = "ReferenceJob";
+        FreeStyleProject ref = createJobWithWorkspaceFile(refName, "eclipse.txt");
+        enableWarnings(ref, publisher -> publisher.setUnstableTotalAll(7));
+
+        AnalysisResult refResult = scheduleBuildAndAssertStatus(ref, Result.UNSTABLE);
+        assertThat(refResult).hasTotalSize(8);
+        assertThat(refResult).hasOverallResult(Result.UNSTABLE);
+
+        FreeStyleProject project = createJobWithWorkspaceFile(jobName, "eclipse2.txt");
+        IssuesRecorder recorder = enableWarnings(project, publisher -> {
+            publisher.setReferenceJobName(refName);
+//            publisher.setAggregatingResults(true);
+            publisher.setUnstableTotalAll(7);
+        });
+
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.UNSTABLE);
+        assertThat(result).hasTotalSize(6);
+        assertThat(result).hasOverallResult(Result.UNSTABLE);
+    }
+
+    // Copied from IssuesRecorderITest
+
+    /**
+     * Creates a new {@link FreeStyleProject freestyle job}. The job will get a generated name.
+     *
+     * @return the created job
+     */
+    private FreeStyleProject createJob(String name) {
+        try {
+            return j.createFreeStyleProject(name);
+        }
+        catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    /**
+     * Creates a new {@link FreeStyleProject freestyle job} and copies the specified resources to the workspace folder.
+     * The job will get a generated name.
+     *
+     * @param fileNames
+     *         the files to copy to the workspace
+     *
+     * @return the created job
+     */
+    private FreeStyleProject createJobWithWorkspaceFile(String name, final String... fileNames) {
+        FreeStyleProject job = createJob(name);
+        copyFilesToWorkspace(job, fileNames);
+        return job;
+    }
+
+    /**
+     * Enables the warnings plugin for the specified job. I.e., it registers a new {@link IssuesRecorder } recorder for
+     * the job.
+     *
+     * @param job
+     *         the job to register the recorder for
+     *
+     * @return the created recorder
+     */
+    @CanIgnoreReturnValue
+    private IssuesRecorder enableWarnings(final FreeStyleProject job) {
+        IssuesRecorder publisher = new IssuesRecorder();
+        publisher.setTools(Collections.singletonList(new ToolConfiguration("**/*issues.txt", new Eclipse())));
+        job.getPublishersList().add(publisher);
+        return publisher;
+    }
+
+    /**
+     * Enables the warnings plugin for the specified job. I.e., it registers a new {@link IssuesRecorder } recorder for
+     * the job.
+     *
+     * @param job
+     *         the job to register the recorder for
+     * @param configuration
+     *         configuration of the recorder
+     *
+     * @return the created recorder
+     */
+    @CanIgnoreReturnValue
+    private IssuesRecorder enableWarnings(final FreeStyleProject job, final Consumer<IssuesRecorder> configuration) {
+        IssuesRecorder publisher = enableWarnings(job);
+        configuration.accept(publisher);
+        return publisher;
+    }
+
+    /**
+     * Schedules a new build for the specified job and returns the created {@link AnalysisResult} after the build has
+     * been finished.
+     *
+     * @param job
+     *         the job to schedule
+     * @param status
+     *         the expected result for the build
+     *
+     * @return the created {@link ResultAction}
+     */
+    @SuppressWarnings({"illegalcatch", "OverlyBroadCatchBlock"})
+    private AnalysisResult scheduleBuildAndAssertStatus(final FreeStyleProject job, final Result status) {
+        try {
+            FreeStyleBuild build = j.assertBuildStatus(status, job.scheduleBuild2(0));
+
+            ResultAction action = build.getAction(ResultAction.class);
+
+            assertThat(action).isNotNull();
+
+            return action.getResult();
+        }
+        catch (Exception e) {
+            throw new AssertionError(e);
+        }
+    }
+}
