1f4bb33b128902df794917f90e99c342063dba6f
Michaela Reitschuster <michaelarstr@gmail.com>
2018-05-30 10:38:47 +0200

    introduction of qualitygate integrationtests

---
+++ src/test/java/io/jenkins/plugins/analysis/warnings/FreeStyleProjectQualityGateITest.java
@@ -0,0 +1,29 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import java.io.IOException;
+
+import hudson.model.FreeStyleProject;
+
+/**
+ * Integration tests of {@link FreeStyleProject} with {@link io.jenkins.plugins.analysis.core.quality.QualityGate}.
+ *
+ * @author Michaela Reitschuster
+ */
+public class FreeStyleProjectQualityGateITest extends AbstractQualityGateITest<FreeStyleProject> {
+
+    @Override
+    protected FreeStyleProject getProject() {
+        FreeStyleProject job = createJob();
+        copyFilesToWorkspace(job, "checkstyle-qualitygate.xml");
+        return job;
+    }
+
+    private FreeStyleProject createJob() {
+        try {
+            return j.createFreeStyleProject();
+        }
+        catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+}
---
+++ src/test/java/io/jenkins/plugins/analysis/warnings/MavenProjectQualityGateITest.java
@@ -0,0 +1,58 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.jvnet.hudson.test.ToolInstallations;
+
+import hudson.FilePath;
+import hudson.maven.MavenModuleSet;
+import hudson.model.TopLevelItem;
+import hudson.tasks.Maven;
+import hudson.tasks.Maven.MavenInstallation;
+
+/**
+ * Integration tests of {@link MavenModuleSet} with {@link io.jenkins.plugins.analysis.core.quality.QualityGate}.
+ *
+ * @author Michaela Reitschuster
+ */
+public class MavenProjectQualityGateITest extends AbstractQualityGateITest<MavenModuleSet> {
+
+    @Before
+    public void initializeMaven() throws Exception {
+        MavenInstallation mvn = ToolInstallations.configureMaven35();
+        MavenInstallation m3 = new MavenInstallation("apache-maven-3.3.9", mvn.getHome(), j.NO_PROPERTIES);
+        j.jenkins.getDescriptorByType(Maven.DescriptorImpl.class).setInstallations(m3);
+
+    }
+
+    @Override
+    protected MavenModuleSet getProject() {
+        MavenModuleSet project = createJob();
+        copyFilesToWorkspace(project, "checkstyle-qualitygate.xml");
+        copyToWorkspace(project, "pom.xml");
+        return project;
+    }
+
+    private void copyToWorkspace(final TopLevelItem job, final String... fileNames) {
+        try {
+            FilePath workspace = j.jenkins.getWorkspaceFor(job);
+            for (String fileName : fileNames) {
+                workspace.child(fileName).copyFrom(asInputStream(fileName));
+            }
+        }
+        catch (IOException | InterruptedException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    private MavenModuleSet createJob() {
+        try {
+            return j.createProject(MavenModuleSet.class);
+        }
+        catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+}
