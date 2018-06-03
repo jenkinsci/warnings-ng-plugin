d0d17b00fda8a055522ee65b1b1615cc30159b90
Manuel <hampp@hm.edu>
2018-06-03 21:09:23 +0200

    [JENKINS-50077] Added integration tests for Regex-Filter [JENKINS-50080] Added integration tests for Remote-API

---
+++ src/test/java/io/jenkins/plugins/analysis/warnings/IssueRecorderRegexTest.java
@@ -0,0 +1,366 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+
+import edu.hm.hafner.analysis.Issue;
+import io.jenkins.plugins.analysis.core.model.AnalysisResult;
+import io.jenkins.plugins.analysis.core.model.ExcludeCategory;
+import io.jenkins.plugins.analysis.core.model.ExcludeFile;
+import io.jenkins.plugins.analysis.core.model.ExcludeModule;
+import io.jenkins.plugins.analysis.core.model.ExcludePackage;
+import io.jenkins.plugins.analysis.core.model.ExcludeType;
+import io.jenkins.plugins.analysis.core.model.IncludeCategory;
+import io.jenkins.plugins.analysis.core.model.IncludeFile;
+import io.jenkins.plugins.analysis.core.model.IncludeModule;
+import io.jenkins.plugins.analysis.core.model.IncludePackage;
+import io.jenkins.plugins.analysis.core.model.IncludeType;
+import io.jenkins.plugins.analysis.core.model.RegexpFilter;
+import io.jenkins.plugins.analysis.core.model.StaticAnalysisTool;
+import io.jenkins.plugins.analysis.core.steps.IssuesRecorder;
+import io.jenkins.plugins.analysis.core.steps.ToolConfiguration;
+import static org.assertj.core.api.Assertions.*;
+
+import hudson.FilePath;
+import hudson.model.FreeStyleProject;
+import hudson.model.TopLevelItem;
+
+/**
+ * Integration tests of the warnings plug-in in freestyle jobs. Tests the new recorder {@link IssuesRecorder}. Subclass
+ * to test the regex filter
+ *
+ * @author Manuel Hampp
+ */
+public class IssueRecorderRegexTest extends IssuesRecorderITest {
+
+    /**
+     * Tests the module expression filter by comparing the result with expected.
+     */
+    @Test
+    public void shouldFilterIssuesByModule() {
+        // get collection of filters, and expcepted results
+        Map<RegexpFilter, Integer[]> categoryFiltersWithResult = filterProviderModules();
+
+        // run tests for all in collection
+        for (Entry<RegexpFilter, Integer[]> entry : categoryFiltersWithResult.entrySet()) {
+
+            // move files to workspace
+            String[] workspaceFiles = {"ModuleRegexTest/pmd.xml", "ModuleRegexTest/pom.xml", "ModuleRegexTest/m1/pom.xml", "ModuleRegexTest/m2/pom.xml"};
+
+            // set up project
+            FreeStyleProject project = prepareFilesAndCreateJob(workspaceFiles);
+            enableWarningsWithAnalysisTool(
+                    project,
+                    recorder -> recorder.setFilters(Collections.singletonList(entry.getKey())),
+                    new Pmd());
+
+            // get result
+            AnalysisResult result = scheduleBuild(project);
+
+            // compare result with expected values
+            assertThat(result.getIssues()
+                    .stream()
+                    .map(Issue::getLineStart)
+                    .collect(Collectors.toList())).containsOnly(entry.getValue());
+        }
+    }
+
+    /**
+     * Tests the category and file expression filter by comparing the result with expected.
+     */
+    @Test
+    public void shouldFilterIssuesForCheckstyle() {
+        // get collection of filters, and expcepted results
+        Map<RegexpFilter, Integer[]> categoryFiltersWithResult = filterProviderCheckstyle();
+
+        // run tests for all in collection
+        for (Entry<RegexpFilter, Integer[]> entry : categoryFiltersWithResult.entrySet()) {
+
+            // set up environment
+            FreeStyleProject project = createJobWithWorkspaceFile("checkstyleregextest.xml");
+            enableWarningsWithAnalysisTool(project,
+                    recorder -> recorder.setFilters(Collections.singletonList(entry.getKey())),
+                    new CheckStyle());
+
+            // get result
+            AnalysisResult result = scheduleBuild(project);
+
+            // compare result with expected values
+            assertThat(result.getIssues()
+                    .stream()
+                    .map(Issue::getLineStart)
+                    .collect(Collectors.toList())).containsOnly(entry.getValue());
+        }
+    }
+
+    /**
+     * Tests the package and type expression filter by comparing the result with expected.
+     */
+    @Test
+    public void shouldFilterIssuesForPMD() {
+        // get collection of filters, and expcepted results
+        Map<RegexpFilter, Integer[]> categoryFiltersWithResult = filterProviderPMD();
+
+        // run tests for all in collection
+        for (Entry<RegexpFilter, Integer[]> entry : categoryFiltersWithResult.entrySet()) {
+
+            // set up environment
+            FreeStyleProject project = createJobWithWorkspaceFile("pmd-warnings.xml");
+            enableWarningsWithAnalysisTool(project,
+                    recorder -> recorder.setFilters(Collections.singletonList(entry.getKey())), new Pmd());
+
+            // get result
+            AnalysisResult result = scheduleBuild(project);
+
+            // compare result with expected values
+            assertThat(result.getIssues()
+                    .stream()
+                    .map(Issue::getLineStart)
+                    .collect(Collectors.toList())).containsOnly(entry.getValue());
+        }
+    }
+
+    /**
+     * Provides a map, that contains the filters and the linenumbers that are expceted to remain after filtering.
+     */
+    private Map<RegexpFilter, Integer[]> filterProviderModules() {
+        /*
+        CopyToClipboard.java:54         com.avaloq.adt.env.internal.ui.actions          Basic CollapsibleIfStatements   Normal  1
+        ChangeSelectionAction.java:14   com.avaloq.adt.env.internal.ui.actions.change   Import Statement Rules  UnusedImports   Normal  1
+         */
+        HashMap<RegexpFilter, Integer[]> filterResultMap = new HashMap<>();
+        filterResultMap.put(new RegexpFilter("m1", new ExcludeModule()), new Integer[]{14});
+        filterResultMap.put(new RegexpFilter("m1", new IncludeModule()), new Integer[]{54});
+
+        return filterResultMap;
+    }
+
+    /**
+     * Provides a map, that contains the filters and the linenumbers that are expceted to remain after filtering.
+     */
+    private Map<RegexpFilter, Integer[]> filterProviderCheckstyle() {
+        /*
+          CsharpNamespaceDetector.java:30   -Blocks RightCurlyCheck High 1
+          CsharpNamespaceDetector.java:37   -Blocks RightCurlyCheck High 1
+          CsharpNamespaceDetector.java:17   -Design DesignForExtensionCheck High 1
+          CsharpNamespaceDetector.java:22   -Design DesignForExtensionCheck High 1
+          CsharpNamespaceDetector.java:42   -Sizes LineLengthCheck High 1
+          CsharpNamespaceDetector.java:29   -Sizes LineLengthCheck High 1
+          FileFinder.java:99                -Blocks RightCurlyCheck High 1
+
+         */
+        HashMap<RegexpFilter, Integer[]> filterResultMap = new HashMap<>();
+        filterResultMap.put(new RegexpFilter("Blocks", new IncludeCategory()), new Integer[]{30, 37, 99});
+        filterResultMap.put(new RegexpFilter("Blocks", new ExcludeCategory()), new Integer[]{17, 22, 42, 29});
+        filterResultMap.put(new RegexpFilter("(Blocks|Design)", new ExcludeCategory()), new Integer[]{42, 29});
+        filterResultMap.put(new RegexpFilter("(Blocks|Design)", new IncludeCategory()),
+                new Integer[]{30, 37, 17, 22, 99});
+        filterResultMap.put(new RegexpFilter(".*Csharp.*", new ExcludeFile()), new Integer[]{99});
+        filterResultMap.put(new RegexpFilter(".*Csharp.*", new IncludeFile()), new Integer[]{30, 37, 17, 22, 42, 29});
+
+        return filterResultMap;
+    }
+
+    /**
+     * Provides a map, that contains the filters and the linenumbers that are expceted to remain after filtering.
+     */
+    private Map<RegexpFilter, Integer[]> filterProviderPMD() {
+        /*
+        CopyToClipboard.java:54         com.avaloq.adt.env.internal.ui.actions          Basic CollapsibleIfStatements   Normal  1
+        ChangeSelectionAction.java:14   com.avaloq.adt.env.internal.ui.actions.change   Import Statement Rules  UnusedImports   Normal  1
+        SelectSourceDialog.java:938     com.avaloq.adt.env.internal.ui.dialogs          Basic Rules EmptyCatchBlock Low 1
+        SelectSourceDialog.java:980     com.avaloq.adt.env.internal.ui.dialogs          Basic Rules EmptyCatchBlock High    1
+         */
+        HashMap<RegexpFilter, Integer[]> filterResultMap = new HashMap<>();
+        filterResultMap.put(new RegexpFilter(".*actions", new IncludePackage()), new Integer[]{54});
+        filterResultMap.put(new RegexpFilter(".*actions.*", new IncludePackage()), new Integer[]{54, 14});
+        filterResultMap.put(new RegexpFilter(".*actions.*", new ExcludePackage()), new Integer[]{938, 980});
+        filterResultMap.put(new RegexpFilter(".*actions", new ExcludePackage()), new Integer[]{14, 938, 980});
+
+        filterResultMap.put(new RegexpFilter(".*EmptyCatchBlock", new IncludeType()), new Integer[]{938, 980});
+        filterResultMap.put(new RegexpFilter(".*EmptyCatchBlock", new ExcludeType()), new Integer[]{54, 14});
+
+        return filterResultMap;
+    }
+
+    /**
+     * Enables warnings for the given job with the given analysis tool.
+     */
+    private void enableWarningsWithAnalysisTool(final FreeStyleProject job,
+            final Consumer<IssuesRecorder> configuration,
+            StaticAnalysisTool staticAnalysisTool) {
+        IssuesRecorder publisher = enableWarningsWithAnalysisTool(job, staticAnalysisTool);
+        configuration.accept(publisher);
+    }
+
+    /**
+     * Enables the warnings plugin for the specified job. I.e., it registers a new {@link IssuesRecorder } recorder for
+     * the job.
+     *
+     * @param job
+     *         the job to register the recorder for
+     *
+     * @return the created recorder
+     */
+    @CanIgnoreReturnValue
+    private IssuesRecorder enableWarningsWithAnalysisTool(final FreeStyleProject job,
+            StaticAnalysisTool staticAnalysisTool) {
+        IssuesRecorder publisher = new IssuesRecorder();
+        publisher.setTools(Collections.singletonList(new ToolConfiguration("**/*issues.txt", staticAnalysisTool)));
+        job.getPublishersList().add(publisher);
+        return publisher;
+    }
+
+    /**
+     * Creates a new {@link FreeStyleProject freestyle job} and copies the specified resources to the workspace folder.
+     * The job will get a generated name.
+     *
+     * @param fileNames
+     *         the files to copy to the workspace
+     *
+     * @return the created job
+     */
+    private FreeStyleProject prepareFilesAndCreateJob(final String... fileNames) {
+        FreeStyleProject job = createJob();
+        prepareAndCopyFilesToWorkspace(job, fileNames);
+        return job;
+    }
+
+    /**
+     * Prepares files and copies them to the workspace.
+     *
+     * @param job
+     *         the job to get the workspace for
+     * @param fileNames
+     *         the dir to copy
+     */
+    private void prepareAndCopyFilesToWorkspace(final TopLevelItem job, final String... fileNames) {
+        try {
+            FilePath workspace = j.jenkins.getWorkspaceFor(job);
+            for (String fileName : fileNames) {
+                // copy pom file without renaiming
+                if (fileName.endsWith("pom.xml")) {
+                    workspace.child(fileName).copyFrom(asInputStream(fileName));
+                }
+                // replace placeholders and copy issue xml
+                workspace.child(createWorkspaceFileName(fileName))
+                        .copyFrom(
+                                new ReplacingInputStream(asInputStream(fileName), "WORKSPACEDIRPLACEHOLDER".getBytes(),
+                                        workspace.getRemote().getBytes()));
+            }
+        }
+        catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+/**
+ * Simple filter for InputStream that replaces string parts with another.
+ */
+class ReplacingInputStream extends FilterInputStream {
+    private LinkedList<Integer> input = new LinkedList<Integer>();
+    private LinkedList<Integer> output = new LinkedList<Integer>();
+    private final byte[] search;
+    private final byte[] replacement;
+
+    ReplacingInputStream(InputStream in, byte[] search, byte[] replacement) {
+        super(in);
+        this.search = search;
+        this.replacement = replacement;
+    }
+
+    private boolean isMatchFound() {
+        Iterator<Integer> inputIterator = input.iterator();
+        for (byte b : search) {
+            if (!inputIterator.hasNext() || b != inputIterator.next()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void readAhead() throws IOException {
+        while (input.size() < search.length) {
+            int next = super.read();
+            input.offer(next);
+            if (next == -1) {
+                break;
+            }
+        }
+    }
+
+    @Override
+    public int read(final byte[] b) throws IOException {
+        return super.read(b);
+    }
+
+    @Override
+    public int read(final byte[] bytes, final int off, final int len) throws IOException {
+
+        if (bytes == null) {
+            throw new NullPointerException();
+        }
+        else if (off < 0 || len < 0 || len > bytes.length - off) {
+            throw new IndexOutOfBoundsException();
+        }
+        else if (len == 0) {
+            return 0;
+        }
+
+        int c = read();
+        if (c == -1) {
+            return -1;
+        }
+        bytes[off] = (byte) c;
+
+        int i = 1;
+        try {
+            for (; i < len; i++) {
+                c = read();
+                if (c == -1) {
+                    break;
+                }
+                bytes[off + i] = (byte) c;
+            }
+        }
+        catch (IOException ignored) {
+            // ignore
+        }
+        return i;
+
+    }
+
+    @Override
+    public int read() throws IOException {
+        if (output.isEmpty()) {
+            readAhead();
+            if (isMatchFound()) {
+                for (byte aSearch : search) {
+                    input.remove();
+                }
+                for (byte b : replacement) {
+                    output.offer((int) b);
+                }
+            }
+            else {
+                output.add(input.remove());
+            }
+        }
+        return output.remove();
+    }
+}
