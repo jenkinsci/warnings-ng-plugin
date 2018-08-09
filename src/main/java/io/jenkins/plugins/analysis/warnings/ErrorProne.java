189d50136ac056a1b12b352ff3966981333a05ca
Ulli Hafner <ullrich.hafner@gmail.com>
2018-08-09 14:55:44 +0200

    Added icons for error prone and DRY tools.

--- src/main/java/io/jenkins/plugins/analysis/warnings/ErrorProne.java
+++ src/main/java/io/jenkins/plugins/analysis/warnings/ErrorProne.java
@@ -5,6 +5,8 @@ import javax.annotation.Nonnull;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import edu.hm.hafner.analysis.parser.violations.ErrorProneAdapter;
+import static hudson.plugins.warnings.WarningsDescriptor.IMAGE_PREFIX;
+import io.jenkins.plugins.analysis.core.model.StaticAnalysisLabelProvider;
 import io.jenkins.plugins.analysis.core.model.StaticAnalysisTool;
 
 import hudson.Extension;
@@ -30,6 +32,26 @@ public class ErrorProne extends StaticAnalysisTool {
         return new ErrorProneAdapter();
     }
 
+    /** Provides the labels for the static analysis tool. */
+    private static class LabelProvider extends StaticAnalysisLabelProvider {
+        private static final String SMALL_ICON_URL = IMAGE_PREFIX + "bug-24x24.png";
+        private static final String LARGE_ICON_URL = IMAGE_PREFIX + "bug-48x48.png";
+
+        LabelProvider() {
+            super(ID, Messages.Violations_ErrorProne());
+        }
+
+        @Override
+        public String getSmallIconUrl() {
+            return SMALL_ICON_URL;
+        }
+
+        @Override
+        public String getLargeIconUrl() {
+            return LARGE_ICON_URL;
+        }
+    }
+
     /** Descriptor for this static analysis tool. */
     @Extension
     public static class Descriptor extends StaticAnalysisToolDescriptor {
@@ -42,6 +64,11 @@ public class ErrorProne extends StaticAnalysisTool {
         @Override
         public String getDisplayName() {
             return Messages.Violations_ErrorProne();
+        }
+
+        @Override
+        public String getUrl() {
+            return "https://errorprone.info";
         }
     }
 }
