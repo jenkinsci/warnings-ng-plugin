6aed0bf4a62b7ba8c535799c3ed42d8ae18b0126
Ulli Hafner <ullrich.hafner@gmail.com>
2018-08-24 00:29:47 +0200

    Removed JavaDoc imports.

--- src/main/java/io/jenkins/plugins/analysis/WarningsPlugin.java
+++ src/main/java/io/jenkins/plugins/analysis/WarningsPlugin.java
@@ -1,19 +1,16 @@
 package io.jenkins.plugins.analysis;
 
-import java.util.ServiceLoader;
-
 import io.jenkins.plugins.analysis.warnings.PmdMessages;
-import net.sourceforge.pmd.lang.LanguageRegistry;
 
 import hudson.Plugin;
 
 /**
  * Entry point for the warnings plug-in. Currently only used to read the PMD rules, that are loaded from the PMD library
- * using the {@link ServiceLoader} with the default class loader. If this code is executed later on using the plug-in
- * class loader then no message rules are found.
+ * using the {@link java.util.ServiceLoader} with the default class loader. If this code is executed later on using the
+ * plug-in class loader then no message rules are found.
  *
  * @author Ullrich Hafner
- * @see LanguageRegistry
+ * @see net.sourceforge.pmd.lang.LanguageRegistry
  */
 @SuppressWarnings("deprecation")
 public class WarningsPlugin extends Plugin {
