6d302be992923450e591e2ffe03356949de20275
Ulli Hafner <ullrich.hafner@gmail.com>
2018-09-02 20:45:34 +0200

    Fixed broken test due to changed order of series.

--- src/test/java/io/jenkins/plugins/analysis/core/charts/PriorityChartTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/charts/PriorityChartTest.java
@@ -38,9 +38,9 @@ class PriorityChartTest {
                 .isArray()
                 .ofLength(3);
 
-        verifySeries(model.getSeries().get(0), Severity.WARNING_HIGH, 1, 2);
+        verifySeries(model.getSeries().get(0), Severity.WARNING_LOW, 3, 6);
         verifySeries(model.getSeries().get(1), Severity.WARNING_NORMAL, 2, 4);
-        verifySeries(model.getSeries().get(2), Severity.WARNING_LOW, 3, 6);
+        verifySeries(model.getSeries().get(2), Severity.WARNING_HIGH, 1, 2);
     }
 
     private void verifySeries(final LineSeries high, final Severity severity, 
