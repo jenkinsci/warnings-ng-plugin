0267bf20feea5d1c679e08883e062788e2d0b054
Ulli Hafner <ullrich.hafner@gmail.com>
2018-09-18 21:10:03 +0200

    Added support for KtLint.

---
+++ src/main/java/io/jenkins/plugins/analysis/warnings/KtLint.java
@@ -0,0 +1,62 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import javax.annotation.Nonnull;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import edu.hm.hafner.analysis.parser.checkstyle.CheckStyleParser;
+import io.jenkins.plugins.analysis.core.model.StaticAnalysisTool;
+
+import hudson.Extension;
+
+/**
+ * Provides a parser and customized messages for ktlint. Delegates to {@link CheckStyleParser}.
+ *
+ * @author Ullrich Hafner
+ */
+public class KtLint extends StaticAnalysisTool {
+    private static final long serialVersionUID = -7944828406964963020L;
+    static final String ID = "ktlint";
+
+    /** Creates a new instance of {@link KtLint}. */
+    @DataBoundConstructor
+    public KtLint() {
+        super();
+        // empty constructor required for stapler
+    }
+
+    @Override
+    public boolean canScanConsoleLog() {
+        return false;
+    }
+
+    @Override
+    public CheckStyleParser createParser() {
+        return new CheckStyleParser();
+    }
+
+    /** Descriptor for this static analysis tool. */
+    @Extension
+    public static class Descriptor extends StaticAnalysisToolDescriptor {
+        /** Creates the descriptor instance. */
+        public Descriptor() {
+            super(ID);
+        }
+
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return Messages.Warnings_KtLint_Name();
+        }
+
+        @Override
+        public String getHelp() {
+            return "Use option --reporter=checkstyle.";
+        }
+
+        @Override
+        public String getUrl() {
+            return "https://arturbosch.github.io/detekt/";
+        }
+    }
+}
