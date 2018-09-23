15e0b39401204da1f7b0e34e58a6055a0cc813fb
Ulli Hafner <ullrich.hafner@gmail.com>
2018-09-23 18:36:03 +0200

    Improved tests for DryTableModel.

--- src/test/java/io/jenkins/plugins/analysis/warnings/DryTableModelTest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/DryTableModelTest.java
@@ -1,73 +1,82 @@
 package io.jenkins.plugins.analysis.warnings;
 
+import java.util.List;
+import java.util.Locale;
+
 import org.junit.jupiter.api.Test;
 
+import edu.hm.hafner.analysis.Issue;
+import edu.hm.hafner.analysis.IssueBuilder;
+import edu.hm.hafner.analysis.Report;
+import edu.hm.hafner.analysis.parser.dry.DuplicationGroup;
+import io.jenkins.plugins.analysis.core.model.DescriptionProvider;
+import io.jenkins.plugins.analysis.core.model.DetailsTableModel;
+import io.jenkins.plugins.analysis.core.model.FileNameRenderer;
+import io.jenkins.plugins.analysis.core.model.FileNameRenderer.BuildFolderFacade;
+import io.jenkins.plugins.analysis.core.model.StaticAnalysisLabelProvider.DefaultAgeBuilder;
 import io.jenkins.plugins.analysis.warnings.DuplicateCodeScanner.DryTableModel;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 /**
  * Tests the class {@link DryTableModel}.
  *
  * @author Ullrich Hafner
  */
-// FIXME: enable using the new model
 class DryTableModelTest {
-    private static final int EXPECTED_NUMBER_OF_COLUMNS = 6;
-    private static final String EMPTY_DETAILS = "<div class=\"details-control\" data-description=\"&lt;p&gt;&lt;strong&gt;&lt;/strong&gt;&lt;/p&gt; description\"></div>";
+    private static final String DESCRIPTION = "DESCRIPTION";
 
     @Test
     void shouldConvertIssueToArrayOfColumns() {
-//        IssueBuilder builder = new IssueBuilder();
-//        builder.setReference("1");
-//        DuplicationGroup group = new DuplicationGroup();
-//        Issue issue = builder.setFileName("/path/to/file-1")
-//                .setLineStart(15)
-//                .setLineEnd(29)
-//                .setAdditionalProperties(group)
-//                .build();
-//        Issue duplicate = builder.setFileName("/path/to/file-2")
-//                .setLineStart(5)
-//                .setLineEnd(19)
-//                .setAdditionalProperties(group)
-//                .build();
-//
-//        group.add(issue);
-//        group.add(duplicate);
-//
-//        DryTableModel model = new DryTableModel();
-//        Report report = mock(Report.class);
-//
-//        BuildFolderFacade buildFolder = mock(BuildFolderFacade.class);
-//        when(buildFolder.canAccessAffectedFileOf(any())).thenReturn(true);
-//        FileNameRenderer fileNameRenderer = new FileNameRenderer(buildFolder);
-//
-//        List<String> firstColumns = model.getRow(report, issue, String::valueOf, fileNameRenderer, "description");
-//        assertThatJson(firstColumns).isArray().ofLength(EXPECTED_NUMBER_OF_COLUMNS);
-//
-//        assertThat(firstColumns.get(0)).isEqualTo(EMPTY_DETAILS);
-//        assertThat(firstColumns.get(1)).matches(createFileLinkMatcher("file-1", 15));
-//        assertThat(firstColumns.get(2)).isEqualTo("<a href=\"NORMAL\">Normal</a>");
-//        assertThat(firstColumns.get(3)).isEqualTo("15");
-//        assertThat(firstColumns.get(4)).matches(createLinkMatcher("file-2", 5));
-//        assertThat(firstColumns.get(5)).isEqualTo("1");
-//
-//        List<String> secondColumns = model.getRow(report, duplicate, String::valueOf, fileNameRenderer, "description");
-//        assertThatJson(secondColumns).isArray().ofLength(EXPECTED_NUMBER_OF_COLUMNS);
-//
-//        assertThat(firstColumns.get(0)).isEqualTo(EMPTY_DETAILS);
-//        assertThat(secondColumns.get(1)).matches(createFileLinkMatcher("file-2", 5));
-//        assertThat(secondColumns.get(2)).isEqualTo("<a href=\"NORMAL\">Normal</a>");
-//        assertThat(secondColumns.get(3)).isEqualTo("15");
-//        assertThat(secondColumns.get(4)).matches(createLinkMatcher("file-1", 15));
-//        assertThat(secondColumns.get(5)).isEqualTo("1");
-    }
+        Locale.setDefault(Locale.ENGLISH);
 
-    private static String createFileLinkMatcher(final String fileName, final int lineNumber) {
-        return "<a href=\\\"source.[0-9a-f-]+/#" + lineNumber + "\\\">"
-                + fileName + ":" + lineNumber
-                + "</a>";
-    }
+        DetailsTableModel model = createModel();
 
-    private static String createLinkMatcher(final String fileName, final int lineNumber) {
-        return String.format("<ul><li>%s</li></ul>", createFileLinkMatcher(fileName, lineNumber));
+        IssueBuilder builder = new IssueBuilder();
+        builder.setReference("1");
+        DuplicationGroup group = new DuplicationGroup();
+        Issue issue = builder.setFileName("/path/to/file-1")
+                .setLineStart(10)
+                .setLineEnd(24)
+                .setAdditionalProperties(group)
+                .build();
+        Issue duplicate = builder.setFileName("/path/to/file-2")
+                .setLineStart(5)
+                .setLineEnd(19)
+                .setAdditionalProperties(group)
+                .build();
+
+        group.add(issue);
+        group.add(duplicate);
+
+        Report report = new Report();
+        report.add(issue).add(duplicate);
+        
+        assertThat(model.getHeaders(report)).hasSize(6);
+        assertThat(model.getWidths(report)).hasSize(6);
+        List<List<String>> rows = model.getContent(report);
+        assertThat(rows).hasSize(2);
+
+        List<String> columns = rows.get(0);
+        assertThat(columns).hasSize(6);
+        assertThat(columns.get(0)).contains(DESCRIPTION);
+        assertThat(columns.get(1)).contains("file-1:10").contains(issue.getId().toString());
+        assertThat(columns.get(2)).isEqualTo("<a href=\"NORMAL\">Normal</a>");
+        assertThat(columns.get(3)).isEqualTo("15");
+        assertThat(columns.get(4)).contains("file-2:5").contains(duplicate.getId().toString());
+        assertThat(columns.get(5)).isEqualTo("1");
+   }
+
+    private DetailsTableModel createModel() {
+        DescriptionProvider descriptionProvider = mock(DescriptionProvider.class);
+        when(descriptionProvider.getDescription(any())).thenReturn(DESCRIPTION);
+        BuildFolderFacade buildFolder = mock(BuildFolderFacade.class);
+        when(buildFolder.canAccessAffectedFileOf(any())).thenReturn(true);
+        FileNameRenderer fileNameRenderer = new FileNameRenderer(buildFolder);
+
+        DefaultAgeBuilder ageBuilder = new DefaultAgeBuilder(1, "url");
+
+        return new DryTableModel(ageBuilder, fileNameRenderer, descriptionProvider);
     }
 }
