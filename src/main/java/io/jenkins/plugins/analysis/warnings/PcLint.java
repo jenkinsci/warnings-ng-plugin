ffe2d2d52a0a6d4b8e5443d59454f7accf611fb8
Ulli Hafner <ullrich.hafner@gmail.com>
2018-10-29 20:58:35 +0100

    Added PC-Lint (reuses MsBuild parser).

---
+++ src/main/java/io/jenkins/plugins/analysis/warnings/PcLint.java
@@ -0,0 +1,47 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import javax.annotation.Nonnull;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import edu.hm.hafner.analysis.parser.MsBuildParser;
+import io.jenkins.plugins.analysis.core.model.StaticAnalysisTool;
+
+import hudson.Extension;
+
+/**
+ * Provides a parser and customized messages for the PC-Lint Tool.
+ *
+ * @author Ullrich Hafner
+ */
+public class PcLint extends StaticAnalysisTool {
+    private static final long serialVersionUID = -6022797743536264094L;
+    static final String ID = "pclint";
+
+    /** Creates a new instance of {@link PcLint}. */
+    @DataBoundConstructor
+    public PcLint() {
+        super();
+        // empty constructor required for stapler
+    }
+
+    @Override
+    public MsBuildParser createParser() {
+        return new MsBuildParser();
+    }
+
+    /** Descriptor for this static analysis tool. */
+    @Extension
+    public static class Descriptor extends StaticAnalysisToolDescriptor {
+        /** Creates the descriptor instance. */
+        public Descriptor() {
+            super(ID);
+        }
+
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return Messages.Warnings_PCLint_ParserName();
+        }
+    }
+}
