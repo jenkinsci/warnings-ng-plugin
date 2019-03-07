197fa8bdc3d4f879217901bb13370c0f90e5bc9b
Bassam Khouri <bassam.khouri@cenx.com>
2019-03-07 00:45:44 -0500

    [JENKINS-56418] Add custom rflint icon
    Change Description:
    When RfLint parser is used, the Jenkins build URL does not display a Robot
    Framework logo on the Jenkins Build URL page, instead, an "Attention" icon is
    displayed.  This change ensure the Robot Framework logo is used when the
    respective parser
    
    Testing Done:
    1. Spun up a local Jenkins instance
    2. Installed the warning-ng plugin by uploading the HPI file via the Jenkins
       UI
    3. Create a freestyle project that uses the flint parser
    4. Triggered a build and ensured the UI is displaying the logo correctly

--- src/main/java/io/jenkins/plugins/analysis/warnings/RfLint.java
+++ src/main/java/io/jenkins/plugins/analysis/warnings/RfLint.java
@@ -3,6 +3,8 @@ package io.jenkins.plugins.analysis.warnings;
 import edu.hm.hafner.analysis.parser.RfLintParser;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
+import io.jenkins.plugins.analysis.core.model.IconLabelProvider;
+import io.jenkins.plugins.analysis.core.model.StaticAnalysisLabelProvider;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.jenkinsci.Symbol;
 import hudson.Extension;
@@ -44,5 +46,10 @@ public class RfLint extends ReportScanningTool {
         public String getDisplayName() {
             return Messages.Warnings_RFLint_ParserName();
         }
+
+        public StaticAnalysisLabelProvider getLabelProvider() {
+            return new IconLabelProvider(getId(), getDisplayName());
+        }
+
     }
 }
