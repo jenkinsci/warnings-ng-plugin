2d3c402fc477fea0b0e95ea5ba0ecc3e633fb8e9
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-07 16:16:01 +0100

    Added help to code warrior.

--- src/main/java/io/jenkins/plugins/analysis/warnings/MetrowerksCodeWarrior.java
+++ src/main/java/io/jenkins/plugins/analysis/warnings/MetrowerksCodeWarrior.java
@@ -48,6 +48,17 @@ public class MetrowerksCodeWarrior extends ReportScanningToolSuite {
         public String getDisplayName() {
             return Messages.Warnings_MetrowerksCodeWarrior_ParserName();
         }
+
+        @Override
+        public String getHelp() {
+            return "<p><p>Ensure that the output from the CodeWarrior build tools is in the expected format. "
+                    + "If there are warnings present, but they are not found, then it is likely that the format is incorrect. "
+                    + "The mwccarm compiler and mwldarm linker tools may support a configurable message style. "
+                    + "This can be used to enforce the expected output format, which may be different from Metrowerks "
+                    + "CodeWarrior (and thus require a different tool). For example the following could be appended to "
+                    + "the build flags:</p>"
+                    + "<p><code>-msgstyle gcc -nowraplines</code></p></p>";
+        }
     }
 }
 
