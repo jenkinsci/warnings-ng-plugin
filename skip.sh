fa616ee96ff81bf8d8616781b4e9c155a3a78b3e
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-11 10:21:44 +0100

    Fixed startup scripts.

--- skip.sh
+++ skip.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ -z "$JENKINS_HOME" ]]; then
-    JENKINS_HOME=../jenkins-home
+    JENKINS_HOME=../docker/volumes/jenkins-home
     echo "JENKINS_HOME is not defined, using $JENKINS_HOME"
 fi
 
 mvn install -DskipTests || { echo "Build failed"; exit 1; }
 
-echo "Installing plugin in JENKINS_HOME"
+echo "Installing plugin in $JENKINS_HOME"
 rm -rf $JENKINS_HOME/plugins/warnings-ng*
 cp -fv target/warnings-ng.hpi $JENKINS_HOME/plugins/warnings-ng.jpi
 
-IS_RUNNING=`docker-compose ps -q jenkins`
+IS_RUNNING=`docker-compose ps -q jenkins-master`
 if [[ "$IS_RUNNING" != "" ]]; then
     echo "Restarting Jenkins..."
     docker-compose restart
