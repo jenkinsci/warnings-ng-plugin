cba4f79701245a08979b9ff375dfbf70c6732bba
Ulli Hafner <ullrich.hafner@gmail.com>
2019-03-13 16:08:15 +0100

    Moved no-prototype code into separate file.

--- src/main/webapp/js/issues-detail.js
+++ src/main/webapp/js/issues-detail.js
@@ -1,28 +1,4 @@
 (function ($) {
-    /**
-     * Solves Bootstrap and Prototype.js conflict.
-     *
-     * @link http://jsfiddle.net/dgervalle/hhBc6/
-     * @link http://www.softec.lu/site/DevelopersCorner/BootstrapPrototypeConflict
-     */
-    jQuery.noConflict();
-    if (Prototype.BrowserFeatures.ElementExtensions) {
-        var disablePrototypeJS = function (method, pluginsToDisable) {
-                var handler = function (event) {
-                    event.target[method] = undefined;
-                    setTimeout(function () {
-                        delete event.target[method];
-                    }, 0);
-                };
-                pluginsToDisable.each(function (plugin) {
-                    jQuery(window).on(method + '.bs.' + plugin, handler);
-                });
-            },
-            pluginsToDisable = ['collapse', 'dropdown', 'modal', 'tooltip', 'popover', 'tab'];
-        disablePrototypeJS('show', pluginsToDisable);
-        disablePrototypeJS('hide', pluginsToDisable);
-    }
-
     $.fn.extend({
         /**
          * Renders a trend chart in the specified div using ECharts.
