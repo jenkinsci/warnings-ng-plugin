61409319185a348b20f9ea1018bfffef23f74b57
RedTo <huber124@hm.edu>
2019-04-02 21:45:55 +0200

    added TabLabelProviderTests with 8 default tests

---
+++ src/test/java/io/jenkins/plugins/analysis/core/model/TabLabelProviderTest.java
@@ -0,0 +1,83 @@
+package io.jenkins.plugins.analysis.core.model;
+
+import org.junit.jupiter.api.Test;
+
+import edu.hm.hafner.analysis.Issue;
+import edu.hm.hafner.analysis.Report;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests the class {@link TabLabelProvider}.
+ *
+ * @author Tobias Redl
+ */
+class TabLabelProviderTest {
+
+    private TabLabelProvider createTabLabelProvider(final String fileName) {
+        Issue issue = mock(Issue.class);
+        when(issue.getFileName()).thenReturn(fileName);
+
+        Report report = new Report();
+        report.add(issue);
+
+        return new TabLabelProvider(report);
+    }
+
+    @Test
+    void shouldReturnPackageNameJava() {
+        TabLabelProvider tabLabelProvider = createTabLabelProvider("Testfile.java");
+
+        assertEquals(tabLabelProvider.getPackageName(), Messages.Tab_Package());
+    }
+
+    @Test
+    void shouldReturnPackageNamePython() {
+        TabLabelProvider tabLabelProvider = createTabLabelProvider("Testfile.py");
+
+        assertEquals(tabLabelProvider.getPackageName(), Messages.Tab_Package());
+    }
+
+    @Test
+    void shouldReturnPackageNameCS() {
+        TabLabelProvider tabLabelProvider = createTabLabelProvider("Testfile.cs");
+
+        assertEquals(tabLabelProvider.getPackageName(), Messages.Tab_Namespace());
+    }
+
+    @Test
+    void shouldReturnPackageNameFallback() {
+        TabLabelProvider tabLabelProvider = createTabLabelProvider("Test.file");
+
+        assertEquals(tabLabelProvider.getPackageName(), Messages.Tab_Folder());
+    }
+
+    @Test
+    void shouldReturnPackagesJava() {
+        TabLabelProvider tabLabelProvider = createTabLabelProvider("Testfile.java");
+
+        assertEquals(tabLabelProvider.getPackages(), Messages.Tab_Packages());
+    }
+
+    @Test
+    void shouldReturnPackagesPython() {
+        TabLabelProvider tabLabelProvider = createTabLabelProvider("Testfile.py");
+
+        assertEquals(tabLabelProvider.getPackages(), Messages.Tab_Packages());
+    }
+
+    @Test
+    void shouldReturnPackagesCS() {
+        TabLabelProvider tabLabelProvider = createTabLabelProvider("Testfile.cs");
+
+        assertEquals(tabLabelProvider.getPackages(), Messages.Tab_Namespaces());
+    }
+
+    @Test
+    void shouldReturnPackagesFallback() {
+        TabLabelProvider tabLabelProvider = createTabLabelProvider("Test.file");
+
+        assertEquals(tabLabelProvider.getPackages(), Messages.Tab_Folders());
+    }
+}
