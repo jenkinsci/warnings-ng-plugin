30839a42fd2b77e06eb34921579aa91d9bd41355
osboxes.org <matthias.herpers@gmail.com>
2019-04-02 09:57:06 +0200

    files2

--- src/test/java/io/jenkins/plugins/analysis/core/charts/HealthTrendChartTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/charts/HealthTrendChartTest.java
@@ -1,4 +1,20 @@
 package io.jenkins.plugins.analysis.core.charts;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the class {@link HealthTrendChart}.
+ *
+ * @author Matthias Herpers
+ */
 
 public class HealthTrendChartTest {
+
+    @Test
+    public void creatorTestWithNull(){
+        HealthTrendChart hTC = new HealthTrendChart(null);
+        hTC.create(null,null);
+        Assert.assertTrue(true);
+
+    }
 }
--- src/test/java/io/jenkins/plugins/analysis/core/charts/SeverityPieChartTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/charts/SeverityPieChartTest.java
@@ -1,4 +1,20 @@
 package io.jenkins.plugins.analysis.core.charts;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+/**
+ * Tests the class {@link SeverityPieChart}.
+ *
+ * @author Matthias Herpers
+ */
+
 public class SeverityPieChartTest {
+
+
+    @Test
+    public void creatorTestWithNull(){
+        SeverityPieChart sPC = new SeverityPieChart();
+        sPC.create(null);
+        Assert.assertTrue(true);
+    }
 }
