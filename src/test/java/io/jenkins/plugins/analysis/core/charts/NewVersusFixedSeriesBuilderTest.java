3b0d5960b6375095b1478088047fef5cd8e82e5b
Vroni22 <vz@accu-rate.de>
2019-04-03 20:41:29 +0200

    Created test class for NewVersusFixedSeriesBuilder

---
+++ src/test/java/io/jenkins/plugins/analysis/core/charts/NewVersusFixedSeriesBuilderTest.java
@@ -0,0 +1,16 @@
+package io.jenkins.plugins.analysis.core.charts;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the class {@link NewVersusFixedSeriesBuilder}.
+ *
+ * @author Ullrich Hafner
+ */
+class NewVersusFixedSeriesBuilderTest {
+
+
+    @Test
+    void computeSeries() {
+    }
+}
