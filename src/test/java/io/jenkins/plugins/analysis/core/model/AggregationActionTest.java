6f245e1e2f941b95a13b183768fccdb16b899cc8
Ulli Hafner <ullrich.hafner@gmail.com>
2019-04-15 21:54:23 +0200

    Simplified tests.

--- src/test/java/io/jenkins/plugins/analysis/core/model/AggregationActionTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/model/AggregationActionTest.java
@@ -14,6 +14,7 @@ import hudson.model.Run;
 import io.jenkins.plugins.analysis.core.restapi.AggregationApi;
 import io.jenkins.plugins.analysis.core.restapi.ToolApi;
 import io.jenkins.plugins.analysis.core.testutil.JobStubs;
+import io.jenkins.plugins.analysis.core.testutil.SoftAssertions;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -24,51 +25,27 @@ import static org.mockito.Mockito.*;
  * @author Florian Hageneder
  */
 class AggregationActionTest {
+    private static final int SIZE = 1;
 
     @Test
     void shouldNotReturnIconFileName() {
-        AggregationAction sut = new AggregationAction();
+        AggregationAction action = new AggregationAction();
 
-        String fileName = sut.getIconFileName();
-
-        assertThat(fileName).isNull();
-    }
-
-    @Test
-    void shouldReturnDisplayName() {
-        AggregationAction sut = new AggregationAction();
-
-        String displayName = sut.getDisplayName();
-
-        assertThat(displayName).isEqualTo(Messages.Aggregation_Name());
-    }
-
-    @Test
-    void shouldReturnUrlName() {
-        AggregationAction sut = new AggregationAction();
-
-        String url = sut.getUrlName();
-
-        assertThat(url).isEqualTo("warnings-ng");
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(action.getIconFileName()).isNull();
+            softly.assertThat(action.getDisplayName()).isEqualTo(Messages.Aggregation_Name());
+            softly.assertThat(action.getUrlName()).isEqualTo("warnings-ng");
+        });
     }
 
     @Test
     void shouldNeverReturnMultipleProjectActions() {
-        AggregationAction sut = new AggregationAction();
-        sut.onLoad(mock(Run.class));
+        AggregationAction action = new AggregationAction();
+        action.onLoad(mock(Run.class));
 
-        Collection<? extends Action> projectActions = sut.getProjectActions();
+        Collection<? extends Action> projectActions = action.getProjectActions();
 
         assertThat(projectActions).hasSize(1);
-    }
-
-    @Test
-    void shouldReturnAggregatedTrendActionAsProjectActions() {
-        AggregationAction sut = new AggregationAction();
-        sut.onAttached(mock(Run.class));
-
-        Collection<? extends Action> projectActions = sut.getProjectActions();
-
         assertThat(projectActions.iterator().next()).isInstanceOf(AggregatedTrendAction.class);
     }
 
@@ -76,11 +53,11 @@ class AggregationActionTest {
     void shouldReturnAggregationApi() {
         Run<?, ?> owner = mock(Run.class);
         when(owner.getActions(any())).thenReturn(Collections.emptyList());
-        AggregationAction sut = new AggregationAction();
-        sut.onAttached(owner);
 
-        Api api = sut.getApi();
+        AggregationAction action = new AggregationAction();
+        action.onAttached(owner);
 
+        Api api = action.getApi();
         assertThat(api.bean).isInstanceOf(AggregationApi.class);
     }
 
@@ -88,36 +65,24 @@ class AggregationActionTest {
     void shouldCreateCompleteApi() {
         Run<?, ?> owner = mock(Run.class);
         List<ResultAction> actions = Lists.fixedSize.of(
-                createAction(JobStubs.SPOT_BUGS_ID, JobStubs.SPOT_BUGS_NAME, 42),
-                createAction(JobStubs.CHECK_STYLE_NAME, JobStubs.CHECK_STYLE_NAME, 42)
+                createAction(JobStubs.SPOT_BUGS_ID, JobStubs.SPOT_BUGS_NAME, SIZE),
+                createAction(JobStubs.CHECK_STYLE_NAME, JobStubs.CHECK_STYLE_NAME, SIZE)
         );
         when(owner.getActions(any())).thenAnswer(i -> actions);
-        AggregationAction sut = new AggregationAction();
-        sut.onLoad(owner);
+        AggregationAction action = new AggregationAction();
+        action.onLoad(owner);
 
-        Api api = sut.getApi();
+        Api api = action.getApi();
         AggregationApi aggregationApi = (AggregationApi) api.bean;
 
         assertThat(aggregationApi.getTools()).hasSize(2);
-    }
 
-    @Test
-    void shouldReturnAllUsedTools() {
-        Run<?, ?> owner = mock(Run.class);
-        List<ResultAction> actions = Lists.fixedSize.of(
-                createAction(JobStubs.SPOT_BUGS_ID, JobStubs.SPOT_BUGS_NAME, 42),
-                createAction(JobStubs.CHECK_STYLE_NAME, JobStubs.CHECK_STYLE_NAME, 69)
-        );
-        when(owner.getActions(any())).thenAnswer(i -> actions);
-        AggregationAction sut = new AggregationAction();
-        sut.onLoad(owner);
-
-        List<ToolApi> actually = sut.getTools();
+        List<ToolApi> actually = action.getTools();
 
         assertThat(actually).hasSize(2);
     }
 
-    ResultAction createAction(final String id, final String name, final int size) {
+    private ResultAction createAction(final String id, final String name, final int size) {
         ResultAction resultAction = mock(ResultAction.class);
 
         AnalysisResult result = mock(AnalysisResult.class);
@@ -131,5 +96,4 @@ class AggregationActionTest {
 
         return resultAction;
     }
-
 }
