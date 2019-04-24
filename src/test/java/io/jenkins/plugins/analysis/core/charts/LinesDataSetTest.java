72ad935a959d245842d5dbf5b9656f6bcdf19c3f
Ulli Hafner <ullrich.hafner@gmail.com>
2019-04-24 23:40:26 +0200

    Simplified test.

--- src/test/java/io/jenkins/plugins/analysis/core/charts/LinesDataSetTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/charts/LinesDataSetTest.java
@@ -4,9 +4,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.collections.impl.factory.Maps;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 /**
@@ -15,76 +14,57 @@ import static org.assertj.core.api.Assertions.*;
  * @author Lorenz Munsch
  */
 class LinesDataSetTest {
-
-    private static final String TESTLABEL = "Testlabel";
-
-    private Map<String, Integer> getdataSetSeriesMap() {
-        Map<String, Integer>  dataSetSeries = new HashMap<>();
-        dataSetSeries.put("Eins", 1);
-        dataSetSeries.put("Zwei", 2);
-        dataSetSeries.put("Drei", 3);
-        dataSetSeries.put("Vier", 4);
-
-        return dataSetSeries;
-    }
+    private static final String X_AXIS_LABEL = "X_AXIS_LABEL";
+    private static final String FIRST_DATA_SET = "Eins";
+    private static final String SECOND_DATA_SET = "Zwei";
 
     @Test
-    void shouldGetXAxisSize() {
+    void shouldAddAndVerifySeries() {
         LinesDataSet linesDataSet  = new LinesDataSet();
         assertThat(linesDataSet.getXAxisSize()).isEqualTo(0);
+        assertThat(linesDataSet.hasSeries(FIRST_DATA_SET)).isFalse();
 
-        Map<String, Integer>  dataSetSeries = new HashMap<>();
-        linesDataSet.add(TESTLABEL, dataSetSeries);
+        linesDataSet.add(X_AXIS_LABEL, createSeries());
         assertThat(linesDataSet.getXAxisSize()).isEqualTo(1);
-        assertThat(linesDataSet.getXAxisLabels()).containsOnly("Testlabel");
+        assertThat(linesDataSet.getXAxisLabels()).containsOnly(X_AXIS_LABEL);
+        assertThat(linesDataSet.getDataSetIds()).containsOnlyOnce(FIRST_DATA_SET, SECOND_DATA_SET);
+
+        assertThat(linesDataSet.hasSeries(FIRST_DATA_SET)).isTrue();
+        assertThat(linesDataSet.getSeries(FIRST_DATA_SET)).containsOnly(1);
+
+        assertThat(linesDataSet.hasSeries(SECOND_DATA_SET)).isTrue();
+        assertThat(linesDataSet.getSeries(SECOND_DATA_SET)).containsOnly(2);
     }
 
     @Test
-    void shouldGetDataSetIds() {
+    void shouldThrowExceptionIfSeriesDoesNotExist() {
         LinesDataSet linesDataSet  = new LinesDataSet();
-        Map<String, Integer>  dataSetSeries = getdataSetSeriesMap();
-        linesDataSet.add(TESTLABEL, dataSetSeries);
-        assertThat(linesDataSet.getDataSetIds())
-                .hasSize(4)
-                .contains("Eins")
-                .contains("Zwei")
-                .contains("Drei")
-                .contains("Vier");
-    }
 
-    @Test
-    void shouldHasSeries() {
-        LinesDataSet linesDataSet  = new LinesDataSet();
-        assertThat(linesDataSet.hasSeries("Eins")).isEqualTo(false);
-        Map<String, Integer>  dataSetSeries = getdataSetSeriesMap();
-        linesDataSet.add(TESTLABEL, dataSetSeries);
-        assertThat(linesDataSet.hasSeries("Eins")).isEqualTo(true);
-        assertThat(linesDataSet.getSeries("Eins")).isEqualTo(Collections.singletonList(1));
-    }
+        linesDataSet.add(X_AXIS_LABEL, createSeries());
 
-    @Test
-    void shouldGetSeriesWithMissingEntry() {
-        LinesDataSet linesDataSet  = new LinesDataSet();
-        assertThat(linesDataSet.hasSeries("Eins")).isEqualTo(false);
-        Map<String, Integer>  dataSetSeries = getdataSetSeriesMap();
-        linesDataSet.add(TESTLABEL, dataSetSeries);
-
-        assertThatThrownBy(() -> linesDataSet.getSeries("Test"))
+        assertThatThrownBy(() -> linesDataSet.getSeries("WrongId"))
                 .isInstanceOf(AssertionError.class)
                 .hasMessageContaining("No dataset")
                 .hasMessageContaining("registered");
     }
 
     @Test
-    void shouldAddWithException() {
+    void shouldThrowExceptionIfSameLabelIsAddedTwice() {
         LinesDataSet linesDataSet  = new LinesDataSet();
-        assertThat(linesDataSet.hasSeries("Eins")).isEqualTo(false);
         Map<String, Integer>  dataSetSeries = Collections.emptyMap();
-        linesDataSet.add(TESTLABEL, dataSetSeries);
 
-        assertThatThrownBy(() -> linesDataSet.add("Testlabel", dataSetSeries))
+        linesDataSet.add(X_AXIS_LABEL, dataSetSeries);
+        assertThatThrownBy(() -> linesDataSet.add(X_AXIS_LABEL, dataSetSeries))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("Label already registered:");
+    }
+
+    private Map<String, Integer> createSeries() {
+        Map<String, Integer> dataSetSeries = new HashMap<>();
+        dataSetSeries.put(FIRST_DATA_SET, 1);
+        dataSetSeries.put(SECOND_DATA_SET, 2);
+
+        return dataSetSeries;
     }
 
 }
