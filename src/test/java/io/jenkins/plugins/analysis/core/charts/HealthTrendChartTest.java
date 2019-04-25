95c5d452345377d57a532d2aa5052e965c28bce6
Ulli Hafner <ullrich.hafner@gmail.com>
2019-04-25 21:02:04 +0200

    Fixed wrong values in tests.

--- src/test/java/io/jenkins/plugins/analysis/core/charts/HealthTrendChartTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/charts/HealthTrendChartTest.java
@@ -22,50 +22,67 @@ import static org.mockito.Mockito.*;
  * @author Matthias Herpers
  */
 class HealthTrendChartTest {
-    /**
-     * Verifies that a HealthTrendChart with two Results shows right x/Axis labels.
-     */
     @Test
-    void shouldCreateHealthChartForJobAndMultipleActions() {
+    void shouldCreateHealthChart() {
         HealthDescriptor healthDescriptor = new HealthDescriptor(5, 10, Severity.WARNING_NORMAL);
         HealthTrendChart chart = new HealthTrendChart(healthDescriptor);
 
-        List<AnalysisBuildResult> resultsCheckStyle = new ArrayList<>();
-        resultsCheckStyle.add(createResult(0, 0, 0, 1));
-        resultsCheckStyle.add(createResult(0, 0, 10, 2));
-        resultsCheckStyle.add(createResult(0, 3, 0, 3));
-        resultsCheckStyle.add(createResult(0, 7, 0, 4));
-        resultsCheckStyle.add(createResult(11, 0, 0, 5));
-        resultsCheckStyle.add(createResult(200, 0, 0, 6));
+        List<AnalysisBuildResult> resultsCheckStyle = createBuildResults();
         LinesChartModel model = chart.create(resultsCheckStyle, new ChartModelConfiguration());
 
         assertThatJson(model).node("xAxisLabels")
-                .isArray().hasSize(6).containsExactly("#1", "#2", "#3", "#4", "#5", "#6");
+                .isArray().containsExactly("#1", "#2", "#3", "#4");
         assertThatJson(model).node("series")
                 .isArray().hasSize(3);
         assertThat(healthDescriptor.isEnabled()).isTrue();
         assertThat(model.getSeries().get(0).getName()).isEqualTo("Excellent");
-        verifySeries(model, 0, 0, 0, 3, 5, 5, 5);
+        verifySeries(model, 0, 0, 5, 5, 5);
         assertThat(model.getSeries().get(1).getName()).isEqualTo("Satisfactory");
-        verifySeries(model, 1, 0, 0, 0, 2, 5, 5);
+        verifySeries(model, 1, 0, 0, 5, 5);
         assertThat(model.getSeries().get(2).getName()).isEqualTo("Failing");
-        verifySeries(model, 2, 0, 0, 0, 0, 1, 190);
+        verifySeries(model, 2, 0, 0, 0, 5);
+    }
+
+    @Test
+    void shouldCreateFallbackChartIfHealthIsDisabled() {
+        HealthDescriptor healthDescriptor = new HealthDescriptor(-1, -1, Severity.WARNING_NORMAL);
+        HealthTrendChart chart = new HealthTrendChart(healthDescriptor);
+
+        List<AnalysisBuildResult> resultsCheckStyle = createBuildResults();
+        LinesChartModel model = chart.create(resultsCheckStyle, new ChartModelConfiguration());
+
+        assertThatJson(model).node("xAxisLabels")
+                .isArray().containsExactly("#1", "#2", "#3", "#4");
+        assertThatJson(model).node("series")
+                .isArray().hasSize(1);
+        assertThat(healthDescriptor.isEnabled()).isFalse();
+        assertThat(model.getSeries().get(0).getName()).isEqualTo("Total (health report disabled)");
+        verifySeries(model, 0, 0, 5, 10, 15);
+    }
+
+    private List<AnalysisBuildResult> createBuildResults() {
+        List<AnalysisBuildResult> resultsCheckStyle = new ArrayList<>();
+        resultsCheckStyle.add(createResult(0, 0, 0, 1));
+        resultsCheckStyle.add(createResult(5, 0, 0, 2));
+        resultsCheckStyle.add(createResult(5, 5, 0, 3));
+        resultsCheckStyle.add(createResult(5, 5, 5, 4));
+        return resultsCheckStyle;
     }
 
     private AnalysisBuildResult createResult(final int high, final int normal, final int low, final int number) {
         AnalysisBuildResult buildResult = mock(AnalysisBuildResult.class);
 
+        when(buildResult.getTotalSize()).thenReturn(high + normal + low);
         when(buildResult.getTotalSizeOf(Severity.WARNING_HIGH)).thenReturn(high);
         when(buildResult.getTotalSizeOf(Severity.WARNING_NORMAL)).thenReturn(normal);
         when(buildResult.getTotalSizeOf(Severity.WARNING_LOW)).thenReturn(low);
 
         AnalysisBuild build = new BuildProperties(number, "#" + number, 10);
         when(buildResult.getBuild()).thenReturn(build);
-        when(buildResult.getTotalSize()).thenReturn(high + normal);
         return buildResult;
     }
 
-    private void verifySeries(LinesChartModel model, int index, final int... numbers) {
+    private void verifySeries(final LinesChartModel model, final int index, final int... numbers) {
         for (int i = 0; i < numbers.length; i++) {
             assertThat(model.getSeries().get(index).getData().get(i)).isEqualTo(numbers[i]);
         }
