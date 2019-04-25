18d43cabaffbe38c29233995456cef7d89ffd0b9
osboxes.org <matthias.herpers@gmail.com>
2019-04-25 10:58:43 +0200

    Different kind of Asserting

--- src/test/java/io/jenkins/plugins/analysis/core/charts/SeverityPieChartTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/charts/SeverityPieChartTest.java
@@ -11,77 +11,71 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 /**
- * Tests the class {@link SeverityPieChart}.
- *
+ * Tests the class {@link SeverityPieChart}
  * @author Matthias Herpers
  */
 class SeverityPieChartTest {
-
     /**
      *  Verifies that a SeverityPieChart with empty report shows only predefined severities without Severity.ERROR.
      */
     @Test
     void shouldCreateChartFromEmptyReport() {
-
         SeverityPieChart severityPieChart = new SeverityPieChart();
-        int numberOfDefaultPieData = Severity.getPredefinedValues().size() - 1; // Without Severity.ERROR
         Report report = createReport(0, 0, 0, 0);
         PieChartModel pieChartModel = severityPieChart.create(report);
         List<PieData> data = pieChartModel.getData();
-        for (PieData severity : data) {
-            assertThat(Severity.getPredefinedValues().contains(severity));
-        }
-
-        assertThat(data.size()).isEqualTo(numberOfDefaultPieData);
+        assertThat(data.get(0).getName()).isEqualTo("High");// Without Severity.ERROR
+        assertThat(data.get(0).getValue()).isEqualTo(0);
+        assertThat(data.get(1).getName()).isEqualTo("Normal");
+        assertThat(data.get(1).getValue()).isEqualTo(0);
+        assertThat(data.get(2).getName()).isEqualTo("Low");
+        assertThat(data.get(2).getValue()).isEqualTo(0);
     }
-
 
     /**
      *  Verifies that a SeverityPieChart with one Error shows all Severities.
      */
     @Test
     void shouldCreateChartFromReportWithOneError() {
-
         SeverityPieChart severityPieChart = new SeverityPieChart();
-        int numberOfDefaultPieData = Severity.getPredefinedValues().size(); // With Severity.ERROR
         Report report = createReport(0, 0, 0, 1);
         PieChartModel pieChartModel = severityPieChart.create(report);
         List<PieData> data = pieChartModel.getData();
-        for (PieData severity : data) {
-            assertThat(Severity.getPredefinedValues().contains(severity));
-        }
-
-        assertThat(data.size()).isEqualTo(numberOfDefaultPieData);
+        assertThat(data.get(0).getName()).isEqualTo("Error");// With Severity.ERROR
+        assertThat(data.get(0).getValue()).isEqualTo(1);
+        assertThat(data.get(1).getName()).isEqualTo("High");
+        assertThat(data.get(1).getValue()).isEqualTo(0);
+        assertThat(data.get(2).getName()).isEqualTo("Normal");
+        assertThat(data.get(2).getValue()).isEqualTo(0);
+        assertThat(data.get(3).getName()).isEqualTo("Low");
+        assertThat(data.get(3).getValue()).isEqualTo(0);
     }
-
 
     /**
      *  Verifies that a SeverityPieChart with one of each severity shows all Severities.
      */
     @Test
     void shouldCreateChartFromSimpleReport() {
-
         SeverityPieChart severityPieChart = new SeverityPieChart();
-        int numberOfDefaultPieData = Severity.getPredefinedValues().size(); // With Severity.ERROR
         Report report = createReport(1, 1, 1, 1);
         PieChartModel pieChartModel = severityPieChart.create(report);
         List<PieData> data = pieChartModel.getData();
-        for (PieData severity : data) {
-            assertThat(Severity.getPredefinedValues().contains(severity));
-        }
-
-        assertThat(data.size()).isEqualTo(numberOfDefaultPieData);
+        assertThat(data.get(0).getName()).isEqualTo("Error");// With Severity.ERROR
+        assertThat(data.get(0).getValue()).isEqualTo(1);
+        assertThat(data.get(1).getName()).isEqualTo("High");
+        assertThat(data.get(1).getValue()).isEqualTo(1);
+        assertThat(data.get(2).getName()).isEqualTo("Normal");
+        assertThat(data.get(2).getValue()).isEqualTo(1);
+        assertThat(data.get(3).getName()).isEqualTo("Low");
+        assertThat(data.get(3).getValue()).isEqualTo(1);
     }
-
 
     private Report createReport(final int high, final int normal, final int low, final int error) {
         Report buildResult = mock(Report.class);
-
         when(buildResult.getSizeOf(Severity.WARNING_HIGH)).thenReturn(high);
         when(buildResult.getSizeOf(Severity.WARNING_NORMAL)).thenReturn(normal);
         when(buildResult.getSizeOf(Severity.WARNING_LOW)).thenReturn(low);
         when(buildResult.getSizeOf(Severity.ERROR)).thenReturn(error);
         return buildResult;
     }
-
 }
