a9dc8291281395c4caa369c22925dec0d077bbfe
Nils Engelbrecht <nils.engelbrecht@telefonica.com>
2019-05-01 23:15:04 +0200

    commit before merge

--- src/test/java/io/jenkins/plugins/analysis/warnings/recorder/FreestyleJobITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/FreestyleJobITest.java
@@ -9,6 +9,8 @@ import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 
 import io.jenkins.plugins.analysis.core.filter.ExcludeFile;
+import io.jenkins.plugins.analysis.core.filter.ExcludeModule;
+import io.jenkins.plugins.analysis.core.filter.ExcludePackage;
 import io.jenkins.plugins.analysis.core.model.AnalysisResult;
 import io.jenkins.plugins.analysis.core.steps.IssuesRecorder;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite;
@@ -93,5 +95,55 @@ public class FreestyleJobITest extends IntegrationTestWithJenkinsPerSuite {
                 "-> PASSED - Total number of issues (any severity): 5 - Quality QualityGate: 10",
                 "-> Some quality gates have been missed: overall result is WARNING");
         assertThat(result).hasQualityGateStatus(QualityGateStatus.WARNING);
+    }
+
+    @Test
+    public void shouldCreateFreestyleJobWithHealth0P() {
+        FreeStyleProject project = createFreeStyleJobWithWarnings("healthTestFile");
+
+        enableHealthReport(project);
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        assertThat(result).hasTotalSize(10);
+        assertThat(project.getBuildHealth().getScore()).isEqualTo(0);
+    }
+
+    @Test
+    public void shouldCreateFreestyleJobWithHealth90P() {
+        FreeStyleProject project = createFreeStyleJobWithWarnings("healthTestFile");
+
+        IssuesRecorder recorder = enableHealthReport(project);
+        recorder.setFilters(Collections.singletonList(new ExcludeFile(".*0.*")));
+
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        assertThat(result).hasTotalSize(9);
+        assertThat(project.getBuildHealth().getScore()).isEqualTo(10);
+    }
+
+    @Test
+    public void shouldCreateFreestyleJobWithHealth100P() {
+        FreeStyleProject project = createFreeStyleJobWithWarnings("healthTestFile");
+
+        IssuesRecorder recorder = enableHealthReport(project);
+        recorder.setFilters(Collections.singletonList(new ExcludeFile(".*.*")));
+
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        assertThat(result).hasTotalSize(0);
+        assertThat(project.getBuildHealth().getScore()).isEqualTo(100);
+    }
+
+    private IssuesRecorder enableHealthReport(final FreeStyleProject project) {
+        Java java = new Java();
+        java.setPattern("**/*.txt");
+        IssuesRecorder recorder = enableWarnings(project, java);
+        recorder.setHealthy(1);
+        recorder.setUnhealthy(9);
+        return recorder;
+        // recorder.setMinimumSeverity(Severity.WARNING.getName());
+    }
+
+    private FreeStyleProject createFreeStyleJobWithWarnings(final String fileName) {
+        FreeStyleProject project = createFreeStyleProject();
+        copySingleFileToWorkspace(project, "../" + fileName + ".txt", "java.txt");
+        return project;
     }
 }
