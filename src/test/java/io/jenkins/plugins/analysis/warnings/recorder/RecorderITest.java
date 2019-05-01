db5c5654734bac7c2b1b0fd6e30daa249299a070
osboxes.org <matthias.herpers@gmail.com>
2019-05-01 20:38:24 +0200

    added Info- and ErrorMessage assertions

--- src/test/java/io/jenkins/plugins/analysis/warnings/recorder/RecorderITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/RecorderITest.java
@@ -1,7 +1,13 @@
 package io.jenkins.plugins.analysis.warnings.recorder;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.impl.factory.Lists;
 import org.junit.Test;
 
+import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlNumberInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -18,23 +24,26 @@ import static io.jenkins.plugins.analysis.core.assertions.Assertions.*;
 
 public class RecorderITest extends IntegrationTestWithJenkinsPerSuite {
 
-    private void verifyHealthOfProjectWithWarnings(String file, int warnings, int health, boolean withHealthReport){
+    private void verifyHealthOfProjectWithWarnings(String file, int warnings, int health, boolean withHealthReport) {
         FreeStyleProject project = createFreeStyleProject();
-        copySingleFileToWorkspace(project,file, "javac.txt");
+        copySingleFileToWorkspace(project, file, "javac.txt");
         Java java = new Java();
         java.setPattern("**/*.txt");
         enableWarnings(project, java);
         if (withHealthReport) {
             configureHealthReport(project);
         }
-        AnalysisResult result = scheduleBuildAndAssertStatus(project,Result.SUCCESS);
-        //String buildnumber =String.valueOf(project.getLastBuild().getNumber());
-        //HtmlJenkinsInfo html = new HtmlJenkinsInfo(project, buildnumber+"/java/info");
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        String buildnumber = String.valueOf(project.getLastBuild().getNumber());
+        HtmlJenkinsJavaInfo html = new HtmlJenkinsJavaInfo(project, buildnumber + "/java/info");
 
         assertThat(result).hasTotalSize(warnings);
+        assertThat(result.getInfoMessages()).isEqualTo(html.getInfoMessages());
+        assertThat(result.getErrorMessages()).isEqualTo(html.getErrorMessages());
 
         if (withHealthReport) {
-            assertThat(result.getInfoMessages()).contains("Enabling health report (Healthy=1, Unhealthy=9, Minimum Severity=LOW)");
+            assertThat(result.getInfoMessages()).contains(
+                    "Enabling health report (Healthy=1, Unhealthy=9, Minimum Severity=LOW)");
             assertThat(project.getBuildHealth().getScore()).isEqualTo(health);
         }
         else {
@@ -43,87 +52,85 @@ public class RecorderITest extends IntegrationTestWithJenkinsPerSuite {
     }
 
     private void configureHealthReport(Project project) {
-        HtmlForm webForm = getWebPage(project,"configure").getFormByName("config");
-        ((HtmlNumberInput)webForm.getInputByName("_.healthy")).setText(String.valueOf(1));
-        ((HtmlNumberInput)webForm.getInputByName("_.unhealthy")).setText(String.valueOf(9));
+        HtmlForm webForm = getWebPage(project, "configure").getFormByName("config");
+        ((HtmlNumberInput) webForm.getInputByName("_.healthy")).setText(String.valueOf(1));
+        ((HtmlNumberInput) webForm.getInputByName("_.unhealthy")).setText(String.valueOf(9));
         submit(webForm);
     }
 
     @Test
     public void shouldCreateReportWithZeroWarnings() {
-        verifyHealthOfProjectWithWarnings("demoJavacZeroWarnings.txt",0,100,true);
+        verifyHealthOfProjectWithWarnings("demoJavacZeroWarnings.txt", 0, 100, true);
     }
 
     @Test
     public void shouldCreateReportWithOneWarning() {
-        verifyHealthOfProjectWithWarnings("demoJavacOneWarning.txt",1,90,true);
+        verifyHealthOfProjectWithWarnings("demoJavacOneWarning.txt", 1, 90, true);
     }
 
     @Test
     public void shouldCreateReportWithNineWarnings() {
-        verifyHealthOfProjectWithWarnings("demoJavacNineWarnings.txt",9,10,true);
+        verifyHealthOfProjectWithWarnings("demoJavacNineWarnings.txt", 9, 10, true);
     }
 
     @Test
     public void shouldCreateReportWithTenWarnings() {
-        verifyHealthOfProjectWithWarnings("demoJavacTenWarnings.txt",10,0,true);
+        verifyHealthOfProjectWithWarnings("demoJavacTenWarnings.txt", 10, 0, true);
     }
-
 
     @Test
     public void shouldCreateReportWithZeroWarningsWithDisabledHealthReport() {
-        verifyHealthOfProjectWithWarnings("demoJavacZeroWarnings.txt",0,100,false);
+        verifyHealthOfProjectWithWarnings("demoJavacZeroWarnings.txt", 0, 100, false);
     }
 
     @Test
     public void shouldCreateReportWithOneWarningWithDisabledHealthReport() {
-        verifyHealthOfProjectWithWarnings("demoJavacOneWarning.txt",1,100,false);
+        verifyHealthOfProjectWithWarnings("demoJavacOneWarning.txt", 1, 100, false);
     }
 
     @Test
     public void shouldCreateReportWithNineWarningsWithDisabledHealthReport() {
-        verifyHealthOfProjectWithWarnings("demoJavacNineWarnings.txt",9,100,false);
+        verifyHealthOfProjectWithWarnings("demoJavacNineWarnings.txt", 9, 100, false);
     }
 
     @Test
     public void shouldCreateReportWithTenWarningsWithDisabledHealthReport() {
-        verifyHealthOfProjectWithWarnings("demoJavacTenWarnings.txt",10,100,false);
+        verifyHealthOfProjectWithWarnings("demoJavacTenWarnings.txt", 10, 100, false);
     }
 
-
-    private class HtmlJenkinsInfo {
+    /**
+     * Page object for buildNumber/java/info HtmlPage.
+     */
+    private class HtmlJenkinsJavaInfo {
 
         HtmlPage webPage;
 
-        HtmlJenkinsInfo(Project project, String remainingUrl){
-            webPage = getWebPage(project,remainingUrl);
+        HtmlJenkinsJavaInfo(Project project, String remainingUrl) {
+            String buildnumber = String.valueOf(project.getLastBuild().getNumber());
+            webPage = getWebPage(project, buildnumber + "/java/info");
+        }
+
+        ImmutableList<String> getInfoMessages() {
+
+            return getMessagesById("info");
+        }
+
+        ImmutableList<String> getErrorMessages() {
+
+            return getMessagesById("errors");
+        }
+
+        private ImmutableList<String> getMessagesById(String idString) {
+            List<String> messages = new ArrayList<>();
+            DomElement messageElement = webPage.getElementById(idString);
+            if (messageElement != null && messageElement.hasChildNodes()) {
+                for (DomElement message : messageElement.getChildElements()) {
+                    messages.add(message.getTextContent());
+                }
+            }
+            return Lists.immutable.withAll(messages);
         }
 
     }
-
-    private class HtmlConfiguration {
-        private static final String HEALTHY = "_.healthy";
-        private static final String UNHEALTHY = "_.unhealthy";
-        HtmlForm webForm;
-
-        HtmlConfiguration(Project project) {
-            webForm = getWebPage(project,"configure").getFormByName("config");
-        }
-
-        void setHealthy(int healthy) {
-            ((HtmlNumberInput)webForm.getInputByName(HEALTHY)).setText(String.valueOf(healthy));
-        }
-
-        void setUnhealthy(int unhealthy) {
-            ((HtmlNumberInput)webForm.getInputByName(UNHEALTHY)).setText(String.valueOf(unhealthy));
-        }
-
-        HtmlForm getWebForm() {
-            return webForm;
-        }
-
-
-    }
-
 
 }
