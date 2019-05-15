f3eb8bb8b1184ac49c2e633baf1d3b9ec726461a
Andreas Neumeier <andi@an-9.de>
2019-05-15 16:16:41 +0200

    Method for getting next TrendCarousel Chart

---
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/TrendCarouselITest.java
@@ -0,0 +1,78 @@
+package io.jenkins.plugins.analysis.warnings.recorder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import hudson.model.FreeStyleProject;
+import hudson.model.Result;
+
+import io.jenkins.plugins.analysis.core.model.AnalysisResult;
+import io.jenkins.plugins.analysis.core.steps.IssuesRecorder;
+import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite;
+import io.jenkins.plugins.analysis.warnings.Java;
+import io.jenkins.plugins.analysis.warnings.recorder.pageobj.DetailsViewCharts;
+import io.jenkins.plugins.analysis.warnings.recorder.pageobj.DetailsViewTrendCarousel;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Provides tests for the trend carousel shown on the details page.
+ */
+public class TrendCarouselITest extends IntegrationTestWithJenkinsPerSuite {
+
+
+    /**
+     *
+     */
+    @Test
+    public void shouldShowToolsTrendChart() {
+        FreeStyleProject project = createFreeStyleProject();
+
+        Java java = new Java();
+        java.setPattern("**/*.txt");
+        IssuesRecorder issuesRecorder = enableWarnings(project, java);
+
+        List<AnalysisResult> buildResults = new ArrayList<>();
+        createWorkspaceFileWithWarnings(project, 1, 2);
+        buildResults.add(scheduleBuildAndAssertStatus(project, Result.SUCCESS));
+
+        createWorkspaceFileWithWarnings(project, 1, 2, 3, 4);
+        buildResults.add(scheduleBuildAndAssertStatus(project, Result.SUCCESS));
+
+        createWorkspaceFileWithWarnings(project, 3);
+        buildResults.add(scheduleBuildAndAssertStatus(project, Result.SUCCESS));
+
+        DetailsViewTrendCarousel carousel = new DetailsViewTrendCarousel(getDetailsWebPage(project, buildResults.get(2)));
+        carousel.nextCarouselChart();
+    }
+
+
+    private HtmlPage getDetailsWebPage(final FreeStyleProject project, final AnalysisResult result) {
+        int buildNumber = result.getBuild().getNumber();
+        String pluginId = result.getId();
+        return getWebPage(JavaScriptSupport.JS_ENABLED, project, buildNumber + "/" + pluginId);
+    }
+
+    private void createWorkspaceFileWithWarnings(final FreeStyleProject project,
+            final int... linesWithWarning) {
+        StringBuilder warningText = new StringBuilder();
+        for (int lineNumber : linesWithWarning) {
+            warningText.append(createDeprecationWarning(lineNumber)).append("\n");
+        }
+
+        createFileInWorkspace(project, "javac.txt", warningText.toString());
+    }
+
+    private String createDeprecationWarning(final int lineNumber) {
+        return String.format(
+                "[WARNING] C:\\Path\\SourceFile.java:[%d,42] [deprecation] path.AClass in path has been deprecated\n",
+                lineNumber);
+    }
+}
--- src/test/java/io/jenkins/plugins/analysis/warnings/recorder/pageobj/DetailsViewTrendCarousel.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/pageobj/DetailsViewTrendCarousel.java
@@ -1,6 +1,9 @@
 package io.jenkins.plugins.analysis.warnings.recorder.pageobj;
 
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import net.sf.json.JSONObject;
 
 /**
  * Page Object for the trend carousel in details views.
@@ -17,5 +20,29 @@ public class DetailsViewTrendCarousel {
      */
     public DetailsViewTrendCarousel(final HtmlPage detailsViewWebPage) {
         this.detailsViewWebPage = detailsViewWebPage;
+    }
+
+    public void nextCarouselChart() {
+        final HtmlDivision nextItemButton = detailsViewWebPage.getHtmlElementById("carousel-control-next");
+        final HtmlDivision trendCarousel = detailsViewWebPage.getHtmlElementById("trend-carousel");
+        final HtmlDivision activeItem = (HtmlDivision) trendCarousel.getByXPath("/div[contains(@class, 'active')]").get(0);
+        final String activeItemId = activeItem.getChildNodes().get(0).getNodeName();
+        System.out.println(activeItemId);
+
+        //final HtmlDivision acticeItem = tendCarousel.getByXPath("/div[class='carousel-item active']").get(0);
+
+    }
+
+    public void previouseCarouselChart() {
+
+
+    }
+
+    public JSONObject getCarouselChart() {
+
+    }
+
+    public static void main(String[] args) {
+
     }
 }
