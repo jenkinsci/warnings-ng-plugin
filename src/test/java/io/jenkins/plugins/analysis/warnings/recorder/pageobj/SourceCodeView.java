ec95bb3cae95fd44bbfbde0127523361dd013901
Andreas Moser <amoser@hm.edu>
2019-05-15 17:34:04 +0200

    Fixed some coding style issues.

--- src/test/java/io/jenkins/plugins/analysis/warnings/recorder/pageobj/SourceCodeView.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/pageobj/SourceCodeView.java
@@ -2,8 +2,6 @@ package io.jenkins.plugins.analysis.warnings.recorder.pageobj;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.DomNode;
@@ -16,15 +14,10 @@ public class SourceCodeView {
     /** The html element id of the main panel. */
     public static final String MAIN_PANEL_ELEMENT_ID = "main-panel";
 
-    /** The regex pattern describing the head line of the page (including file name). */
-    public static final Pattern FILE_NAME_PATTERN = Pattern.compile("Content of file (.*)");
-
-    /** The xPath to the file name (starting at the main panel). */
-    public static final String FILE_NAME_XPATH = "//h1";
     /** The xPath to the source code (starting at the main panel). */
     public static final String SOURCE_CODE_XPATH = "//pre//code";
     /** The xPath to the source code producing the issue (starting at the main panel). */
-    public static final String ISSUE_SOURCE_CODE_XPATH = "//pre//code[2]";
+    public static final String AFFECTED_LINES_XPATH = "//pre//code[2]";
     /** The xPath to the message describing the issue (starting at the main panel). */
     public static final String ISSUE_MESSAGE_XPATH = "//pre//div//div";
 
@@ -32,33 +25,13 @@ public class SourceCodeView {
     private HtmlPage sourceCodeViewPage;
 
     /**
-     * Constructor.
+     * Creates a new instance of {@link SourceCodeView}.
      *
      * @param sourceCodeViewPage
      *         The source code view html page.
      */
-    public SourceCodeView(HtmlPage sourceCodeViewPage) {
+    public SourceCodeView(final HtmlPage sourceCodeViewPage) {
         this.sourceCodeViewPage = sourceCodeViewPage;
-    }
-
-    /**
-     * Extracts the name of the file that is displayed on the html page.
-     *
-     * @return The file name.
-     */
-    public String getFileName() {
-        String name;
-
-        List<DomNode> headingByXPath = getMainPanel().getByXPath(FILE_NAME_XPATH);
-        if (!headingByXPath.isEmpty()) {
-            String heading = headingByXPath.get(0).asText();
-            Matcher matcher = FILE_NAME_PATTERN.matcher(heading);
-            if (matcher.matches()) {
-                return matcher.group(1);
-            }
-        }
-
-        throw new NoSuchElementException("Can't find the file name on page.");
     }
 
     /**
@@ -79,22 +52,22 @@ public class SourceCodeView {
     }
 
     /**
-     * Extracts the source code line that produces the issue.
+     * Extracts the source code lines affected by the issue.
      *
-     * @return The source code line producing the issue.
+     * @return The source code lines affected by the issue.
      */
-    public String getIssueSourceCodeLine() {
-        StringBuilder sourceCodeLineBuilder = new StringBuilder();
-        List<DomNode> issueSourceCodeList = getMainPanel().getByXPath(ISSUE_SOURCE_CODE_XPATH);
+    public String getAffectedLines() {
+        StringBuilder affectedLinesBuilder = new StringBuilder();
+        List<DomNode> affectedLines = getMainPanel().getByXPath(AFFECTED_LINES_XPATH);
 
-        if (!issueSourceCodeList.isEmpty()) {
-            issueSourceCodeList.forEach(c -> sourceCodeLineBuilder.append(c.asText()));
+        if (affectedLines.isEmpty()) {
+            throw new NoSuchElementException("Can't find affected source code lines on page.");
         }
         else {
-            throw new NoSuchElementException("Can't find the issue source code line on page.");
+            affectedLines.forEach(c -> affectedLinesBuilder.append(c.asText()));
         }
 
-        return sourceCodeLineBuilder.toString();
+        return affectedLinesBuilder.toString();
     }
 
     /**
@@ -104,19 +77,13 @@ public class SourceCodeView {
      */
     public String getIssueMessage() {
         StringBuilder issueMessageBuilder = new StringBuilder();
-        List<DomNode> issueMessageList = getMainPanel().getByXPath(ISSUE_MESSAGE_XPATH);
+        List<DomNode> issueMessages = getMainPanel().getByXPath(ISSUE_MESSAGE_XPATH);
 
-        if (!issueMessageList.isEmpty()) {
-            for (DomNode node : issueMessageList) {
-                String issueMessage = node.asText();
-
-                //replacing "new line" and "tab" character at the start of the message
-                issueMessage = issueMessage.replace("\t", "").replace("\n", "");
-                issueMessageBuilder.append(issueMessage);
-            }
+        if (issueMessages.isEmpty()) {
+            throw new NoSuchElementException("Can't find the issue message on page.");
         }
         else {
-            throw new NoSuchElementException("Can't find the issue message on page.");
+            issueMessages.forEach(m -> issueMessageBuilder.append(m.asText()));
         }
 
         return issueMessageBuilder.toString();
