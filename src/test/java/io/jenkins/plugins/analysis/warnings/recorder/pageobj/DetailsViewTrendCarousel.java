f083e72cf3f5729edbc72549ac5951ca8a803897
Andreas Neumeier <andi@an-9.de>
2019-05-16 20:33:43 +0200

    Finished implementing TrendCarousel PageObject

--- src/test/java/io/jenkins/plugins/analysis/warnings/recorder/pageobj/DetailsViewTrendCarousel.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/pageobj/DetailsViewTrendCarousel.java
@@ -2,7 +2,6 @@ package io.jenkins.plugins.analysis.warnings.recorder.pageobj;
 
 import java.io.IOException;
 
-import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -13,61 +12,34 @@ import net.sf.json.JSONObject;
  * Page Object for the trend carousel in details views.
  */
 public class DetailsViewTrendCarousel {
-
-    private final WebClient webClient;
     private HtmlPage detailsViewWebPage;
     private HtmlAnchor carouselControlNext;
     private HtmlAnchor carouselControlPrev;
-    private HtmlDivision carouselItemActive;
     private String carouselItemActiveId;
 
     /**
      * Creates the trend carousel PageObject for the details view web page. E.g. {buildNr}/java
      *
      * @param detailsViewWebPage
-     *         The details view webpage to get the trend carousel from.
-     * @param webClient
-     *          The web client which handles the web request.
+     *         The details view web page to get the trend carousel from.
      */
-    public DetailsViewTrendCarousel(final HtmlPage detailsViewWebPage, final WebClient webClient) {
-        this.webClient = webClient;
+    public DetailsViewTrendCarousel(final HtmlPage detailsViewWebPage) {
         setupDetailsViewTrendCarousel(detailsViewWebPage);
-
-        System.out.println(webClient.getBrowserVersion());
     }
 
     private void setupDetailsViewTrendCarousel(final HtmlPage page) {
         detailsViewWebPage = page;
-
-        //webClient.waitForBackgroundJavaScript(20000);
-        //JavaScriptJobManager manager = page.getEnclosingWindow().getJobManager();
-        //while (manager.getJobCount() > 0) {
-        //    try {
-        //        Thread.sleep(1000);
-        //    }
-        //    catch (InterruptedException e) {
-        //        e.printStackTrace();
-        //    }
-        //}
-
         carouselControlNext = (HtmlAnchor) detailsViewWebPage.getByXPath(
-                "//a[contains(@class, 'carousel-control-next')]").get(0);
+                "//div[@id='trend-carousel']/a[contains(@class, 'carousel-control-next')]").get(0);
         carouselControlPrev = (HtmlAnchor) detailsViewWebPage.getByXPath(
-                "//a[contains(@class, 'carousel-control-prev')]").get(0);
+                "//div[@id='trend-carousel']/a[contains(@class, 'carousel-control-prev')]").get(0);
         setCarouselItemActive();
     }
 
     private void setCarouselItemActive() {
-        carouselItemActive = (HtmlDivision) detailsViewWebPage.getByXPath(
+        HtmlDivision carouselItemActive = (HtmlDivision) detailsViewWebPage.getByXPath(
                 "//div[@id='trend-carousel']/div/div[contains(@class, 'carousel-item active')]").get(0);
-        detailsViewWebPage.getByXPath(
-                "//div[@id='trend-carousel']/div/div[contains(@class, 'carousel-item active')]").forEach(e -> System.out
-                .println(((HtmlDivision) e).asXml()));
-
-        //System.out.println(carouselItemActive.asXml());
         carouselItemActiveId = ((HtmlDivision) carouselItemActive.getChildNodes().get(0)).getId();
-
-        System.out.println(carouselItemActiveId);
     }
 
     /**
@@ -79,7 +51,6 @@ public class DetailsViewTrendCarousel {
         try {
             HtmlPage newPage = carouselControlNext.click();
             setupDetailsViewTrendCarousel(newPage);
-            //TO DO setter inside or outside of try?
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -89,7 +60,7 @@ public class DetailsViewTrendCarousel {
     }
 
     /**
-     * Click on the next button to show previous carousel item.
+     * Click on the previous button to show previous carousel item.
      *
      * @return if click was successful
      */
@@ -97,7 +68,6 @@ public class DetailsViewTrendCarousel {
         try {
             HtmlPage newPage = carouselControlPrev.click();
             setupDetailsViewTrendCarousel(newPage);
-            //TO DO setter inside or outside of try?
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -113,5 +83,14 @@ public class DetailsViewTrendCarousel {
      */
     public JSONObject getCarouselItemActive() {
         return new DetailsViewCharts(detailsViewWebPage).getChartModel(carouselItemActiveId);
+    }
+
+    /**
+     * Get the id of the chart which is currently active.
+     *
+     * @return Name of the active chart
+     */
+    public String getCarouselItemActiveId() {
+        return carouselItemActiveId;
     }
 }
