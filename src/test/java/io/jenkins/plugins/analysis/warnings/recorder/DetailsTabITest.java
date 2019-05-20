ba08f44d3ffbeabd2ea47c340c58889cbcc9fff8
Ulli Hafner <ullrich.hafner@gmail.com>
2019-05-20 20:22:28 +0200

    Merged tests into single test and simplified some assertions.

--- src/test/java/io/jenkins/plugins/analysis/warnings/recorder/DetailsTabITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/DetailsTabITest.java
@@ -2,8 +2,6 @@ package io.jenkins.plugins.analysis.warnings.recorder;
 
 import org.junit.Test;
 
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 
@@ -33,13 +31,8 @@ public class DetailsTabITest extends IntegrationTestWithJenkinsPerTest {
         copySingleFileToWorkspace(project, "../java1Warning.txt", "java.txt");
         AnalysisResult analysisResult = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
 
-        assertThat(project.getLastBuild()).isNotNull();
-        HtmlPage htmlPage = getWebPage(JavaScriptSupport.JS_ENABLED, analysisResult);
-
-        DetailsTab detailsTab = new DetailsTab(htmlPage);
-        assertThat(detailsTab.getTabTypes())
-                .hasSize(1)
-                .contains(TabType.ISSUES);
+        DetailsTab detailsTab = createDetailsTab(analysisResult);
+        assertThat(detailsTab.getTabTypes()).containsOnlyOnce(TabType.ISSUES);
         assertThat(detailsTab.getActiveTabType()).isEqualTo(TabType.ISSUES);
 
         IssuesTable active = detailsTab.getActive();
@@ -56,57 +49,26 @@ public class DetailsTabITest extends IntegrationTestWithJenkinsPerTest {
         copySingleFileToWorkspace(project, "../java2Warnings.txt", "java.txt");
         AnalysisResult analysisResult = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
 
-        HtmlPage htmlPage = getWebPage(JavaScriptSupport.JS_ENABLED, analysisResult);
-
-        DetailsTab detailsTab = new DetailsTab(htmlPage);
+        DetailsTab detailsTab = createDetailsTab(analysisResult);
         assertThat(detailsTab.getTabTypes())
-                .hasSize(3)
-                .contains(
-                        TabType.ISSUES,
-                        TabType.FILES,
-                        TabType.FOLDERS);
+                .containsExactly(TabType.FOLDERS, TabType.FILES, TabType.ISSUES);
 
         assertThat(detailsTab.getActiveTabType()).isEqualTo(TabType.FOLDERS);
+
         PropertyTable folders = detailsTab.getActive();
-        assertThat(folders).isInstanceOf(PropertyTable.class);
         assertThat(folders.getRows()).hasSize(2);
 
         detailsTab.select(TabType.ISSUES);
         assertThat(detailsTab.getActiveTabType()).isEqualTo(TabType.ISSUES);
+
         IssuesTable issues = detailsTab.getActive();
-        assertThat(issues).isInstanceOf(IssuesTable.class);
         assertThat(issues.getRows()).hasSize(2);
 
         detailsTab.select(TabType.FILES);
         assertThat(detailsTab.getActiveTabType()).isEqualTo(TabType.FILES);
+
         PropertyTable files = detailsTab.getActive();
-        assertThat(files).isInstanceOf(PropertyTable.class);
         assertThat(files.getRows()).hasSize(2);
-    }
-
-    /**
-     * When four warnings are being recognized in two files the tabs issues, files and folders should be shown.
-     */
-    @Test
-    public void shouldPopulateDetailsTabMultipleFiles() {
-        FreeStyleProject project = createFreeStyleJobWithWarnings();
-
-        copySingleFileToWorkspace(project, "../java2Warnings.txt", "java.txt");
-        copySingleFileToWorkspace(project, "../java2Warnings.txt", "java2.txt");
-        AnalysisResult analysisResult = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
-
-        HtmlPage htmlPage = getWebPage(JavaScriptSupport.JS_ENABLED, analysisResult);
-
-        DetailsTab detailsTab = new DetailsTab(htmlPage);
-        assertThat(detailsTab.getTabTypes())
-                .hasSize(3)
-                .contains(
-                        TabType.ISSUES,
-                        TabType.FILES,
-                        TabType.FOLDERS);
-        assertThat(detailsTab.getActiveTabType()).isEqualTo(TabType.FOLDERS);
-        PropertyTable active = detailsTab.getActive();
-        assertThat(active.getRows()).hasSize(2);
     }
 
     /**
@@ -120,21 +82,20 @@ public class DetailsTabITest extends IntegrationTestWithJenkinsPerTest {
         copySingleFileToWorkspace(project, "../java2Warnings.txt", "java.txt");
         AnalysisResult analysisResult = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
 
-        HtmlPage htmlPage = getWebPage(JavaScriptSupport.JS_ENABLED, analysisResult);
-
-        DetailsTab detailsTab = new DetailsTab(htmlPage);
+        DetailsTab detailsTab = createDetailsTab(analysisResult);
         assertThat(detailsTab.getTabTypes())
-                .hasSize(3)
-                .contains(
-                        TabType.ISSUES,
-                        TabType.FILES,
-                        TabType.FOLDERS);
+                .containsExactly(TabType.FOLDERS, TabType.FILES, TabType.ISSUES);
 
         assertThat(detailsTab.getActiveTabType()).isEqualTo(TabType.FOLDERS);
         detailsTab.select(TabType.ISSUES);
         assertThat(detailsTab.getActiveTabType()).isEqualTo(TabType.ISSUES);
-        DetailsTab refreshedTab = new DetailsTab(getWebPage(JavaScriptSupport.JS_ENABLED, analysisResult));
+
+        DetailsTab refreshedTab = createDetailsTab(analysisResult);
         assertThat(refreshedTab.getActiveTabType()).isEqualTo(TabType.ISSUES);
+    }
+
+    private DetailsTab createDetailsTab(final AnalysisResult analysisResult) {
+        return new DetailsTab(getWebPage(JavaScriptSupport.JS_ENABLED, analysisResult));
     }
 
     private FreeStyleProject createFreeStyleJobWithWarnings() {
