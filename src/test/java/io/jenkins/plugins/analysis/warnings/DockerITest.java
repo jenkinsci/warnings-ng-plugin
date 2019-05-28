7c36dd8065b2e1b00557f81e1646226488489a9f
Nils Engelbrecht <nengelbr@hm.edu>
2019-05-28 12:52:00 +0200

    added javadoc, test file and three test setups

--- src/test/java/io/jenkins/plugins/analysis/warnings/DockerITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/DockerITest.java
@@ -2,6 +2,7 @@ package io.jenkins.plugins.analysis.warnings;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 import org.apache.commons.io.output.TeeOutputStream;
 import org.junit.BeforeClass;
@@ -16,11 +17,15 @@ import org.jenkinsci.test.acceptance.docker.fixtures.JavaContainer;
 import hudson.Functions;
 import hudson.Launcher;
 import hudson.model.Descriptor.FormException;
-import hudson.model.FreeStyleProject;
+import hudson.model.Label;
+import hudson.model.Result;
 import hudson.plugins.sshslaves.SSHLauncher;
+import hudson.security.FullControlOnceLoggedInAuthorizationStrategy;
+import hudson.security.HudsonPrivateSecurityRealm;
 import hudson.slaves.DumbSlave;
 import hudson.util.StreamTaskListener;
 import hudson.util.VersionNumber;
+import jenkins.security.s2m.AdminWhitelistRule;
 
 import io.jenkins.plugins.analysis.core.model.AnalysisResult;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerTest;
@@ -29,7 +34,15 @@ import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assume.*;
 
+/**
+ * Integration tests for dumb-slaves and agents on docker.
+ *
+ * @author Colin Kashel
+ * @author Nils Engelbrecht
+ */
 public class DockerITest extends IntegrationTestWithJenkinsPerTest {
+
+    private static final String SLAVE_LABEL = "slave1";
 
     @Rule
     public DockerRule<JavaContainer> javaDockerRule = new DockerRule<>(JavaContainer.class);
@@ -49,12 +62,75 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
                 new VersionNumber(baos.toString().trim()), greaterThanOrEqualTo(new VersionNumber("1.13.0")));
     }
 
+    /**
+     * This test should run a build on dumb-slave agent (without docker) and should verify jenkins bug 56007.
+     *
+     * @throws IOException
+     *         throws IOException
+     */
     @Test
-    public void shouldStartAgent() {
+    public void shouldStartAgentAndVerifyJenkinsBug() throws IOException {
+        enableSecurity();
         DumbSlave agent = createAgent();
         WorkflowJob job = createPipeline();
+
+        boolean security = getJenkins().jenkins.isUseSecurity();
+        assertThat(getJenkins().jenkins.isUseSecurity()).isTrue();
+
+        copySingleFileToAgentWorkspace(agent, job, "Test.java", "Test.java");
         job.setDefinition(new CpsFlowDefinition("pipeline {\n"
-                + "    agent {label 'slave1'}\n"
+                + "    agent {label '" + SLAVE_LABEL + "'}\n"
+                + "    stages {\n"
+                + "        stage ('Create a fake warning') {\n"
+                + "            steps {\n"
+                + "                 echo 'Test.java:6: warning: [cast] redundant cast to String'\n"
+                + "                 recordIssues tool: java()\n"
+                + "            }\n"
+                + "        }\n"
+                + "    }\n"
+                + "}", true));
+
+        AnalysisResult result = scheduleBuildAndAssertStatus(job, Result.SUCCESS);
+        assertThat(result.getErrorMessages()).doesNotContain(
+                "[ERROR] Can't copy some affected workspace files to Jenkins build folder:");
+        assertThat(agent.getWorkspaceRoot().getName()).isEqualTo("workspace");
+    }
+
+    /**
+     * This test should run a maven build on docker-container.
+     */
+    @Test
+    public void shouldRunMavenBuildOnDockerAgent() {
+        DumbSlave agent = createAgent();
+        WorkflowJob job = createPipeline();
+
+        job.setDefinition(new CpsFlowDefinition("pipeline {\n"
+                + "    agent {label '" + SLAVE_LABEL + "'}\n"
+                + "    stages {\n"
+                + "        stage ('Create a fake warning') {\n"
+                + "            steps {\n"
+                + "                 echo 'Test.java:6: warning: [cast] redundant cast to String'\n"
+                + "                 recordIssues tool: java()\n"
+                + "            }\n"
+                + "        }\n"
+                + "    }\n"
+                + "}", true));
+
+        AnalysisResult result = scheduleSuccessfulBuild(job);
+
+        assertThat(agent.getWorkspaceRoot().getName()).isEqualTo("workspace");
+    }
+
+    /**
+     * This test should run a make/gcc build on docker-container.
+     */
+    @Test
+    public void shouldRunMakeBuildOnDockerAgent() {
+        DumbSlave agent = createAgent();
+        WorkflowJob job = createPipeline();
+
+        job.setDefinition(new CpsFlowDefinition("pipeline {\n"
+                + "    agent {label '" + SLAVE_LABEL + "'}\n"
                 + "    stages {\n"
                 + "        stage ('Create a fake warning') {\n"
                 + "            steps {\n"
@@ -72,7 +148,7 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
 
     private DumbSlave createAgent() {
         try {
-            DumbSlave agent = getJenkins().createOnlineSlave();
+            DumbSlave agent = getJenkins().createOnlineSlave(Label.get(SLAVE_LABEL));
             getJenkins().jenkins.addNode(agent);
             getJenkins().waitOnline(agent);
 
@@ -82,6 +158,18 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
             throw new AssertionError(e);
         }
 
+    }
+
+    private void enableSecurity() throws IOException {
+        HudsonPrivateSecurityRealm privateSecurityRealm = new HudsonPrivateSecurityRealm(false, false, null);
+        privateSecurityRealm.createAccount("usr", "pwd");
+
+        getJenkins().jenkins.setSecurityRealm(privateSecurityRealm);
+        getJenkins().jenkins.setAuthorizationStrategy(new FullControlOnceLoggedInAuthorizationStrategy());
+        Objects.requireNonNull(getJenkins().jenkins.getInjector())
+                .getInstance(AdminWhitelistRule.class)
+                .setMasterKillSwitch(false);
+        getJenkins().jenkins.save();
     }
 
     private DumbSlave createDockerAgent(final DockerContainer container) throws FormException, IOException {
---
+++ src/test/resources/io/jenkins/plugins/analysis/warnings/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    private String test1;
+
+    public Test() {
+        this.test1 = (String) "Test1";
+    }
+}
