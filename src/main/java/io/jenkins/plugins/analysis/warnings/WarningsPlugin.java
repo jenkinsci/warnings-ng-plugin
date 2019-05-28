7063463459661cddf65e8e0c1c9b6e6ef6fec05a
Ulli Hafner <ullrich.hafner@gmail.com>
2019-05-28 12:40:22 +0200

    Enable JSON Parsers in WarningsPlugin.
    Followup to https://github.com/jenkinsci/analysis-model/pull/177.

--- src/main/java/io/jenkins/plugins/analysis/warnings/WarningsPlugin.java
+++ src/main/java/io/jenkins/plugins/analysis/warnings/WarningsPlugin.java
@@ -1,6 +1,10 @@
 package io.jenkins.plugins.analysis.warnings;
 
+import java.util.Collection;
+
 import edu.hm.hafner.analysis.IssueParser;
+import edu.hm.hafner.analysis.parser.JsonLogParser;
+import edu.hm.hafner.analysis.parser.JsonParser;
 import edu.hm.hafner.analysis.parser.XmlParser;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
@@ -8,14 +12,16 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.jenkinsci.Symbol;
 import hudson.Extension;
 
-import io.jenkins.plugins.analysis.core.model.ReportScanningTool;
+import io.jenkins.plugins.analysis.core.model.ReportScanningToolSuite;
+
+import static j2html.TagCreator.*;
 
 /**
  * Provides a parser for the native format of the Warnings Next Generation Plugin.
  *
  * @author Ullrich Hafner
  */
-public class WarningsPlugin extends ReportScanningTool {
+public class WarningsPlugin extends ReportScanningToolSuite {
     private static final long serialVersionUID = 8110398783405047555L;
     private static final String ID = "issues";
 
@@ -27,8 +33,8 @@ public class WarningsPlugin extends ReportScanningTool {
     }
 
     @Override
-    public IssueParser createParser() {
-        return new XmlParser("/report/issue");
+    protected Collection<? extends IssueParser> getParsers() {
+        return asList(new XmlParser("/report/issue"), new JsonLogParser(), new JsonParser());
     }
 
     /** Descriptor for this static analysis tool. */
@@ -49,6 +55,18 @@ public class WarningsPlugin extends ReportScanningTool {
         @Override
         public boolean canScanConsoleLog() {
             return false;
+        }
+
+        @Override
+        public String getHelp() {
+            return p().withText("Create an output file that contains issues in the native Warnings Plugin format, "
+                    + "in either XML or JSON. The supported format is identical to the format of the remote API calls. "
+                    + "The parser is even capable of reading individual lines of a log file that contains issues in JSON format.").render();
+        }
+
+        @Override
+        public String getUrl() {
+            return "https://github.com/jenkinsci/warnings-ng-plugin/blob/master/doc/Documentation.md#export-your-issues-into-a-supported-format";
         }
     }
 }
