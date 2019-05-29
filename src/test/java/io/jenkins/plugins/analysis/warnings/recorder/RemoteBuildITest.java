1d00fd34e60d14264d6defd00d60fc5bbb9adb7e
Sebastian Heunke <sebastian.heunke@hm.edu>
2019-05-29 17:20:50 +0200

    Initial commit for Remote Build tests

---
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/RemoteBuildITest.java
@@ -0,0 +1,35 @@
+package io.jenkins.plugins.analysis.warnings.recorder;
+
+import org.junit.Test;
+
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import hudson.model.Label;
+import hudson.model.Result;
+import hudson.model.Slave;
+
+import io.jenkins.plugins.analysis.core.model.AnalysisResult;
+import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite;
+import io.jenkins.plugins.analysis.warnings.Java;
+
+import static io.jenkins.plugins.analysis.core.assertions.Assertions.*;
+
+public class RemoteBuildITest  extends IntegrationTestWithJenkinsPerSuite {
+
+    @Test
+    public void shouldRunBuildOnDumbSlave() throws Exception {
+
+        Slave agent = createAgentWithEnabledSecurity("slave");
+
+        WorkflowJob project = createPipeline();
+        //Java java = new Java();
+        //java.setPattern("**/*.txt");
+        //enableWarnings(project, java);
+        // TODO: set pattern through cps flow definition
+        createFileInAgentWorkspace(agent, project, "javac.txt", "[WARNING] MyWarn:[1,42] [deprecation] Something is old\n");
+
+        project.setDefinition(new CpsFlowDefinition("node('slave') {recordIssues tools: [java()]}", true));
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        assertThat(result).hasTotalSize(1);
+    }
+}
