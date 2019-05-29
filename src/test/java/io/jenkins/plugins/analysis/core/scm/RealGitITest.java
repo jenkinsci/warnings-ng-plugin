bb167e51eca9a497d49ab17ab1a647984f4fdb8d
Raphael Furch <raphael@familie-furch.de>
2019-05-29 18:12:17 +0200

    clean up code

--- src/test/java/io/jenkins/plugins/analysis/core/scm/RealGitITest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/scm/RealGitITest.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.CreateFileBuilder;
+import org.jvnet.hudson.test.Issue;
 
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import hudson.model.FreeStyleProject;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.extensions.impl.RelativeTargetDirectory;
@@ -20,11 +23,24 @@ import io.jenkins.plugins.analysis.warnings.Doxygen;
 
 import static org.assertj.core.api.Assertions.*;
 
+/**
+ * Tests the jenkins with git in its natural habitat.
+ *
+ * @author Michael Schmid, Raphael Furch
+ */
 public class RealGitITest extends IntegrationTestWithJenkinsPerSuite {
+
+    /**
+     * Rule to init a git repo.
+     */
     @Rule
     public GitSampleRepoRule sampleRepo = new GitSampleRepoRule();
 
+    /**
+     * Validates issue JENKINS-57260 with freestyle projects.
+     */
     @Test
+    @Issue("JENKINS-57260")
     public void gitBlameOnDifferentDirectoryIssue57260FreeStyleProject() {
         gitInitIssue57260();
 
@@ -48,16 +64,56 @@ public class RealGitITest extends IntegrationTestWithJenkinsPerSuite {
                         + " \n"
                         + "src/LCPcalc.cpp:12: Warning: the name `lcp_lexicolemke.c' supplied as the second argument in the \\file statement is not an input file"));
 
-
-        IssuesRecorder recorder = enableWarnings(project, createTool(new Doxygen(), "build/doxygen/doxygen/doxygen.log"));
+        IssuesRecorder recorder = enableWarnings(project,
+                createTool(new Doxygen(), "build/doxygen/doxygen/doxygen.log"));
         recorder.setAggregatingResults(true);
         recorder.setEnabledForFailure(true);
-
 
         scheduleSuccessfulBuild(project);
         AnalysisResult result = scheduleSuccessfulBuild(project);
 
-        assertThat(result.getErrorMessages()).doesNotContain("Can't determine head commit using 'git rev-parse'. Skipping blame.");
+        assertThat(result.getErrorMessages()).doesNotContain(
+                "Can't determine head commit using 'git rev-parse'. Skipping blame.");
+    }
+
+    /**
+     * Validates issue JENKINS-57260 with pipeline.
+     */
+    @Test
+    @Issue("JENKINS-57260")
+    public void gitBlameOnDifferentDirectoryIssue57260Pipeline() {
+        gitInitIssue57260();
+        WorkflowJob project = createPipeline();
+
+        project.setDefinition(new CpsFlowDefinition("pipeline {\n"
+                + "agent any\n"
+                + "options{\n"
+                + "skipDefaultCheckout()\n"
+                + "}\n"
+                + "stages{\n"
+                + "stage('Prepare') {\n"
+                + "  steps {\n"
+                + "    dir('src') {\n"
+                + "      checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: '"
+                + sampleRepo.fileUrl() + "']]])\n"
+                + "    }\n"
+                + "  }\n"
+                + "}\n"
+                + "stage('Doxygen') {\n"
+                + "  steps {\n"
+                + "    dir('build/doxygen') {\n"
+                + "      writeFile file: 'doxygen/doxygen.log', text: '\"Notice: Output directory doc/doxygen/framework does not exist. I have created it for you.\\nsrc/CentralDifferenceSolver.cpp:11: Warning: reached end of file while inside a dot block!\\nThe command that should end the block seems to be missing!\\nsrc/LCPcalc.cpp:12: Warning: the name lcp_lexicolemke.c supplied as the second argument in the file statement is not an input file\"'\n"
+                + "    }\n"
+                + "    recordIssues(aggregatingResults: true, enabledForFailure: true, tools: [ doxygen(name: 'Doxygen', pattern: 'build/doxygen/doxygen/doxygen.log') ] )\n"
+                + "  }\n"
+                + "}\n"
+                + "}}", false));
+
+        scheduleSuccessfulBuild(project);
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertThat(result.getErrorMessages()).doesNotContain(
+                "Can't determine head commit using 'git rev-parse'. Skipping blame.");
+
     }
 
     private void gitInitIssue57260() {
