227fcf9b43e62f93cb345ed2cdbf341b8b1a7f14
Raphael Furch <raphael@familie-furch.de>
2019-05-29 19:01:49 +0200

    add gui tests

--- src/test/java/io/jenkins/plugins/analysis/core/scm/RealGitITest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/scm/RealGitITest.java
@@ -1,11 +1,14 @@
 package io.jenkins.plugins.analysis.core.scm;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.CreateFileBuilder;
 import org.jvnet.hudson.test.Issue;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
@@ -21,6 +24,8 @@ import io.jenkins.plugins.analysis.core.model.AnalysisResult;
 import io.jenkins.plugins.analysis.core.steps.IssuesRecorder;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite;
 import io.jenkins.plugins.analysis.warnings.Doxygen;
+import io.jenkins.plugins.analysis.warnings.recorder.pageobj.SourceControlRow;
+import io.jenkins.plugins.analysis.warnings.recorder.pageobj.SourceControlTable;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,6 +42,14 @@ public class RealGitITest extends IntegrationTestWithJenkinsPerSuite {
     @Rule
     public GitSampleRepoRule sampleRepo = new GitSampleRepoRule();
 
+    private static final String FILE_NAME_1 = "CentralDifferenceSolver.cpp";
+    private static final String FILE_NAME_2 = "LCPcalc.cpp";
+
+    private static final String USER_NAME_1 = "User1";
+    private static final String USER_NAME_2 = "User2";
+
+    private static final String USER_EMAIL_1 = "user1@git.git";
+    private static final String USER_EMAIL_2 = "user2@git.git";
     /**
      * Validates issue JENKINS-57260 with freestyle projects.
      */
@@ -116,20 +129,8 @@ public class RealGitITest extends IntegrationTestWithJenkinsPerSuite {
      */
     @Test
     public void gitBlameWithDifferentUsers() {
-        try {
-            sampleRepo.init();
-            sampleRepo.write("CentralDifferenceSolver.cpp", "1\n2\n3\n4\n5\n6");
-            sampleRepo.write("LCPcalc.cpp", "1\n2\n3\n4\n5\n6\n7");
-            sampleRepo.git("add", "CentralDifferenceSolver.cpp", "LCPcalc.cpp");
-            sampleRepo.git("commit", "--author=\"User1 <user1@git.git>\"", "--all", "--message=\"commit from user1\"");
-            sampleRepo.write("CentralDifferenceSolver.cpp", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12");
-            sampleRepo.write("LCPcalc.cpp", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13");
-            sampleRepo.git("add", "CentralDifferenceSolver.cpp", "LCPcalc.cpp");
-            sampleRepo.git("commit", "--author=\"User2 <user2@git.git>\"", "--all", "--message=\"commit from user2\"");
-        }
-        catch (Exception exception) {
-            throw new RuntimeException(exception);
-        }
+
+        gitInitTwoUser();
 
         FreeStyleProject project = createFreeStyleProject();
         GitSCMBuilder builder = new GitSCMBuilder(new SCMHead("master"), null, sampleRepo.fileUrl(), null);
@@ -150,27 +151,71 @@ public class RealGitITest extends IntegrationTestWithJenkinsPerSuite {
 
         Blames blames = result.getBlames();
         String firstFile = result.getBlames().getFiles()
-                .stream().filter(name -> name.endsWith("CentralDifferenceSolver.cpp"))
+                .stream().filter(name -> name.endsWith(FILE_NAME_1))
                 .findFirst().orElseThrow(() -> new RuntimeException("CentralDifferenceSolver.cpp not found in blames"));
         String secondFile = result.getBlames().getFiles()
-                .stream().filter(name -> name.endsWith("LCPcalc.cpp"))
+                .stream().filter(name -> name.endsWith(FILE_NAME_2))
                 .findFirst().orElseThrow(() -> new RuntimeException("LCPcalc.cpp not found in blames"));
 
-        assertThat(blames.get(firstFile).getName(4)).isEqualTo("User1");
-        assertThat(blames.get(firstFile).getEmail(4)).isEqualTo("user1@git.git");
-        assertThat(blames.get(firstFile).getName(11)).isEqualTo("User2");
-        assertThat(blames.get(firstFile).getEmail(11)).isEqualTo("user2@git.git");
-        assertThat(blames.get(secondFile).getName(5)).isEqualTo("User1");
-        assertThat(blames.get(secondFile).getEmail(5)).isEqualTo("user1@git.git");
+        assertThat(blames.get(firstFile).getName(4)).isEqualTo(USER_NAME_1);
+        assertThat(blames.get(firstFile).getEmail(4)).isEqualTo(USER_EMAIL_1);
+        assertThat(blames.get(firstFile).getName(11)).isEqualTo(USER_NAME_2);
+        assertThat(blames.get(firstFile).getEmail(11)).isEqualTo(USER_EMAIL_2);
+        assertThat(blames.get(secondFile).getName(5)).isEqualTo(USER_NAME_1);
+        assertThat(blames.get(secondFile).getEmail(5)).isEqualTo(USER_EMAIL_1);
     }
 
+    /**
+     * Verify git blames of warnings from different users via gui.
+     */
+    @Test
+    public void gitBlameWithDifferentUsersViaGui() {
+        gitInitTwoUser();
+
+        FreeStyleProject project = createFreeStyleProject();
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        int buildNr = result.getBuild().getNumber();
+        String pluginId = result.getId();
+
+        HtmlPage detailsPage = getWebPage(JavaScriptSupport.JS_ENABLED, project, buildNr + "/" + pluginId);
+        SourceControlTable sourceControlTable = new SourceControlTable(detailsPage);
+
+        List<SourceControlRow> sourceControlRows = sourceControlTable.getRows();
+        assertThat(sourceControlRows.size()).isEqualTo(1);
+
+        assertThat(sourceControlRows.get(0).getValue(SourceControlRow.AUTHOR)).isEqualTo(USER_NAME_1);
+        assertThat(sourceControlRows.get(0).getValue(SourceControlRow.EMAIL)).isEqualTo(USER_EMAIL_1);
+        assertThat(sourceControlRows.get(0).getValue(SourceControlRow.FILE)).contains(FILE_NAME_1);
+
+        assertThat(sourceControlRows.get(1).getValue(SourceControlRow.AUTHOR)).isEqualTo(USER_NAME_2);
+        assertThat(sourceControlRows.get(1).getValue(SourceControlRow.EMAIL)).isEqualTo(USER_EMAIL_2);
+        assertThat(sourceControlRows.get(1).getValue(SourceControlRow.FILE)).contains(FILE_NAME_2);
+    }
     private void gitInitIssue57260() {
         try {
             sampleRepo.init();
-            sampleRepo.write("CentralDifferenceSolver.cpp", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12");
-            sampleRepo.write("LCPcalc.cpp", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13");
-            sampleRepo.git("add", "CentralDifferenceSolver.cpp", "LCPcalc.cpp");
+            sampleRepo.write(FILE_NAME_1, "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12");
+            sampleRepo.write(FILE_NAME_2, "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13");
+            sampleRepo.git("add", FILE_NAME_1, FILE_NAME_2);
             sampleRepo.git("commit", "--all", "--message=init");
+        }
+        catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+
+    private void gitInitTwoUser() {
+        try {
+            sampleRepo.init();
+            sampleRepo.write(FILE_NAME_1, "1\n2\n3\n4\n5\n6");
+            sampleRepo.write(FILE_NAME_2, "1\n2\n3\n4\n5\n6\n7");
+            sampleRepo.git("add", FILE_NAME_1, FILE_NAME_2);
+            sampleRepo.git("commit", "--author=\"" + USER_NAME_1 + " <" + USER_EMAIL_1 + ">\"", "--all", "--message=\"commit from user1\"");
+            sampleRepo.write(FILE_NAME_1, "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12");
+            sampleRepo.write(FILE_NAME_2, "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13");
+            sampleRepo.git("add", FILE_NAME_1, FILE_NAME_2);
+            sampleRepo.git("commit", "--author=\"" + USER_NAME_2 + " <" + USER_EMAIL_2 + ">\"", "--all", "--message=\"commit from user2\"");
         }
         catch (Exception exception) {
             throw new RuntimeException(exception);
