9a100b817ade9a4e0e3727e886012ae695cfe30f
andi <andipabst25@gmail.com>
2019-05-29 11:20:02 +0200

    Test the GitBlamer and SourceControlTable

---
+++ src/test/java/io/jenkins/plugins/analysis/core/scm/GitBlamerITest.java
@@ -0,0 +1,391 @@
+package io.jenkins.plugins.analysis.core.scm;
+
+import java.util.HashMap;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.CreateFileBuilder;
+import org.jvnet.hudson.test.Issue;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import hudson.model.FreeStyleProject;
+import hudson.plugins.git.GitSCM;
+import jenkins.plugins.git.GitSampleRepoRule;
+
+import io.jenkins.plugins.analysis.core.model.AnalysisResult;
+import io.jenkins.plugins.analysis.core.model.AnalysisResultAssert;
+import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerTest;
+import io.jenkins.plugins.analysis.warnings.Java;
+import io.jenkins.plugins.analysis.warnings.recorder.pageobj.DetailsTab;
+import io.jenkins.plugins.analysis.warnings.recorder.pageobj.DetailsTab.TabType;
+import io.jenkins.plugins.analysis.warnings.recorder.pageobj.SourceControlRow;
+import io.jenkins.plugins.analysis.warnings.recorder.pageobj.SourceControlTable;
+
+import static io.jenkins.plugins.analysis.warnings.recorder.pageobj.SourceControlRow.*;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Integration test for GitBlamer with a mocked git repository.
+ *
+ * @author Fabian Janker
+ * @author Andreas Pabst
+ */
+public class GitBlamerITest extends IntegrationTestWithJenkinsPerTest {
+
+    /**
+     * Rule for a git repository.
+     */
+    @Rule
+    public GitSampleRepoRule gitRepo = new GitSampleRepoRule();
+
+    /**
+     * Tests blaming one issue with a fake git repository. Test run from a pipeline script.
+     *
+     * @throws Exception
+     *         if there is a problem with the git repository
+     */
+    @Test
+    public void shouldBlameOneIssueWithPipeline() throws Exception {
+        gitRepo.init();
+        createAndCommitFile("Test.java", "public class Test {}");
+
+        final String testCommit = gitRepo.head();
+
+        createAndCommitFile("Jenkinsfile", "node {\n"
+                + "  stage ('Checkout') {\n"
+                + "    checkout scm\n"
+                + "  }\n"
+                + "  stage ('Build and Analysis') {"
+                + "    echo '[javac] Test.java:1: warning: Test Warning for Jenkins'\n"
+                + "    recordIssues tools: [java()]\n"
+                + "  }\n"
+                + "}");
+
+        WorkflowJob project = createPipeline();
+        project.setDefinition(new CpsScmFlowDefinition(new GitSCM(gitRepo.toString()), "Jenkinsfile"));
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertSuccessfulBlame(result, 1, 1);
+
+        SourceControlTable table = getSourceControlTable(result);
+        assertOneIssue(testCommit, table);
+    }
+
+    /**
+     * Tests blaming one issue with a fake git repository. Test run with a freestyle job.
+     *
+     * @throws Exception
+     *         if there is a problem with the git repository
+     */
+    @Test
+    public void shouldBlameOneIssueWithFreestyle() throws Exception {
+        gitRepo.init();
+        createAndCommitFile("Test.java", "public class Test {}");
+
+        final String testCommit = gitRepo.head();
+
+        FreeStyleProject project = createFreeStyleProject();
+        project.setScm(new GitSCM(gitRepo.toString()));
+        project.getBuildersList()
+                .add(new CreateFileBuilder("warnings.txt", "[javac] Test.java:1: warning: Test Warning for Jenkins"));
+
+        Java javaJob = new Java();
+        javaJob.setPattern("warnings.txt");
+        enableWarnings(project, javaJob);
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertSuccessfulBlame(result, 1, 1);
+
+        SourceControlTable table = getSourceControlTable(result);
+        assertOneIssue(testCommit, table);
+    }
+
+    /**
+     * Tests blaming eleven issues with a fake git repository. Test run from a pipeline script.
+     *
+     * @throws Exception
+     *         if there is a problem with the git repository
+     */
+    @Test
+    public void shouldBlameElevenIssuesWithPipeline() throws Exception {
+        HashMap<String, String> commits = createGitRepository();
+
+        createAndCommitFile("Jenkinsfile", "node {\n"
+                + "  stage ('Checkout') {\n"
+                + "    checkout scm\n"
+                + "  }\n"
+                + "  stage ('Build and Analysis') {"
+                + "    echo '[javac] Test.java:1: warning: Test Warning for Jenkins'\n"
+                + "    echo '[javac] Test.java:2: warning: Test Warning for Jenkins'\n"
+                + "    echo '[javac] Test.java:3: warning: Test Warning for Jenkins'\n"
+                + "    echo '[javac] Test.java:4: warning: Test Warning for Jenkins'\n"
+                + "    echo '[javac] LoremIpsum.java:1: warning: Another Warning for Jenkins'\n"
+                + "    echo '[javac] LoremIpsum.java:2: warning: Another Warning for Jenkins'\n"
+                + "    echo '[javac] LoremIpsum.java:3: warning: Another Warning for Jenkins'\n"
+                + "    echo '[javac] LoremIpsum.java:4: warning: Another Warning for Jenkins'\n"
+                + "    echo '[javac] Bob.java:1: warning: Bobs Warning for Jenkins'\n"
+                + "    echo '[javac] Bob.java:2: warning: Bobs Warning for Jenkins'\n"
+                + "    echo '[javac] Bob.java:3: warning: Bobs Warning for Jenkins'\n"
+                + "    recordIssues tools: [java()]\n"
+                + "  }\n"
+                + "}");
+
+        WorkflowJob project = createPipeline();
+        project.setDefinition(new CpsScmFlowDefinition(new GitSCM(gitRepo.toString()), "Jenkinsfile"));
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertSuccessfulBlame(result, 11, 3);
+
+        SourceControlTable table = getSourceControlTable(result);
+        assertElevenIssues(commits, table);
+    }
+
+    /**
+     * Tests blaming eleven issues with a fake git repository. Test run with a freestyle job.
+     *
+     * @throws Exception
+     *         if there is a problem with the git repository
+     */
+    @Test
+    public void shouldBlameElevenIssuesWithFreestyle() throws Exception {
+        HashMap<String, String> commits = createGitRepository();
+
+        FreeStyleProject project = createFreeStyleProject();
+        project.setScm(new GitSCM(gitRepo.toString()));
+        project.getBuildersList()
+                .add(new CreateFileBuilder("warnings.txt",
+                        "[javac] Test.java:1: warning: Test Warning for Jenkins\n"
+                                + "[javac] Test.java:2: warning: Test Warning for Jenkins\n"
+                                + "[javac] Test.java:3: warning: Test Warning for Jenkins\n"
+                                + "[javac] Test.java:4: warning: Test Warning for Jenkins\n"
+                                + "[javac] LoremIpsum.java:1: warning: Another Warning for Jenkins\n"
+                                + "[javac] LoremIpsum.java:2: warning: Another Warning for Jenkins\n"
+                                + "[javac] LoremIpsum.java:3: warning: Another Warning for Jenkins\n"
+                                + "[javac] LoremIpsum.java:4: warning: Another Warning for Jenkins\n"
+                                + "[javac] Bob.java:1: warning: Bobs Warning for Jenkins\n"
+                                + "[javac] Bob.java:2: warning: Bobs Warning for Jenkins\n"
+                                + "[javac] Bob.java:3: warning: Bobs Warning for Jenkins"));
+
+        Java javaJob = new Java();
+        javaJob.setPattern("warnings.txt");
+        enableWarnings(project, javaJob);
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertSuccessfulBlame(result, 11, 3);
+
+        SourceControlTable table = getSourceControlTable(result);
+        assertElevenIssues(commits, table);
+    }
+
+    /**
+     * Test filtering in the SourceControlTable.
+     *
+     * @throws Exception
+     *         if there is a problem with the git repository
+     */
+    @Test
+    public void shouldFilter() throws Exception {
+        HashMap<String, String> commits = createGitRepository();
+
+        createAndCommitFile("Jenkinsfile", "node {\n"
+                + "  stage ('Checkout') {\n"
+                + "    checkout scm\n"
+                + "  }\n"
+                + "  stage ('Build and Analysis') {"
+                + "    echo '[javac] Test.java:1: warning: Test Warning for Jenkins'\n"
+                + "    echo '[javac] Test.java:2: warning: Test Warning for Jenkins'\n"
+                + "    echo '[javac] Test.java:3: warning: Test Warning for Jenkins'\n"
+                + "    echo '[javac] Test.java:4: warning: Test Warning for Jenkins'\n"
+                + "    echo '[javac] LoremIpsum.java:1: warning: Another Warning for Jenkins'\n"
+                + "    echo '[javac] LoremIpsum.java:2: warning: Another Warning for Jenkins'\n"
+                + "    echo '[javac] LoremIpsum.java:3: warning: Another Warning for Jenkins'\n"
+                + "    echo '[javac] LoremIpsum.java:4: warning: Another Warning for Jenkins'\n"
+                + "    echo '[javac] Bob.java:1: warning: Bobs Warning for Jenkins'\n"
+                + "    echo '[javac] Bob.java:2: warning: Bobs Warning for Jenkins'\n"
+                + "    echo '[javac] Bob.java:3: warning: Bobs Warning for Jenkins'\n"
+                + "    recordIssues tools: [java()]\n"
+                + "  }\n"
+                + "}");
+
+        WorkflowJob project = createPipeline();
+        project.setDefinition(new CpsScmFlowDefinition(new GitSCM(gitRepo.toString()), "Jenkinsfile"));
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertSuccessfulBlame(result, 11, 3);
+
+        SourceControlTable table = getSourceControlTable(result);
+        assertElevenIssues(commits, table);
+
+        table.filter("LoremIpsum.java");
+        assertThat(table.getInfo()).isEqualTo("Showing 1 to 4 of 4 entries (filtered from 11 total entries)");
+        assertThat(table.getRows()).containsExactly(
+                new SourceControlRow("Another Warning for Jenkins", "LoremIpsum.java:1", "John Doe", "john@doe",
+                        commits.get("LoremIpsum"), 1),
+                new SourceControlRow("Another Warning for Jenkins", "LoremIpsum.java:2", "John Doe", "john@doe",
+                        commits.get("LoremIpsum"), 1),
+                new SourceControlRow("Another Warning for Jenkins", "LoremIpsum.java:3", "John Doe", "john@doe",
+                        commits.get("LoremIpsum"), 1),
+                new SourceControlRow("Another Warning for Jenkins", "LoremIpsum.java:4", "John Doe", "john@doe",
+                        commits.get("LoremIpsum"), 1));
+    }
+
+    /**
+     * Test if blaming works on a build out of tree. See JENKINS-57260.
+     *
+     * @throws Exception
+     *         if there is a problem with the git repository
+     */
+    @Issue("JENKINS-57260")
+    @Test
+    public void shouldBlameWithBuildOutOfTree() throws Exception {
+        gitRepo.init();
+        createAndCommitFile("Test.h", "#ifdef \"");
+
+        final String commit = gitRepo.head();
+
+        createAndCommitFile("Jenkinsfile", "pipeline {\n"
+                + "  agent any\n"
+                + "  options {\n"
+                + "    skipDefaultCheckout()\n"
+                + "  }\n"
+                + "  stages {\n"
+                + "    stage('Prepare') {\n"
+                + "      steps {\n"
+                + "        dir('src') {\n"
+                + "          checkout scm\n"
+                + "        }\n"
+                + "      }\n"
+                + "    }\n"
+                + "    stage('Doxygen') {\n"
+                + "      steps {\n"
+                + "        dir('build/doxygen') {\n"
+                + "          sh 'mkdir doxygen'\n"
+                + "          sh 'echo Test.h:1: Error: Unexpected character `\"`> doxygen/doxygen.log'\n"
+                + "        }\n"
+                + "        recordIssues(aggregatingResults: true, enabledForFailure: true, tools: [ doxygen(name: 'Doxygen', pattern: 'build/doxygen/doxygen/doxygen.log') ] )\n"
+                + "      }\n"
+                + "    }\n"
+                + "  }\n"
+                + "}");
+
+        WorkflowJob project = createPipeline();
+        project.setDefinition(new CpsScmFlowDefinition(new GitSCM(gitRepo.toString()), "Jenkinsfile"));
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertSuccessfulBlame(result, 1, 1);
+
+        SourceControlTable table = getSourceControlTable(result);
+        assertThat(table.getInfo()).isEqualTo("Showing 1 to 1 of 1 entries");
+        assertThat(table.getRows()).hasSize(1);
+        assertThat(table.getRows()).containsExactly(
+                new SourceControlRow("Unexpected character", "Test.h:1", "Git SampleRepoRule",
+                        "gits@mplereporule", commit, 1));
+    }
+
+    private SourceControlTable getSourceControlTable(final AnalysisResult result) {
+        HtmlPage page = getWebPage(JavaScriptSupport.JS_ENABLED, result);
+
+        DetailsTab detailsTab = new DetailsTab(page);
+        assertThat(detailsTab.getTabTypes()).contains(TabType.BLAMES);
+
+        return detailsTab.select(TabType.BLAMES);
+    }
+
+    private void assertSuccessfulBlame(final AnalysisResult result, final int numberOfIssues, final int numberOfFiles) {
+        AnalysisResultAssert.assertThat(result).hasNoErrorMessages();
+        AnalysisResultAssert.assertThat(result).hasTotalSize(numberOfIssues);
+        AnalysisResultAssert.assertThat(result)
+                .hasInfoMessages(
+                        "Created blame requests for " + numberOfFiles
+                                + " files - invoking Git blame on agent for each of the requests",
+                        "-> blamed authors of issues in " + numberOfFiles + " files");
+    }
+
+    private void assertOneIssue(final String commit, final SourceControlTable table) {
+        assertThat(table.getColumnNames())
+                .containsExactly(DETAILS, FILE, AGE, AUTHOR, EMAIL, COMMIT);
+        assertThat(table.getInfo()).isEqualTo("Showing 1 to 1 of 1 entries");
+        assertThat(table.getRows()).hasSize(1);
+        assertThat(table.getRows()).containsExactly(
+                new SourceControlRow("Test Warning for Jenkins", "Test.java:1", "Git SampleRepoRule",
+                        "gits@mplereporule", commit, 1));
+    }
+
+    private void assertElevenIssues(final HashMap<String, String> commits, final SourceControlTable table) {
+        assertThat(table.getColumnNames())
+                .containsExactly(DETAILS, FILE, AGE, AUTHOR, EMAIL, COMMIT);
+        assertThat(table.getInfo()).isEqualTo("Showing 1 to 10 of 11 entries");
+        assertThat(table.getRows()).containsExactly(
+                new SourceControlRow("Bobs Warning for Jenkins", "Bob.java:1", "Alice Miller", "alice@miller",
+                        commits.get("Bob"), 1),
+                new SourceControlRow("Bobs Warning for Jenkins", "Bob.java:2", "Alice Miller", "alice@miller",
+                        commits.get("Bob"), 1),
+                new SourceControlRow("Bobs Warning for Jenkins", "Bob.java:3", "Alice Miller", "alice@miller",
+                        commits.get("Bob"), 1),
+                new SourceControlRow("Another Warning for Jenkins", "LoremIpsum.java:1", "John Doe", "john@doe",
+                        commits.get("LoremIpsum"), 1),
+                new SourceControlRow("Another Warning for Jenkins", "LoremIpsum.java:2", "John Doe", "john@doe",
+                        commits.get("LoremIpsum"), 1),
+                new SourceControlRow("Another Warning for Jenkins", "LoremIpsum.java:3", "John Doe", "john@doe",
+                        commits.get("LoremIpsum"), 1),
+                new SourceControlRow("Another Warning for Jenkins", "LoremIpsum.java:4", "John Doe", "john@doe",
+                        commits.get("LoremIpsum"), 1),
+                new SourceControlRow("Test Warning for Jenkins", "Test.java:1", "Git SampleRepoRule",
+                        "gits@mplereporule", commits.get("Test"), 1),
+                new SourceControlRow("Test Warning for Jenkins", "Test.java:2", "Git SampleRepoRule",
+                        "gits@mplereporule", commits.get("Test"), 1),
+                new SourceControlRow("Test Warning for Jenkins", "Test.java:3", "Git SampleRepoRule",
+                        "gits@mplereporule", commits.get("Test"), 1));
+
+        table.goToPage(2);
+        assertThat(table.getInfo()).isEqualTo("Showing 11 to 11 of 11 entries");
+        assertThat(table.getRows()).containsExactly(
+                new SourceControlRow("Test Warning for Jenkins", "Test.java:4", "Git SampleRepoRule",
+                        "gits@mplereporule", commits.get("Test"), 1)
+        );
+    }
+
+    private HashMap<String, String> createGitRepository() throws Exception {
+        HashMap<String, String> commits = new HashMap<>();
+
+        gitRepo.init();
+        createAndCommitFile("Test.java", "public class Test {\n"
+                + "    public Test() {\n"
+                + "        System.out.println(\"Test\");"
+                + "    }\n"
+                + "}");
+
+        commits.put("Test", gitRepo.head());
+
+        gitRepo.git("config", "user.name", "John Doe");
+        gitRepo.git("config", "user.email", "john@doe");
+        createAndCommitFile("LoremIpsum.java", "public class LoremIpsum {\n"
+                + "    public LoremIpsum() {\n"
+                + "        Log.log(\"Lorem ipsum dolor sit amet\");"
+                + "    }\n"
+                + "}");
+
+        commits.put("LoremIpsum", gitRepo.head());
+
+        gitRepo.git("config", "user.name", "Alice Miller");
+        gitRepo.git("config", "user.email", "alice@miller");
+        createAndCommitFile("Bob.java", "public class Bob {\n"
+                + "    public Bob() {\n"
+                + "        Log.log(\"Bob: 'Where are you?'\");"
+                + "    }\n"
+                + "}");
+
+        commits.put("Bob", gitRepo.head());
+
+        return commits;
+    }
+
+    private void createAndCommitFile(final String fileName, final String content) throws Exception {
+        gitRepo.write(fileName, content);
+        gitRepo.git("add", fileName);
+        gitRepo.git("commit", "--message=" + fileName + " created");
+    }
+}
