8a085e0712fe7c1fabf8414bb639a9b3d33042b9
Sebastian Heunke <sebastian.heunke@hm.edu>
2019-05-29 19:56:10 +0200

    Started implementaiton of docker test

--- src/test/java/io/jenkins/plugins/analysis/warnings/recorder/DockerITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/recorder/DockerITest.java
@@ -8,19 +8,24 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.test.acceptance.docker.DockerContainer;
 import org.jenkinsci.test.acceptance.docker.DockerRule;
 import org.jenkinsci.test.acceptance.docker.fixtures.JavaContainer;
 import hudson.Functions;
 import hudson.Launcher;
 import hudson.model.Descriptor.FormException;
+import hudson.model.Result;
 import hudson.plugins.sshslaves.SSHLauncher;
 import hudson.slaves.DumbSlave;
 import hudson.util.StreamTaskListener;
 import hudson.util.VersionNumber;
 
+import io.jenkins.plugins.analysis.core.model.AnalysisResult;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerTest;
 
+import static io.jenkins.plugins.analysis.core.assertions.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assume.*;
@@ -29,7 +34,7 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
     @Rule
     public DockerRule<JavaContainer> javaDockerRule = new DockerRule<>(JavaContainer.class);
 
-    /*
+
     @BeforeClass
     public static void assumeThatWeAreRunningLinux() throws Exception {
         assumeTrue("This test is only for Unix", !Functions.isWindows());
@@ -44,13 +49,30 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
         assumeThat("Docker must be at least 1.13.0 for this test (uses --init)",
                 new VersionNumber(baos.toString().trim()), greaterThanOrEqualTo(new VersionNumber("1.13.0")));
     }
-    */
+
+    @Test
+    public void shouldDoMavenBuildOnSlave() {
+        DumbSlave agent = createAgent();
+
+        WorkflowJob project = createPipeline();
+
+        // TODO: Put actual content in Hello.java
+        // This can be used to trigger a "warning" new org.xml.sax.helpers.AttributeListImpl()
+        // TODO: Trigger actual maven build
+        createFileInAgentWorkspace(agent, project, "Hello.java", "public class Hello extends Old {}");
+        createFileInAgentWorkspace(agent, project, "javac.txt", "[WARNING] Hello.java:[1,42] [deprecation] Something uses Old.class\n");
+
+        project.setDefinition(new CpsFlowDefinition("node('slave') {recordIssues tool: java(pattern: '**/*.txt')}", true));
+        AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        assertThat(result).hasTotalSize(1);
+    }
+
 
     @Test
     public void shouldStartAgent() {
         DumbSlave agent = createAgent();
 
-        assertThat(agent.getWorkspaceRoot().getName()).isEqualTo("/home/test/workspace");
+        assertThat(agent.getWorkspaceRoot().toString()).isEqualTo("/home/test/workspace");
     }
 
     private DumbSlave createAgent() {
