bc27b297dd1add27b8e8c6303d35f2db27cb50ad
Tanja <roithmeier.tanja@gmail.com>
2019-06-11 18:22:51 +0200

    Fix checkstyle warnings

--- src/test/java/io/jenkins/plugins/analysis/warnings/GccContainer.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/GccContainer.java
@@ -3,12 +3,18 @@ package io.jenkins.plugins.analysis.warnings;
 import org.jenkinsci.test.acceptance.docker.DockerFixture;
 import org.jenkinsci.test.acceptance.docker.fixtures.SshdContainer;
 
+/**
+ * Creates a docker container with gcc/make installed.
+ */
 @DockerFixture(
         id = "gcc",
         ports = {22, 8080}
 )
 public class GccContainer extends SshdContainer {
 
-    void GccContainer(){
+    /**
+     * The default constructor.
+     */
+    public GccContainer() {
     }
 }
