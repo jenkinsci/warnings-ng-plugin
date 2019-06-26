7a62ca9a2d5e4fe6edb1ac64c92425550132011e
LorMun <Lorenz.Munsch@gmx.de>
2019-06-26 14:11:10 +0200

    added missing testcases

--- src/test/java/io/jenkins/plugins/analysis/warnings/DockerITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/DockerITest.java
@@ -2,13 +2,20 @@ package io.jenkins.plugins.analysis.warnings;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import org.apache.commons.io.output.TeeOutputStream;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
-
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import hudson.slaves.EnvironmentVariablesNodeProperty.Entry;
 import org.jenkinsci.test.acceptance.docker.DockerContainer;
 import org.jenkinsci.test.acceptance.docker.DockerRule;
+import org.jenkinsci.test.acceptance.docker.fixtures.JavaContainer;
+import hudson.FilePath;
 import hudson.Functions;
 import hudson.Launcher;
 import hudson.model.Descriptor.FormException;
@@ -16,11 +23,13 @@ import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 import hudson.plugins.sshslaves.SSHLauncher;
 import hudson.slaves.DumbSlave;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
 import hudson.tasks.Shell;
 import hudson.util.StreamTaskListener;
 import hudson.util.VersionNumber;
+import hudson.tasks.Maven;
 
-
+import jenkins.security.s2m.AdminWhitelistRule;
 
 import io.jenkins.plugins.analysis.core.model.AnalysisResult;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerTest;
@@ -31,10 +40,24 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assume.*;
 
 public class DockerITest extends IntegrationTestWithJenkinsPerTest {
-    
+
+    /**
+     * Java Docker Rule
+     */
+    @Rule
+    public DockerRule<JavaContainer> javaDockerRule = new DockerRule<>(JavaContainer.class);
+
+    /**
+     * Gcc Docker Rule
+     */
     @Rule
     public DockerRule<GccContainer> gccDockerRule = new DockerRule<>(GccContainer.class);
 
+    /**
+     * Check that it is executed on linux.
+     *
+     * @throws Exception if there was a problem with creating Java agent.
+     */
     @BeforeClass
     public static void assumeThatWeAreRunningLinux() throws Exception {
         assumeTrue("This test is only for Unix", !Functions.isWindows());
@@ -48,6 +71,32 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
 
         assumeThat("Docker must be at least 1.13.0 for this test (uses --init)",
                 new VersionNumber(baos.toString().trim()), greaterThanOrEqualTo(new VersionNumber("1.13.0")));
+    }
+
+    /**
+     * Integrationstest Aufgabe 2. Building with Java Files.
+     *
+     * @throws Exception if there was a problem with creating Java agent.
+     */
+    @Test
+    public void shouldBuildJavaFileOnDumbSlave() throws Exception {
+        DumbSlave slave = jenkinsPerTest.createOnlineSlave();
+
+        FreeStyleProject project = createFreeStyleProject();
+        try {
+            project.setAssignedNode(slave);
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+
+        project.getBuildersList().add(new Maven("verify", null));
+        copySingleFileToAgentWorkspace(slave, project, "JavaHelloWorldForDockerITest.java",
+                "src/main/java/HelloWorld.java");
+        copySingleFileToAgentWorkspace(slave, project, "PomForDockerITest.xml", "pom.xml");
+        enableWarnings(project, createTool(new MavenConsole(), ""));
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertThat(result).isNotNull();
     }
 
     /**
@@ -75,6 +124,104 @@ public class DockerITest extends IntegrationTestWithJenkinsPerTest {
 
         AnalysisResult result = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
         assertThat(result).isNotNull();
+    }
+
+    /**
+     * Integrationstest Aufgabe 2. Building with Security enabled.
+     *
+     * @throws Exception if there was a problem with creating Java agent.
+     */
+    @Test
+    public void shouldBuildJavaFileOnDumbSlaveWithSecurityEnabled() throws Exception {
+        DumbSlave slave = createDumbSlaveWithEnabledSecurity();
+        FreeStyleProject project = createFreeStyleProject();
+        try {
+            project.setAssignedNode(slave);
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+
+        project.getBuildersList().add(new Maven("verify", null));
+        copySingleFileToAgentWorkspace(slave, project, "JavaHelloWorldForDockerITest.java",
+                "src/main/java/HelloWorld.java");
+        copySingleFileToAgentWorkspace(slave, project, "PomForDockerITest.xml", "pom.xml");
+        enableWarnings(project, createTool(new MavenConsole(), ""));
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertThat(result).isNotNull();
+    }
+
+    /**
+     * Class for building DumbSlave with Enabled Security.
+     *
+     * @return DumbSlave with enabled Security.
+     * @throws Exception if there was a problem with creating Java agent.
+     */
+    public DumbSlave createDumbSlaveWithEnabledSecurity() throws Exception {
+        DumbSlave agent = jenkinsPerTest.createOnlineSlave();
+
+        FilePath child = getJenkins().getInstance().getRootPath().child("secrets/filepath-filters.d/30-default.conf");
+        child.delete();
+        child.write("", "ISO_8859_1");
+
+        Objects.requireNonNull(getJenkins().jenkins.getInjector()).getInstance(AdminWhitelistRule.class)
+                .setMasterKillSwitch(false);
+        getJenkins().jenkins.save();
+        return agent;
+    }
+
+    /**
+     * Creates Java Agent
+     */
+    @Test
+    public void shouldStartAgent() {
+        DumbSlave agent = createJavaAgent();
+
+        assertThat(agent.getWorkspaceRoot().getName()).isEqualTo("workspace");
+    }
+
+    /*
+     * Integrationstest Aufgabe 3., Building With Java Container
+     */
+    @Test
+    public void shouldBuildJavaProjectWithContainer() {
+        DumbSlave agent = createJavaAgent();
+
+        FreeStyleProject project = createFreeStyleProject();
+        try {
+            project.setAssignedNode(agent);
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+
+        project.getBuildersList().add(new Maven("verify", null));
+        copySingleFileToAgentWorkspace(agent, project, "JavaHelloWorldForDockerITest.java",
+                "src/main/java/HelloWorld.java");
+        copySingleFileToAgentWorkspace(agent, project, "PomForDockerITest.xml", "pom.xml");
+        enableWarnings(project, createTool(new MavenConsole(), ""));
+
+        AnalysisResult result = scheduleSuccessfulBuild(project);
+        assertThat(result).isNotNull();
+    }
+
+    private DumbSlave createJavaAgent() {
+        try {
+            JavaContainer javaContainer = javaDockerRule.get();
+
+            DumbSlave agent = createAgent(javaContainer);
+
+            List<EnvironmentVariablesNodeProperty> properties = new ArrayList<>();
+            properties.add(new EnvironmentVariablesNodeProperty(
+                    new Entry("JAVA_HOME", "/usr/lib/jvm/java-8-openjdk-amd64/jre")));
+            agent.setNodeProperties(properties);
+            getJenkins().jenkins.addNode(agent);
+            getJenkins().waitOnline(agent);
+
+            return agent;
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        }
+
     }
 
     /**
