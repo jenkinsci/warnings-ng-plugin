16da2259beb4516922506b4dfeef8e59f2b7e00d
Name From Git-Plugin-Test <email.from.git.plugin.test@example.com>
2019-07-17 10:19:02 +0300

    Added PVS-Studio analyzer

---
+++ src/main/java/io/jenkins/plugins/analysis/warnings/PVSStudio.java
@@ -0,0 +1,73 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import edu.hm.hafner.analysis.IssueParser;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.jenkinsci.Symbol;
+import hudson.Extension;
+
+import io.jenkins.plugins.analysis.core.model.IconLabelProvider;
+import io.jenkins.plugins.analysis.core.model.ReportScanningTool;
+import io.jenkins.plugins.analysis.core.model.StaticAnalysisLabelProvider;
+
+import edu.hm.hafner.analysis.parser.pvsstudio.PVSStudioParser;
+
+/**
+ * Provides a parser and customized messages for PVS-Studio static analyzer.
+ *
+ * @author PVS-Studio Team
+ */
+public class PVSStudio extends ReportScanningTool {
+
+    private static final long serialVersionUID = -1114828406964963020L;
+    private static final String ID = "pvs-studio"; // history chart title
+
+    @DataBoundConstructor
+    public PVSStudio() {
+        super();
+        // empty constructor required for stapler
+    }
+
+    @Override
+    public IssueParser createParser() {
+        return new PVSStudioParser();
+    }
+
+    private static class LabelProvider extends IconLabelProvider {
+
+        LabelProvider(final String id, final String name, final String iconName) {
+            super(id, name, iconName);
+        }
+    }
+
+    /** Descriptor for this static analysis tool. */
+    @Symbol("PVS-Studio")
+    @Extension
+    public static class Descriptor extends ReportScanningToolDescriptor {
+
+        /** Creates the descriptor instance. */
+        public Descriptor() {
+            super(ID);
+        }
+
+        @NonNull
+        @Override
+        public String getDisplayName() {
+            return "PVS-Studio";
+        } // title page
+
+        @Override
+        public StaticAnalysisLabelProvider getLabelProvider() {
+            return new LabelProvider(ID, "PVS-Studio", "pvs");
+        }
+
+        @Override
+        public String getPattern() {
+            return "**/**.plog";
+        }
+    }
+
+}
+
+
