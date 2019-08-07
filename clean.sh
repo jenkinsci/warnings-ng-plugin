699cdfb2088151276ae6542da62bfa8ae68bcd9c
Ulli Hafner <ullrich.hafner@gmail.com>
2019-08-07 21:55:27 +0200

    Improve detection of running Jenkins in Docker container.

--- clean.sh
+++ clean.sh
@@ -11,7 +11,7 @@ echo "Installing plugin in $JENKINS_HOME"
 rm -rf $JENKINS_HOME/plugins/warnings-ng*
 cp -fv target/warnings-ng.hpi $JENKINS_HOME/plugins/warnings-ng.jpi
 
-IS_RUNNING=`docker-compose ps -q jenkins-master`
+IS_RUNNING=$(docker-compose ps -q jenkins-master)
 if [[ "$IS_RUNNING" != "" ]]; then
     echo "Restarting Jenkins..."
     docker-compose restart
