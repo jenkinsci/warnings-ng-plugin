ef7b4817ae7216c7b232af83537245752e873121
Ulli Hafner <ullrich.hafner@gmail.com>
2019-11-13 10:22:24 +0100

    Fix repeatable delete button if all elements have been removed.

--- src/main/java/io/jenkins/plugins/analysis/core/util/GlobalConfigurationItem.java
+++ src/main/java/io/jenkins/plugins/analysis/core/util/GlobalConfigurationItem.java
@@ -3,6 +3,9 @@ package io.jenkins.plugins.analysis.core.util;
 import edu.hm.hafner.util.VisibleForTesting;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.StaplerRequest;
 import jenkins.model.GlobalConfiguration;
 
 /**
@@ -56,13 +59,30 @@ public class GlobalConfigurationItem extends GlobalConfiguration {
         return this;
     }
 
+    @Override
+    public boolean configure(final StaplerRequest req, final JSONObject json) throws FormException {
+        clearRepeatableProperties();
 
-    @Override @SuppressWarnings("PMD.AvoidSynchronizedAtMethodLevel")
+        return super.configure(req, json);
+    }
+
+    /**
+     * Clears all model elements of a repeatable property. Due to a bug in Stapler data binding of repeatable properties
+     * the model elements are only changed if they consist of one or more values. If all values have been removed and
+     * the associated form is empty, then the setter is not invoked anymore.
+     */
+    protected void clearRepeatableProperties() {
+        // empty default implementation
+    }
+
+    @Override
+    @SuppressWarnings("PMD.AvoidSynchronizedAtMethodLevel")
     public final synchronized void load() {
         actualLoad.run();
     }
 
-    @Override @SuppressWarnings("PMD.AvoidSynchronizedAtMethodLevel")
+    @Override
+    @SuppressWarnings("PMD.AvoidSynchronizedAtMethodLevel")
     public final synchronized void save() {
         actualSave.run();
     }
