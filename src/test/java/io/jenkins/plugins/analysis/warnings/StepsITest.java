21b4dbdd954db23e473a6cef0fe2570bf6362d97
Ulli Hafner <ullrich.hafner@gmail.com>
2020-02-23 11:42:23 +0100

    Add getAuthor to Whitelisting test.

--- src/test/java/io/jenkins/plugins/analysis/warnings/StepsITest.java
+++ src/test/java/io/jenkins/plugins/analysis/warnings/StepsITest.java
@@ -88,6 +88,7 @@ public class StepsITest extends IntegrationTestWithJenkinsPerTest {
                 + "         issues.each { issue ->\n"
                 + "             echo issue.toString()\n"
                 + "             echo issue.getOrigin()\n"
+                + "             echo issue.getAuthorName()\n"
                 + "         }"
                 + "  }\n"
                 + "}", true));
