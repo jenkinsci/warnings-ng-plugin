ed2072941c5e2093ea8309cbbc753d1fe87684df
ThoGro <grossbec@hm.edu>
2020-05-28 00:26:17 +0200

    Test Metrics Aggregation Plugin with MetricsViewAction

--- plugin/src/test/java/io/jenkins/plugins/analysis/warnings/plugins/MetricsAggregationPluginITest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/warnings/plugins/MetricsAggregationPluginITest.java
@@ -3,12 +3,17 @@ package io.jenkins.plugins.analysis.warnings.plugins;
 import org.junit.Test;
 
 import hudson.model.FreeStyleProject;
-import hudson.model.Result;
+import hudson.model.Run;
 
-import io.jenkins.plugins.analysis.core.model.AnalysisResult;
+import io.jenkins.plugins.analysis.core.steps.IssuesRecorder;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite;
+import io.jenkins.plugins.analysis.warnings.Cpd;
 import io.jenkins.plugins.analysis.warnings.Java;
-import io.jenkins.plugins.metrics.analysis.steps.MetricsRecorder;
+import io.jenkins.plugins.analysis.warnings.Pmd;
+import io.jenkins.plugins.analysis.warnings.SpotBugs;
+import io.jenkins.plugins.metrics.model.metric.MetricDefinition;
+import io.jenkins.plugins.metrics.view.MetricsView;
+import io.jenkins.plugins.metrics.view.MetricsViewAction;
 
 import static io.jenkins.plugins.analysis.core.assertions.Assertions.*;
 
@@ -18,14 +23,23 @@ public class MetricsAggregationPluginITest extends IntegrationTestWithJenkinsPer
     public void shouldAggregateMetrics() {
         FreeStyleProject project = createJavaWarningsFreestyleProject("pmd.xml", "cpd.xml", "spotbugsXml.xml");
 
-        MetricsRecorder recorder = new MetricsRecorder();
-        recorder.setFilePattern("**/*.txt");
-
+        IssuesRecorder recorder = new IssuesRecorder();
+        Pmd pmd = new Pmd();
+        pmd.setPattern("**/pmd*.txt");
+        Cpd cpd = new Cpd();
+        cpd.setPattern("**/cpd*.txt");
+        SpotBugs spotBugs = new SpotBugs();
+        spotBugs.setPattern("**/spotbugs*.txt");
+        recorder.setTools(pmd, cpd, spotBugs);
         project.getPublishersList().add(recorder);
 
-        AnalysisResult analysisResult = scheduleBuildAndAssertStatus(project, Result.SUCCESS);
+        Run<?, ?> build = buildSuccessfully(project);
+        MetricsView view = (MetricsView) build.getAction(MetricsViewAction.class).getTarget();
 
-        assertThat(analysisResult).hasTotalSize(3);
+        assertThat(view.getSupportedMetrics()).contains(
+                new MetricDefinition("ERRORS"),new MetricDefinition("WARNING_HIGH"), new MetricDefinition("WARNING_NORMAL"),
+                new MetricDefinition("WARNING_LOW"), new MetricDefinition("AUTHORS"), new MetricDefinition("COMMITS")
+        );
     }
 
     private FreeStyleProject createJavaWarningsFreestyleProject(final String... files) {
