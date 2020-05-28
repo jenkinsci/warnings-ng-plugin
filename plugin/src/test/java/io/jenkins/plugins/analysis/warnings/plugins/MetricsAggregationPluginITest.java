06536df754672bf9e59683c84afab93e7e52f99d
Ulli Hafner <ullrich.hafner@gmail.com>
2020-05-28 20:52:20 +0200

    Add JavaDoc.

--- plugin/src/test/java/io/jenkins/plugins/analysis/warnings/plugins/MetricsAggregationPluginITest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/warnings/plugins/MetricsAggregationPluginITest.java
@@ -8,7 +8,6 @@ import hudson.model.Run;
 import io.jenkins.plugins.analysis.core.steps.IssuesRecorder;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerSuite;
 import io.jenkins.plugins.analysis.warnings.Cpd;
-import io.jenkins.plugins.analysis.warnings.Java;
 import io.jenkins.plugins.analysis.warnings.Pmd;
 import io.jenkins.plugins.analysis.warnings.SpotBugs;
 import io.jenkins.plugins.metrics.model.metric.MetricDefinition;
@@ -17,37 +16,30 @@ import io.jenkins.plugins.metrics.view.MetricsViewAction;
 
 import static io.jenkins.plugins.analysis.core.assertions.Assertions.*;
 
+/**
+ * This class tests the compatibility between the warnings-ng and the Metrics Aggregation plugin. It makes sure
+ * the default integration (without creating a publisher) works, so that the metrics are available if there is
+ * a static analysis result.
+ */
 public class MetricsAggregationPluginITest extends IntegrationTestWithJenkinsPerSuite {
-
+    /** Verifies that the metrics action is available automatically. */
     @Test
     public void shouldAggregateMetrics() {
-        FreeStyleProject project = createJavaWarningsFreestyleProject("pmd.xml", "cpd.xml", "spotbugsXml.xml");
+        FreeStyleProject project = createFreeStyleProjectWithWorkspaceFiles("pmd.xml", "cpd.xml", "spotbugsXml.xml");
 
         IssuesRecorder recorder = new IssuesRecorder();
-        Pmd pmd = new Pmd();
-        pmd.setPattern("**/pmd*.txt");
-        Cpd cpd = new Cpd();
-        cpd.setPattern("**/cpd*.txt");
-        SpotBugs spotBugs = new SpotBugs();
-        spotBugs.setPattern("**/spotbugs*.txt");
-        recorder.setTools(pmd, cpd, spotBugs);
+        recorder.setTools(createTool(new Pmd(), "**/pmd*.txt"),
+                createTool(new Cpd(), "**/cpd*.txt"),
+                createTool(new SpotBugs(), "**/spotbugs*.txt"));
         project.getPublishersList().add(recorder);
 
         Run<?, ?> build = buildSuccessfully(project);
         MetricsView view = (MetricsView) build.getAction(MetricsViewAction.class).getTarget();
 
         assertThat(view.getSupportedMetrics()).contains(
-                new MetricDefinition("ERRORS"),new MetricDefinition("WARNING_HIGH"), new MetricDefinition("WARNING_NORMAL"),
-                new MetricDefinition("WARNING_LOW"), new MetricDefinition("AUTHORS"), new MetricDefinition("COMMITS")
+                new MetricDefinition("ERRORS"), new MetricDefinition("WARNING_HIGH"),
+                new MetricDefinition("WARNING_NORMAL"), new MetricDefinition("WARNING_LOW"),
+                new MetricDefinition("AUTHORS"), new MetricDefinition("COMMITS")
         );
     }
-
-    private FreeStyleProject createJavaWarningsFreestyleProject(final String... files) {
-        FreeStyleProject project = createFreeStyleProjectWithWorkspaceFiles(files);
-        Java java = new Java();
-        java.setPattern("**/*.txt");
-        enableWarnings(project, java);
-        return project;
-    }
-
 }
