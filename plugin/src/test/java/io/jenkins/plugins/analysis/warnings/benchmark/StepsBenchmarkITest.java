4b3c8d936eaab8c2816a8701f3f45214cf1b525c
Ulli Hafner <ullrich.hafner@gmail.com>
2020-06-06 19:17:20 +0200

    Fix architecture problems.

--- plugin/src/test/java/io/jenkins/plugins/analysis/warnings/benchmark/StepsBenchmarkITest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/warnings/benchmark/StepsBenchmarkITest.java
@@ -6,22 +6,18 @@ import org.openjdk.jmh.annotations.Benchmark;
 
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import hudson.model.Cause;
 import jenkins.benchmark.jmh.JmhBenchmark;
 import jenkins.benchmark.jmh.JmhBenchmarkState;
-import jenkins.model.Jenkins;
 
 @JmhBenchmark
-public class StepsBenchmark {
+public class StepsBenchmarkITest {
 
     public static class JenkinsState extends JmhBenchmarkState {
         WorkflowJob job;
 
         @Override
         public void setup() throws Exception {
-            Jenkins jenkins = Jenkins.getInstanceOrNull();
-            job = jenkins.createProject(WorkflowJob.class, "Benchmark Job");
+            job = getJenkins().createProject(WorkflowJob.class, "Benchmark Job");
             configureScanner(job, "checkstyle1");
         }
 
@@ -46,12 +42,9 @@ public class StepsBenchmark {
         }
     }
 
-
     @Benchmark
     public void benchmark(JenkinsState state) throws IOException {
-
         state.getJob().scheduleBuild2(0);
-
     }
 
 
