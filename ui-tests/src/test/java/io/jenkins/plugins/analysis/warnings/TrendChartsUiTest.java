bf33e411f9e48b44e21e9690dff00c6ed18d4f28
Mitja <m.oldenbourg@hm.edu>
2020-06-16 18:51:35 +0200

    broken git. Hope to fix it. Changes: executeScript now returns correct JSON - Big error in JSON.stringify. AssertThatJson.inPath somehow does not work. Still figuring this out.

--- ui-tests/src/test/java/io/jenkins/plugins/analysis/warnings/TrendChartsUiTest.java
+++ ui-tests/src/test/java/io/jenkins/plugins/analysis/warnings/TrendChartsUiTest.java
@@ -4,11 +4,14 @@ import net.minidev.json.JSONObject;
 import net.minidev.json.JSONArray;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import net.minidev.json.parser.JSONParser;
 import net.minidev.json.parser.ParseException;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
+import static net.javacrumbs.jsonunit.jsonpath.JsonPathAdapter.inPath;
+
 
 import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
 import org.jenkinsci.test.acceptance.junit.WithPlugins;
@@ -27,7 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TrendChartsUiTest extends AbstractJUnitTest {
     private static final String WARNINGS_PLUGIN_PREFIX = "/";
     private static final String SOURCE_VIEW_FOLDER = WARNINGS_PLUGIN_PREFIX + "trend_charts_tests/";
-    private static final JSONParser JSON_PARSER = new JSONParser(JSONParser.MODE_PERMISSIVE);
+    private static final JSONParser JSON_PARSER = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
 
     /**
      * Click on next-button switches between different Chart-types.
@@ -45,18 +48,19 @@ public class TrendChartsUiTest extends AbstractJUnitTest {
 
         AnalysisResult analysisResultPage = new AnalysisResult(build, "java");
         analysisResultPage.open();
-        WebElement trendChart = analysisResultPage.getTrendChart();
-        WebElement nextButton = trendChart.findElement(By.className("carousel-control-next-icon"));
+        analysisResultPage.clickNextOnTrendCarousel();
 
-        assertThat(trendChart.findElement(By.id("severities-trend-chart")).isDisplayed()).isTrue();
+        assertThat(analysisResultPage.trendChartIsDisplayed("severities-trend-chart"));
 
-        nextButton.click();
+        analysisResultPage.clickNextOnTrendCarousel();
+        waitFor().until(() -> !analysisResultPage.trendChartIsDisplayed("tools-trend-chart"));
 
-        assertThat(trendChart.findElement(By.id("tools-trend-chart")).isDisplayed()).isTrue();
+        assertThat(analysisResultPage.trendChartIsDisplayed("tools-trend-chart"));
 
-        nextButton.click();
+        analysisResultPage.clickNextOnTrendCarousel();
+        waitFor().until(() -> !analysisResultPage.trendChartIsDisplayed("new-versus-fixed-trend-chart"));
 
-        assertThat(trendChart.findElement(By.id("new-versus-fixed-trend-chart")).isDisplayed()).isTrue();
+        assertThat(analysisResultPage.trendChartIsDisplayed("new-versus-fixed-trend-chart"));
     }
 
     /** Verifies all Charts after a series of 2 builds. */
@@ -65,85 +69,110 @@ public class TrendChartsUiTest extends AbstractJUnitTest {
         Build build = buildFreeStyleJobTwiceWithJavacIssues();
         AnalysisResult analysisResultPage = new AnalysisResult(build, "java");
         analysisResultPage.open();
-        JSONObject toolsTrendChart = analysisResultPage.getToolsTrendChart();
-        JSONObject newVersusFixedTrendChart = analysisResultPage.getNewVersusFixedTrendChart();
-        JSONObject severitiesTrendChart = analysisResultPage.getSeveritiesTrendChart();
+
+        String severitiesTrendChart = analysisResultPage.getSeveritiesTrendChart();
+        String toolsTrendChart = analysisResultPage.getToolsTrendChart();
+        String newVersusFixedTrendChart = analysisResultPage.getNewVersusFixedTrendChart();
+
+        verifySeveritiesChart(severitiesTrendChart);
         verifyToolsChart(toolsTrendChart);
         verifyNewVersusFixedChart(newVersusFixedTrendChart);
-        verifySeveritiesChart(severitiesTrendChart);
     }
 
-    /** Verifies Severity Chart after a series of 2 builds. */
-    private void verifySeveritiesChart(final JSONObject severitiesTrendChart) throws ParseException {
-        JSONArray xAxisArray = (JSONArray) JSON_PARSER.parse(severitiesTrendChart.getAsString("xAxis"));
-        JSONObject xAxisObject = (JSONObject) xAxisArray.get(0);
-        JSONArray xAxisData = (JSONArray) xAxisObject.get("data");
+    /**
+     * Verifies Severity Chart after a series of 2 builds.
+     *
+     * @param severitiesTrendChart
+     *         ONObject with values from Severities Trendchart
+     */
+    private void verifySeveritiesChart(final String severitiesTrendChart) {
 
-        assertThat(xAxisData.size()).isEqualTo(2);
+        assertThatJson(json(severitiesTrendChart)).inPath("$.xAxis[*].data").isArray().isEqualTo(json("[[\"#1\", \"#2\"]]"));
+                //.node("data").isArray().isEqualTo(json("[\"#1\", \"#2\"]"));
 
-        JSONArray seriesArray = (JSONArray) JSON_PARSER.parse(severitiesTrendChart.getAsString("series"));
-
-        assertThat(seriesArray.size()).isEqualTo(2);
-
-        JSONObject seriesObjectNormal = (JSONObject) seriesArray.get(0);
-        JSONObject seriesObjectError = (JSONObject) seriesArray.get(1);
-        String seriesNewName = seriesObjectNormal.getAsString("name");
-        String seriesFixedName = seriesObjectError.getAsString("name");
-
-        assertThat(seriesNewName).isEqualTo("Normal");
-        assertThat(seriesFixedName).isEqualTo("Error");
-
-        JSONArray seriesNewData = (JSONArray) seriesObjectNormal.get("data");
-        JSONArray seriesFixedData = (JSONArray) seriesObjectError.get("data");
-
-        assertThat(convertToIntArray(seriesNewData)).isEqualTo(new int[] {4, 2});
-        assertThat(convertToIntArray(seriesFixedData)).isEqualTo(new int[] {0, 1});
+//
+//        assertThat(xAxisData.size()).isEqualTo(2);
+//
+//        JSONArray seriesArray = (JSONArray) JSON_PARSER.parse(severitiesTrendChart.getAsString("series"));
+//
+//        assertThat(seriesArray.size()).isEqualTo(2);
+//
+//        JSONObject seriesObjectNormal = (JSONObject) seriesArray.get(0);
+//        JSONObject seriesObjectError = (JSONObject) seriesArray.get(1);
+//        String seriesNewName = seriesObjectNormal.getAsString("name");
+//        String seriesFixedName = seriesObjectError.getAsString("name");
+//
+//        assertThat(seriesNewName).isEqualTo("Normal");
+//        assertThat(seriesFixedName).isEqualTo("Error");
+//
+//        JSONArray seriesNewData = (JSONArray) seriesObjectNormal.get("data");
+//        JSONArray seriesFixedData = (JSONArray) seriesObjectError.get("data");
+//
+//        assertThat(convertToIntArray(seriesNewData)).isEqualTo(new int[] {4, 2});
+//        assertThat(convertToIntArray(seriesFixedData)).isEqualTo(new int[] {0, 1});
     }
 
-    /** Verifies Tools Chart after a series of 2 builds. */
-    private void verifyToolsChart(final JSONObject toolsTrendChart) throws ParseException {
-        JSONArray xAxisArray = (JSONArray) JSON_PARSER.parse(toolsTrendChart.getAsString("xAxis"));
-        JSONObject xAxisObject = (JSONObject) xAxisArray.get(0);
-        JSONArray xAxisData = (JSONArray) xAxisObject.get("data");
+    /**
+     * Verifies Tools Chart after a series of 2 builds.
+     *
+     * @param toolsTrendChart
+     *         ONObject with values from Severities Tools TrendChart
+     */
+    private void verifyToolsChart(final String toolsTrendChart) throws ParseException {
+        assertThatJson(toolsTrendChart).isArray();
 
-        assertThat(xAxisData.size()).isEqualTo(2);
 
-        JSONArray seriesArray = (JSONArray) JSON_PARSER.parse(toolsTrendChart.getAsString("series"));
-        JSONObject seriesObject = (JSONObject) seriesArray.get(0);
-        String seriesName = seriesObject.getAsString("name");
 
-        assertThat(seriesName).isEqualTo("java");
-
-        JSONArray seriesData = (JSONArray) seriesObject.get("data");
-
-        assertThat(convertToIntArray(seriesData)).isEqualTo(new int[] {4, 3});
+//                .hasSize(2)
+//                .contains(first)
+//                .contains(second);
+//        JSONArray xAxisArray = (JSONArray) JSON_PARSER.parse(toolsTrendChart.getAsString("xAxis"));
+//        JSONObject xAxisObject = (JSONObject) xAxisArray.get(0);
+//        JSONArray xAxisData = (JSONArray) xAxisObject.get("data");
+//
+//        assertThat(xAxisData.size()).isEqualTo(2);
+//
+//        JSONArray seriesArray = (JSONArray) JSON_PARSER.parse(toolsTrendChart.getAsString("series"));
+//        JSONObject seriesObject = (JSONObject) seriesArray.get(0);
+//        String seriesName = seriesObject.getAsString("name");
+//
+//        assertThat(seriesName).isEqualTo("java");
+//
+//        JSONArray seriesData = (JSONArray) seriesObject.get("data");
+//
+//        assertThat(convertToIntArray(seriesData)).isEqualTo(new int[] {4, 3});
     }
 
-    /** Verifies New-Versus-Fixed Chart after a series of 2 builds. */
-    private void verifyNewVersusFixedChart(final JSONObject newVersusFixedTrendChart) throws ParseException {
-        JSONArray xAxisArray = (JSONArray) JSON_PARSER.parse(newVersusFixedTrendChart.getAsString("xAxis"));
-        JSONObject xAxisObject = (JSONObject) xAxisArray.get(0);
-        JSONArray xAxisData = (JSONArray) xAxisObject.get("data");
-
-        assertThat(xAxisData.size()).isEqualTo(2);
-
-        JSONArray seriesArray = (JSONArray) JSON_PARSER.parse(newVersusFixedTrendChart.getAsString("series"));
-
-        assertThat(seriesArray.size()).isEqualTo(2);
-
-        JSONObject seriesObjectNew = (JSONObject) seriesArray.get(0);
-        JSONObject seriesObjectFixed = (JSONObject) seriesArray.get(1);
-        String seriesNewName = seriesObjectNew.getAsString("name");
-        String seriesFixedName = seriesObjectFixed.getAsString("name");
-
-        assertThat(seriesNewName).isEqualTo("New");
-        assertThat(seriesFixedName).isEqualTo("Fixed");
-
-        JSONArray seriesNewData = (JSONArray) seriesObjectNew.get("data");
-        JSONArray seriesFixedData = (JSONArray) seriesObjectFixed.get("data");
-
-        assertThat(convertToIntArray(seriesNewData)).isEqualTo(new int[] {0, 1});
-        assertThat(convertToIntArray(seriesFixedData)).isEqualTo(new int[] {0, 2});
+    /**
+     * Verifies New-Versus-Fixed Chart after a series of 2 builds.
+     *
+     * @param newVersusFixedTrendChart
+     *         ONObject with values from Severities new Versus Fixed TrendChart
+     */
+    private void verifyNewVersusFixedChart(final String newVersusFixedTrendChart) throws ParseException {
+//        JSONArray xAxisArray = (JSONArray) JSON_PARSER.parse(newVersusFixedTrendChart.getAsString("xAxis"));
+//        JSONObject xAxisObject = (JSONObject) xAxisArray.get(0);
+//        JSONArray xAxisData = (JSONArray) xAxisObject.get("data");
+//
+//        assertThat(xAxisData.size()).isEqualTo(2);
+//
+//        JSONArray seriesArray = (JSONArray) JSON_PARSER.parse(newVersusFixedTrendChart.getAsString("series"));
+//
+//        assertThat(seriesArray.size()).isEqualTo(2);
+//
+//        JSONObject seriesObjectNew = (JSONObject) seriesArray.get(0);
+//        JSONObject seriesObjectFixed = (JSONObject) seriesArray.get(1);
+//        String seriesNewName = seriesObjectNew.getAsString("name");
+//        String seriesFixedName = seriesObjectFixed.getAsString("name");
+//
+//        assertThat(seriesNewName).isEqualTo("New");
+//        assertThat(seriesFixedName).isEqualTo("Fixed");
+//
+//        JSONArray seriesNewData = (JSONArray) seriesObjectNew.get("data");
+//        JSONArray seriesFixedData = (JSONArray) seriesObjectFixed.get("data");
+//
+//        assertThat(convertToIntArray(seriesNewData)).isEqualTo(new int[] {0, 1});
+//        assertThat(convertToIntArray(seriesFixedData)).isEqualTo(new int[] {0, 2});
     }
 
     private FreeStyleJob createFreeStyleJob(final String... resourcesToCopy) {
