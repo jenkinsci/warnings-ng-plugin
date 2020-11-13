4566a73e2088af70276a83271107f5f3c31775ad
dogray <dogray@ebay.com>
2020-11-13 09:23:48 -0800

    Deprecate former constructor. Remove comment.

--- plugin/src/main/java/io/jenkins/plugins/analysis/core/restapi/ToolApi.java
+++ plugin/src/main/java/io/jenkins/plugins/analysis/core/restapi/ToolApi.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.analysis.core.restapi;
 
+import java.util.Collections;
 import java.util.Map;
 
 import edu.hm.hafner.analysis.Severity;
@@ -33,10 +34,27 @@ public class ToolApi {
      *         the URL to the latest results
      * @param size
      *         the number of warnings
+     * @deprecated
+     *         use {@link #ToolApi(String, String, String, int, Map)} instead.
+     */
+    @Deprecated
+    public ToolApi(final String id, final String name, final String latestUrl, final int size) {
+        this(name, id, latestUrl, size, Collections.emptyMap());
+    }
+
+    /**
+     * Creates a new instance of {@link ToolApi}.
+     *
+     * @param id
+     *         unique ID of the tool
+     * @param name
+     *         human readable name of the tool
+     * @param latestUrl
+     *         the URL to the latest results
+     * @param size
+     *         the number of warnings
      * @param sizePerSeverity
-     *         the number of warnings, grouped by severity. Assumption is that
-     *         this is not null, and that it is properly populated with counts
-     *         that when summed, total the "size" parameter.
+     *         the number of warnings, grouped by severity.
      */
     public ToolApi(final String id, final String name, final String latestUrl, final int size, final Map<Severity, Integer> sizePerSeverity) {
         this.name = name;
