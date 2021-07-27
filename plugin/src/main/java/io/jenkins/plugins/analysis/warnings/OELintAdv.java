0c3995e9ece8aeec6d0f50355e5eee69e8257783
offa <bm-dev@yandex.com>
2021-07-27 14:33:18 +0200

    Add oelint-adv support.

---
+++ plugin/src/main/java/io/jenkins/plugins/analysis/warnings/OELintAdv.java
@@ -0,0 +1,30 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.jenkinsci.Symbol;
+import hudson.Extension;
+
+import io.jenkins.plugins.analysis.core.model.AnalysisModelParser;
+
+/**
+ * Provides parsers and customized messages for oelint-adv.
+ */
+public class OELintAdv extends AnalysisModelParser {
+    private static final long serialVersionUID = 1L;
+    private static final String ID = "oelint-adv";
+
+    /** Creates a new instance of {@link OELintAdv}. */
+    @DataBoundConstructor
+    public OELintAdv() {
+    }
+
+    /** Descriptor for this static analysis tool. */
+    @Symbol("oelintAdv")
+    @Extension
+    public static class Descriptor extends AnalysisModelParserDescriptor {
+        /** Creates the descriptor instance. */
+        public Descriptor() {
+            super(ID);
+        }
+    }
+}
