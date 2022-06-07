bef022ff5ab08b98c758c83e4d8ad8106f0cca3f
Dominik Jantschar <Dominik.jantschar@adesso.de>
2022-06-07 09:38:29 +0200

    Updated revApi in pom.xml. Added RevApi blank parser.

---
+++ plugin/src/main/java/io/jenkins/plugins/analysis/warnings/RevApi.java
@@ -0,0 +1,38 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.jenkinsci.Symbol;
+import hudson.Extension;
+
+import io.jenkins.plugins.analysis.core.model.AnalysisModelParser;
+
+/**
+ * Provides a parser and customized messages for RevApi. Delegates to {@link }
+ */
+public class RevApi extends AnalysisModelParser {
+    private static final long serialVersionUID = -8571635906342563283L;
+
+    private static final String ID = "revapi";
+
+    /** Creates a new instance of {@link RevApi}. */
+    @DataBoundConstructor
+    public RevApi() {
+        super();
+        // empty constructor required for stapler
+    }
+
+    /** Descriptor for this static analysis tool. */
+    @Symbol("revApi")
+    @Extension
+    public static class Descriptor extends AnalysisModelParserDescriptor {
+        /** Creates the descriptor instance. */
+        public Descriptor() {
+            super(ID);
+        }
+
+        @Override
+        public boolean canScanConsoleLog() {
+            return false;
+        }
+    }
+}
