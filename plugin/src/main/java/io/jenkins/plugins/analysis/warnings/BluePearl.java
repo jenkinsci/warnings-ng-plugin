6c4eea4f9d68f53c4931860416ae78391df4eab3
Simon Matthews <simon@kahuna.sj.bps>
2022-06-08 15:39:50 -0700

    First pass at registering Blue Pearl parser

---
+++ plugin/src/main/java/io/jenkins/plugins/analysis/warnings/BluePearl.java
@@ -0,0 +1,36 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.jenkinsci.Symbol;
+import hudson.Extension;
+
+import io.jenkins.plugins.analysis.core.model.AnalysisModelParser;
+
+
+/**
+ * Selects a {@link BLue Pearl Parser} using the specified ID.
+ *
+ * @author Simon Matthews
+ */
+public class BluePearl extends AnalysisModelParser {
+    private static final long serialVersionUID = 1L;
+    private static final String ID = "bluepearl";
+
+  /** Creates a new instance of {@link BluePearl}. */
+    @DataBoundConstructor
+    public BluePearl() {
+        super();
+        // empty constructor required for stapler
+    }
+    @Symbol("bluePearl")
+    @Extension
+    public static class Descriptor extends AnalysisModelParserDescriptor {
+        /** Creates the descriptor instance. */
+        public Descriptor() {
+            super(ID);
+        }
+
+      
+    }
+
+}
