673127efd5466c8cdd50567a2e042ae43324b170
Alexander Brandes <mc.cache@web.de>
2022-07-11 09:58:40 +0200

    chore: Use stylelint SVG

--- plugin/src/main/java/io/jenkins/plugins/analysis/warnings/StyleLint.java
+++ plugin/src/main/java/io/jenkins/plugins/analysis/warnings/StyleLint.java
@@ -7,8 +7,8 @@ import org.jenkinsci.Symbol;
 import hudson.Extension;
 
 import io.jenkins.plugins.analysis.core.model.AnalysisModelParser;
-import io.jenkins.plugins.analysis.core.model.IconLabelProvider;
 import io.jenkins.plugins.analysis.core.model.StaticAnalysisLabelProvider;
+import io.jenkins.plugins.analysis.core.model.SvgIconLabelProvider;
 
 /**
  * Provides a parser and customized messages for Stylelint. Delegates to {@link CheckStyleParser}.
@@ -42,7 +42,7 @@ public class StyleLint extends AnalysisModelParser {
 
         @Override
         public StaticAnalysisLabelProvider getLabelProvider() {
-            return new IconLabelProvider(getId(), getDisplayName(), getDescriptionProvider());
+            return new SvgIconLabelProvider(getId(), getDisplayName(), getDescriptionProvider(), "stylelint");
         }
     }
 }
