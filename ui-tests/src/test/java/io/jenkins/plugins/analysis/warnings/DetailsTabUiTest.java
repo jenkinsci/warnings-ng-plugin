2dc49ebee07ceedb0f2f9a7f9b0bac0a6ef3e797
Ulli Hafner <ullrich.hafner@gmail.com>
2023-03-18 23:30:54 +0100

    Fix changed API in Selenium 4.

--- ui-tests/src/test/java/io/jenkins/plugins/analysis/warnings/DetailsTabUiTest.java
+++ ui-tests/src/test/java/io/jenkins/plugins/analysis/warnings/DetailsTabUiTest.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.analysis.warnings;
 
+import java.time.Duration;
 import java.util.Collection;
 import java.util.List;
 
@@ -316,7 +317,7 @@ public class DetailsTabUiTest extends UiTest {
      *         WebElement that should contain the expected string
      */
     private void waitUntilCondition(final WebElement target, final String expectedString) {
-        WebDriverWait wait = new WebDriverWait(driver, 2, 100);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2), Duration.ofMillis(100));
         wait.until(ExpectedConditions.textToBePresentInElement(target, expectedString));
     }
 }
