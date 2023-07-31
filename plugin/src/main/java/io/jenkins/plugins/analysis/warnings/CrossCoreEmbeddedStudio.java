ffff5ac8fea7e102b2997219c4b303b17a06a069
Arthur MINGASSON <arthur.mingasson@devialet.com>
2023-07-31 16:29:14 +0200

    Add parser for CCES (CrossCore Embedded Studio from Analog Devices)

---
+++ plugin/src/main/java/io/jenkins/plugins/analysis/warnings/CrossCoreEmbeddedStudio.java
@@ -0,0 +1,33 @@
+package io.jenkins.plugins.analysis.warnings;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.jenkinsci.Symbol;
+import hudson.Extension;
+
+import io.jenkins.plugins.analysis.core.model.AnalysisModelParser;
+
+/**
+ * A parser for CrossCore Embedded Studio (CCES) from Analog Devices
+ */
+public class CrossCoreEmbeddedStudio extends AnalysisModelParser {
+    private static final long serialVersionUID = 1814097426285660166L;
+    private static final String ID = "crosscore-embedded-studio";
+
+    /** Creates a new instance of {@link CrossCoreEmbeddedStudio}. */
+    @DataBoundConstructor
+    public CrossCoreEmbeddedStudio() {
+        super();
+        // empty constructor required for stapler
+    }
+
+    /** Descriptor for this static analysis tool. */
+    @Symbol("CrossCoreEmbeddedStudioParser")
+    @Extension
+    public static class Descriptor extends AnalysisModelParserDescriptor {
+        /** Creates the descriptor instance. */
+        public Descriptor() {
+            super(ID);
+        }
+    }
+
+}
