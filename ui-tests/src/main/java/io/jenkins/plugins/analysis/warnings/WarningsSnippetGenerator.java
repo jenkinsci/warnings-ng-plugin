3320e05aed965a22a3c653f282b0608d0c7738ee
Ulli Hafner <ullrich.hafner@gmail.com>
2024-03-13 08:48:20 +0100

    Wait for the configuration area to be visible.

--- ui-tests/src/main/java/io/jenkins/plugins/analysis/warnings/WarningsSnippetGenerator.java
+++ ui-tests/src/main/java/io/jenkins/plugins/analysis/warnings/WarningsSnippetGenerator.java
@@ -1,12 +1,16 @@
 package io.jenkins.plugins.analysis.warnings;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+
 import org.jenkinsci.test.acceptance.po.Control;
 import org.jenkinsci.test.acceptance.po.PageObject;
 import org.jenkinsci.test.acceptance.po.SnippetGenerator;
 import org.jenkinsci.test.acceptance.po.WorkflowJob;
 
 /**
- * Ehances thbe {@link PageObject} for the {@link SnippetGenerator} with the steps of the warnings plugin.
+ * Enhances the {@link PageObject} for the {@link SnippetGenerator} with the steps of the warnings plugin.
  *
  * @author Lion Kosiuk
  */
@@ -31,7 +35,9 @@ public class WarningsSnippetGenerator extends SnippetGenerator {
      */
     public IssuesRecorder selectRecordIssues() {
         selectSampleStep.select(RECORD_ISSUES_OPTION);
-        elasticSleep(2000);
+        waitFor().withTimeout(Duration.ofSeconds(5))
+                .until(find(By.xpath("//div[contains(@class, 'jenkins-form-label') and text() = 'recordIssues']"))::isDisplayed);
+
         return new IssuesRecorder(getContext(), "/prototype");
     }
 }
