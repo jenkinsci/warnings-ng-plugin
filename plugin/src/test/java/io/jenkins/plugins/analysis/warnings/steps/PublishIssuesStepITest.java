4a36b65f5f605b45b8665e60de6fce17cfd3c23b
Jesse Glick <jglick@cloudbees.com>
2024-04-12 11:26:29 -0400

    Trying to satisfy arbitrary demands from PMD & Checkstyle

--- plugin/src/test/java/io/jenkins/plugins/analysis/warnings/steps/PublishIssuesStepITest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/warnings/steps/PublishIssuesStepITest.java
@@ -4,18 +4,23 @@ import io.jenkins.plugins.analysis.core.model.ResultAction;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerTest;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.jupiter.api.Test;
 
 import static io.jenkins.plugins.analysis.core.assertions.Assertions.*;
 
 final class PublishIssuesStepITest extends IntegrationTestWithJenkinsPerTest {
     @Test
-    void noWorkspace() throws Exception {
+    void noWorkspace() {
         var p = createProject(WorkflowJob.class, "p");
         p.setDefinition(new CpsFlowDefinition("def r; node {r = scanForIssues tool: eclipse(pattern: 'x.txt')}; publishIssues issues: [r]", true));
-        getJenkins().jenkins.getWorkspaceFor(p).child("x.txt").copyFrom(PublishIssuesStepITest.class.getResource("eclipse.txt"));
-        var b = getJenkins().buildAndAssertSuccess(p);
+        WorkflowRun b;
+        try {
+            getJenkins().jenkins.getWorkspaceFor(p).child("x.txt").copyFrom(PublishIssuesStepITest.class.getResource("eclipse.txt"));
+            b = getJenkins().buildAndAssertSuccess(p);
+        } catch (Exception x) {
+            throw new AssertionError(x);
+        }
         assertThat(b.getAction(ResultAction.class).getResult().getIssues()).hasSize(8);
     }
-
 }
