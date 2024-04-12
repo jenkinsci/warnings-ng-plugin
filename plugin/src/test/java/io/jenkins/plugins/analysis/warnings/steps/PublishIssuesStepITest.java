20d1aa877b50b3e97564998ce4bf67bf2203671f
Jesse Glick <jglick@cloudbees.com>
2024-04-12 12:33:12 -0400

    Another attempt at satisfying PMD & Checkstyle (`pmd:check` works locally but `checkstyle:check` passes locally despite failing in CI)

--- plugin/src/test/java/io/jenkins/plugins/analysis/warnings/steps/PublishIssuesStepITest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/warnings/steps/PublishIssuesStepITest.java
@@ -4,23 +4,18 @@ import io.jenkins.plugins.analysis.core.model.ResultAction;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerTest;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.jupiter.api.Test;
 
 import static io.jenkins.plugins.analysis.core.assertions.Assertions.*;
 
 final class PublishIssuesStepITest extends IntegrationTestWithJenkinsPerTest {
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     @Test
-    void noWorkspace() {
+    void noWorkspace() throws Exception {
         var p = createProject(WorkflowJob.class, "p");
         p.setDefinition(new CpsFlowDefinition("def r; node {r = scanForIssues tool: eclipse(pattern: 'x.txt')}; publishIssues issues: [r]", true));
-        WorkflowRun b;
-        try {
-            getJenkins().jenkins.getWorkspaceFor(p).child("x.txt").copyFrom(PublishIssuesStepITest.class.getResource("eclipse.txt"));
-            b = getJenkins().buildAndAssertSuccess(p);
-        } catch (Exception x) {
-            throw new AssertionError(x);
-        }
+        getJenkins().jenkins.getWorkspaceFor(p).child("x.txt").copyFrom(PublishIssuesStepITest.class.getResource("eclipse.txt"));
+        var b = getJenkins().buildAndAssertSuccess(p);
         assertThat(b.getAction(ResultAction.class).getResult().getIssues()).hasSize(8);
     }
 }
