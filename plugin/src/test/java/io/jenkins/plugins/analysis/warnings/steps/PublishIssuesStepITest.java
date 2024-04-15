a7f4f755acff4cdd460124d918e74a6723d7661d
Ulli Hafner <ullrich.hafner@gmail.com>
2024-04-15 11:13:26 +0200

    Simplify test.

--- plugin/src/test/java/io/jenkins/plugins/analysis/warnings/steps/PublishIssuesStepITest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/warnings/steps/PublishIssuesStepITest.java
@@ -1,21 +1,21 @@
 package io.jenkins.plugins.analysis.warnings.steps;
 
+import org.junit.jupiter.api.Test;
+
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+
 import io.jenkins.plugins.analysis.core.model.ResultAction;
 import io.jenkins.plugins.analysis.core.testutil.IntegrationTestWithJenkinsPerTest;
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.junit.jupiter.api.Test;
 
 import static io.jenkins.plugins.analysis.core.assertions.Assertions.*;
 
-final class PublishIssuesStepITest extends IntegrationTestWithJenkinsPerTest {
-    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+class PublishIssuesStepITest extends IntegrationTestWithJenkinsPerTest {
     @Test
     void noWorkspace() throws Exception {
-        var p = createProject(WorkflowJob.class, "p");
-        p.setDefinition(new CpsFlowDefinition("def r; node {r = scanForIssues tool: eclipse(pattern: 'x.txt')}; publishIssues issues: [r]", true));
-        getJenkins().jenkins.getWorkspaceFor(p).child("x.txt").copyFrom(PublishIssuesStepITest.class.getResource("eclipse.txt"));
-        var b = getJenkins().buildAndAssertSuccess(p);
-        assertThat(b.getAction(ResultAction.class).getResult().getIssues()).hasSize(8);
+        var job = createPipelineWithWorkspaceFilesWithSuffix("eclipse.txt");
+        job.setDefinition(new CpsFlowDefinition("def r; node {r = scanForIssues tool: eclipse(pattern: '*issues.txt')}; publishIssues issues: [r]", true));
+
+        var build = getJenkins().buildAndAssertSuccess(job);
+        assertThat(build.getAction(ResultAction.class).getResult().getIssues()).hasSize(8);
     }
 }
