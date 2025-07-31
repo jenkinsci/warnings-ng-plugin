42db5abbf8fc6ce4cf628d3903a7bdb573f5f123
Ulli Hafner <ullrich.hafner@gmail.com>
2025-07-31 16:53:37 +0200

    [JENKINS-75935][JENKINS-75748] Remove duplicate transient actions
    It is sufficient to return the found project actions.
    The previously created transient action does not make sense.

--- plugin/src/main/java/io/jenkins/plugins/analysis/core/model/MissingResultFallbackHandler.java
+++ plugin/src/main/java/io/jenkins/plugins/analysis/core/model/MissingResultFallbackHandler.java
@@ -55,7 +55,6 @@ public final class MissingResultFallbackHandler extends TransientActionFactory<J
             List<Action> actions = new ArrayList<>();
             for (ResultAction action : resultActions) {
                 actions.addAll(action.getProjectActions());
-                actions.add(new TransientProjectResultAction(action));
             }
 
             if (!resultActions.isEmpty()) {
@@ -64,28 +63,5 @@ public final class MissingResultFallbackHandler extends TransientActionFactory<J
         }
 
         return Collections.emptyList();
-    }
-
-    /**
-     * A wrapper record for {@link ResultAction} that provides an absolute URL for the link on the side panel.
-     *
-     * @param resultAction
-     *          Valid Result Action
-     */
-    private record TransientProjectResultAction(ResultAction resultAction) implements Action {
-        @Override
-        public String getIconFileName() {
-            return resultAction.getIconFileName();
-        }
-
-        @Override
-        public String getDisplayName() {
-            return resultAction.getDisplayName();
-        }
-
-        @Override
-        public String getUrlName() {
-            return resultAction.getAbsoluteUrl();
-        }
     }
 }
