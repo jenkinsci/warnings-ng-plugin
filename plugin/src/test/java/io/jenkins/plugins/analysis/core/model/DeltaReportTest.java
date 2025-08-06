b6f489057177048fa095ffd63d6899834c0f4394
Valentin Delaye <jonesbusy@users.noreply.github.com>
2025-08-06 07:13:57 +0200

    Remove usages of Commons Lang 2

--- plugin/src/test/java/io/jenkins/plugins/analysis/core/model/DeltaReportTest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/core/model/DeltaReportTest.java
@@ -1,6 +1,6 @@
 package io.jenkins.plugins.analysis.core.model;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 import edu.hm.hafner.analysis.Issue;
