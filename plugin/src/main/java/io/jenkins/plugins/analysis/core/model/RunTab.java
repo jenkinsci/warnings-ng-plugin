4d6584df66891ba8db256a0af1d7410b0b838132
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-10-21 23:12:36 +0100

    Working

--- plugin/src/main/java/io/jenkins/plugins/analysis/core/model/RunTab.java
+++ plugin/src/main/java/io/jenkins/plugins/analysis/core/model/RunTab.java
@@ -1,11 +1,17 @@
 package io.jenkins.plugins.analysis.core.model;
 
 import hudson.model.Actionable;
+import jakarta.servlet.ServletException;
 import jenkins.model.Tab;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
+import org.kohsuke.stapler.StaplerRequest2;
+import org.kohsuke.stapler.StaplerResponse2;
 
+import java.io.IOException;
 import java.util.List;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
 
 /**
  * TODO
@@ -40,13 +46,27 @@ public class RunTab extends Tab {
         return getObject().getActions(ResultAction.class);
     }
 
-    public ResultAction getDynamic(String name) {
+    public void doIndex(StaplerRequest2 req, StaplerResponse2 rsp) throws IOException, ServletException {
+        req.setAttribute("resultAction", getWarningActions().get(0));
+        req.getView(this, "index.jelly").forward(req, rsp);
+    }
+
+    public void doDynamic(StaplerRequest2 req, StaplerResponse2 rsp) throws IOException, ServletException {
+        String action = req.getRestOfPath().substring(1);
+        ResultAction resultAction = null;
+
         for (ResultAction ui : getWarningActions()) {
             String urlName = ui.getUrlName();
-            if (urlName != null && urlName.equals(name)) {
-                return ui;
+            if (urlName != null && urlName.equals(action)) {
+                resultAction = ui;
             }
         }
-        return null;
+
+        if (resultAction == null) {
+            return;
+        }
+
+        req.setAttribute("resultAction", resultAction);
+        req.getView(this, "index.jelly").forward(req, rsp);
     }
 }
