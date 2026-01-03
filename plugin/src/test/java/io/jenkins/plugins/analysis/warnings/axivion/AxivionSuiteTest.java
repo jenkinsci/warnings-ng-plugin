976db167af9d509fab061dbd083f90127a5a24b9
Ulli Hafner <ullrich.hafner@gmail.com>
2026-01-03 17:54:30 +0100

    Add `ignoreSuppressedOrJustified` in `hashCode` evaluation

--- plugin/src/test/java/io/jenkins/plugins/analysis/warnings/axivion/AxivionSuiteTest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/warnings/axivion/AxivionSuiteTest.java
@@ -2,6 +2,9 @@ package io.jenkins.plugins.analysis.warnings.axivion;
 
 import org.junit.jupiter.api.Test;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+
 import static org.assertj.core.api.Assertions.*;
 
 class AxivionSuiteTest {
@@ -16,5 +19,12 @@ class AxivionSuiteTest {
 
         assertThat(newTool("https://axivion.com/projects/vscode plugin").getProjectUrl())
                 .isEqualTo("https://axivion.com/projects/vscode%20plugin");
+    }
+
+    @Test
+    void shouldAdhereToEquals() {
+        EqualsVerifier.forClass(AxivionSuite.class)
+                .suppress(Warning.NULL_FIELDS, Warning.NONFINAL_FIELDS)
+                .verify();
     }
 }
