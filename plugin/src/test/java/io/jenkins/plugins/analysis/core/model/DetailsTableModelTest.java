1c3be301235cfeb16dfc9c3a84005b8c992d8d29
Akash Manna <akash.manna.mymail@gmail.com>
2026-01-08 12:32:41 +0530

    [JENKINS-62036] Issue parsers display incorrect data when message or description contains HTML

--- plugin/src/test/java/io/jenkins/plugins/analysis/core/model/DetailsTableModelTest.java
+++ plugin/src/test/java/io/jenkins/plugins/analysis/core/model/DetailsTableModelTest.java
@@ -28,6 +28,67 @@ class DetailsTableModelTest extends AbstractDetailsModelTest {
     }
 
     @Test
+    @org.junitpioneer.jupiter.Issue("JENKINS-62036")
+    void shouldEscapeHtmlInMessage() {
+        try (var builder = new IssueBuilder()) {
+            builder.setMessage("<script>alert('XSS')</script><b>Bold text</b>");
+            var model = createRow(builder.build());
+
+            var actualColumn = model.getDescription();
+            var actualMessage = model.getMessage();
+            
+            assertThat(actualColumn)
+                    .doesNotContain("<script>alert")
+                    .doesNotContain("<b>Bold text</b>")
+                    .contains("&amp;lt;script&amp;gt;")
+                    .contains("&amp;lt;b&amp;gt;")
+                    .contains("Bold text");
+            
+            assertThat(actualMessage)
+                    .doesNotContain("<script>")
+                    .doesNotContain("</script>")
+                    .doesNotContain("<b>Bold text</b>")
+                    .contains("&lt;script&gt;")
+                    .contains("&lt;b&gt;")
+                    .contains("Bold text");
+        }
+    }
+
+    @Test
+    @org.junitpioneer.jupiter.Issue("JENKINS-62036")
+    void shouldEscapeHtmlInDescription() {
+        try (var builder = new IssueBuilder()) {
+            builder.setMessage("Normal message");
+            var issue = builder.build();
+            var model = createRow(issue, i -> "<img src=x onerror=alert('XSS')><div>Description</div>");
+
+            var actualColumn = model.getDescription();
+            
+            assertThat(actualColumn)
+                    .doesNotContain("<img src=x onerror")  
+                    .doesNotContain("<div>Description</div>")
+                    .contains("&amp;lt;img")
+                    .contains("&amp;lt;div&amp;gt;")
+                    .contains("Description");
+        }
+    }
+
+    @Test
+    @org.junitpioneer.jupiter.Issue("JENKINS-62036")
+    void shouldEscapeSpecialHtmlCharacters() {
+        try (var builder = new IssueBuilder()) {
+            builder.setMessage("Error: 'value' < 10 && 'name' > \"test\"");
+            var model = createRow(builder.build());
+
+            var actualColumn = model.getDescription();
+            var actualMessage = model.getMessage();
+            
+            assertThat(actualColumn).contains("&lt;").contains("&gt;");
+            assertThat(actualMessage).contains("&lt;").contains("&gt;");
+        }
+    }
+
+    @Test
     void shouldCreateSortableFileName() {
         var issue = createIssue(1);
         var model = createRow(issue);
@@ -49,6 +110,11 @@ class DetailsTableModelTest extends AbstractDetailsModelTest {
 
     private TableRow createRow(final Issue issue) {
         return new TableRow(createAgeBuilder(), createFileNameRenderer(), i -> DESCRIPTION, issue,
+                createJenkinsFacade());
+    }
+
+    private TableRow createRow(final Issue issue, final DescriptionProvider descriptionProvider) {
+        return new TableRow(createAgeBuilder(), createFileNameRenderer(), descriptionProvider, issue,
                 createJenkinsFacade());
     }
 }
