45afa3ade686da676a24e454b4eb18ae9ee430e6
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2026-04-17 08:52:06 +0100

    Address style issue

--- plugin/src/main/java/io/jenkins/plugins/analysis/core/model/RunTabFactory.java
+++ plugin/src/main/java/io/jenkins/plugins/analysis/core/model/RunTabFactory.java
@@ -13,15 +13,15 @@ import java.util.List;
  * Adds a Warnings tab to the run page.
  */
 @Extension
-public class RunTabFactory extends TransientActionFactory<Run> {
+public class RunTabFactory extends TransientActionFactory<Run<?, ?>> {
     @Override
-    public Class<Run> type() {
-        return Run.class;
+    public Class<Run<?, ?>> type() {
+        return (Class<Run<?, ?>>) (Class<?>) Run.class;
     }
 
     @NonNull
     @Override
-    public Collection<? extends Tab> createFor(@NonNull final Run target) {
+    public Collection<? extends Tab> createFor(@NonNull final Run<?, ?> target) {
         return List.of(new RunTab(target));
     }
 }
