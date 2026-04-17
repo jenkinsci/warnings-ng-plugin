62e4c7bebd811f52a2a0a8c1727d9efbb7b5b253
Ulli Hafner <ullrich.hafner@gmail.com>
2026-04-17 08:32:33 +0200

    Fix broken Jenkins baseline and analysis model version
    Also remove javac warnings.

--- plugin/src/main/java/io/jenkins/plugins/analysis/core/model/RunTabFactory.java
+++ plugin/src/main/java/io/jenkins/plugins/analysis/core/model/RunTabFactory.java
@@ -1,27 +1,29 @@
 package io.jenkins.plugins.analysis.core.model;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+
+import java.util.Collection;
+import java.util.List;
+
 import hudson.Extension;
 import hudson.model.Run;
 import jenkins.model.Tab;
 import jenkins.model.TransientActionFactory;
 
-import java.util.Collection;
-import java.util.List;
-
 /**
  * Adds a Warnings tab to the run page.
  */
 @Extension
-public class RunTabFactory extends TransientActionFactory<Run> {
+public class RunTabFactory extends TransientActionFactory<Run<?, ?>> {
     @Override
-    public Class<Run> type() {
-        return Run.class;
+    @SuppressWarnings("unchecked")
+    public Class<Run<?, ?>> type() {
+        return (Class) Run.class;
     }
 
     @NonNull
     @Override
-    public Collection<? extends Tab> createFor(@NonNull final Run target) {
+    public Collection<? extends Tab> createFor(@NonNull final Run<?, ?> target) {
         return List.of(new RunTab(target));
     }
 }
