1c9e1f344707e779f9510b3a42a3707fe1355afc
Akash Manna <akash.manna.mymail@gmail.com>
2026-08-09 23:37:45 +0530

    Refactor permission checks in GrepParser to use project context

--- plugin/src/main/java/io/jenkins/plugins/analysis/warnings/GrepParser.java
+++ plugin/src/main/java/io/jenkins/plugins/analysis/warnings/GrepParser.java
@@ -234,7 +234,7 @@ public class GrepParser extends Tool {
         @POST
         public FormValidation doCheckIncludePattern(@AncestorInPath final AbstractProject<?, ?> project,
                 @QueryParameter final String includePattern) {
-            if (!JENKINS.hasPermission(Item.CONFIGURE, project)) {
+            if (!project.hasPermission(Item.CONFIGURE)) {
                 return FormValidation.ok();
             }
             return VALIDATION_UTILITIES.doCheckPattern(project, includePattern);
@@ -253,7 +253,7 @@ public class GrepParser extends Tool {
         @POST
         public FormValidation doCheckExcludePattern(@AncestorInPath final AbstractProject<?, ?> project,
                 @QueryParameter final String excludePattern) {
-            if (!JENKINS.hasPermission(Item.CONFIGURE, project)) {
+            if (!project.hasPermission(Item.CONFIGURE)) {
                 return FormValidation.ok();
             }
             return VALIDATION_UTILITIES.doCheckPattern(project, excludePattern);
@@ -272,7 +272,7 @@ public class GrepParser extends Tool {
         @POST
         public FormValidation doCheckRegexp(@AncestorInPath final BuildableItem project,
                 @QueryParameter final String regexp) {
-            if (!JENKINS.hasPermission(Item.CONFIGURE, project)) {
+            if (!project.hasPermission(Item.CONFIGURE)) {
                 return FormValidation.ok();
             }
             return checkRegexp(regexp);
@@ -326,7 +326,7 @@ public class GrepParser extends Tool {
                 @QueryParameter final String example,
                 @QueryParameter final String regexp,
                 @QueryParameter final String severity) {
-            if (StringUtils.isEmpty(example) || !JENKINS.hasPermission(Item.CONFIGURE, project)) {
+            if (StringUtils.isEmpty(example) || !project.hasPermission(Item.CONFIGURE)) {
                 return FormValidation.ok();
             }
 
